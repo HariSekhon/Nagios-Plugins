93495d08733c2ae1808401716263c3480c0e6452
Hari Sekhon <harisekhon@gmail.com>
2013-01-01 22:47:36 +0000

    imported lib_check_nrpe.sh

---
+++ legacy/lib_check_nrpe.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+#  Author: Hari Sekhon
+#  Date: 2007-11-14 10:21:36 +0000 (Wed, 14 Nov 2007)
+#
+#  http://github.com/harisekhon
+#
+#  License: see accompanying LICENSE file
+#
+
+export PATH=$PATH:/usr/lib64/nagios/plugins:/usr/lib/nagios/plugins:/usr/nagios/libexec:/usr/local/nagios/libexec
+
+if ! which check_nrpe &>/dev/null; then
+    echo "CRITICAL: check_nrpe was not found in path"
+    exit $CRITICAL
+fi 
+check_nrpe=`which check_nrpe`
+
+
+if [ ! -f "$check_nrpe" ]; then
+    echo "CRITICAL: $check_nrpe cannot be found"
+    exit $CRITICAL
+fi
+
+if [ ! -x "$check_nrpe" ]; then
+    echo "CRITICAL: $check_nrpe is not set executable!"
+    exit $CRITICAL
+fi
