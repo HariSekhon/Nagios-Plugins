4b1ad95a791c5f5296aa3c5c7361f506404f408b
Hari Sekhon <harisekhon@gmail.com>
2013-01-01 23:37:02 +0000

    imported check_git_branch_checkout.pl

---
+++ check_git_branch_checkout.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl -T
+# nagios: -epn
+#
+#  Author: Hari Sekhon
+#  Date: 2012-07-26 12:09:53 +0100 (Thu, 26 Jul 2012)
+#
+#  http://github.com/harisekhon
+#
+#  License: see accompanying LICENSE file
+#
+
+# Nagios Plugin to check a Git working copy is in the right branch.
+
+# Primarily written for puppetmasters to make sure prod and staging
+# environments dirs had the right branches checked out in them
+
+$VERSION = "0.1";
+
+use strict;
+use warnings;
+BEGIN {
+    use File::Basename;
+    use lib dirname(__FILE__);
+}
+use HariSekhonUtils;
+
+my $directory;
+my $branch;
+my $branch_checkout;
+my $git_default = "git";
+my $git = $git_default;
+%options = (
+    "d|directory=s" => [ \$directory, "Directory path to git working copy" ],
+    "b|branch=s"    => [ \$branch,    "Branch to expect working copy checkout to be" ],
+    "git-binary=s"  => [ \$git,       "Path to git binary. Defaults to '$git_default'" ],
+);
+@usage_order = qw/directory branch/;
+
+get_options();
+
+$directory = validate_directory($directory);
+$branch or usage "branch name not specified";
+$branch    =~ /^([\w-]+)$/ or usage "Invalid branch name given, must be alpha-numeric";
+$branch    = $1;
+$git       = validate_filename($git) or usage "Invalid git path given, failed filename regex";
+$git       =~ /\bgit$/ or usage "Invalid git path given, is not a path to the git command";
+
+vlog2 "directory: $directory
+branch:    $branch
+";
+set_timeout();
+
+chdir($directory) or quit "CRITICAL", "Failed to chdir to directory '$directory'";
+my @output = cmd("$git branch --color=never", 1);
+foreach(@output){
+    if(/^\*\s+(.+)\s*$/){
+        $branch_checkout = $1;
+        last;
+    }
+}
+defined($branch_checkout) or quit "CRITICAL", "Failed to determine current branch checkout for directory '$directory'";
+
+if($branch_checkout eq $branch){
+    quit "OK", "branch '$branch_checkout' currently checked out in directory '$directory'";
+} else {
+    quit "CRITICAL", "branch '$branch_checkout' checked out, expecting '$branch' in directory '$directory'";
+}
