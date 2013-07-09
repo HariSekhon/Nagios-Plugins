5df9b3b4b1dfdc4c7315fd587bd7dcd440c6a79b
Hari Sekhon <harisekhon@gmail.com>
2013-07-09 04:15:43 +0100

    added support to fetch all metrics

--- check_hadoop_metrics.pl
+++ check_hadoop_metrics.pl
@@ -9,9 +9,11 @@
 #  License: see accompanying LICENSE file
 #
 
-$DESCRIPTION = "Nagios Plugin to parse metrics from a given Hadoop daemon's /metrics page";
+$DESCRIPTION = "Nagios Plugin to parse metrics from a given Hadoop daemon's /metrics page
 
-$VERSION = "0.1";
+Currently only supporting the jvm/mapred sections, Fair Scheduler stats from JobTracker may be supported in a later version";
+
+$VERSION = "0.2";
 
 use strict;
 use warnings;
@@ -23,29 +25,33 @@ use LWP::Simple qw/get $ua/;
 use HariSekhonUtils;
 
 my $metrics;
+my $all_metrics;
 
 %options = (
     "H|host=s"         => [ \$host,         "Host to connect to" ],
     "P|port=s"         => [ \$port,         "Port to connect to" ],
     "m|metrics=s"      => [ \$metrics,      "Metric(s) to collect, comma separated. Output in the order specified for convenience. Optional thresholds will only be applied when a single metrics is given" ],
+    "a|all-metrics"    => [ \$all_metrics,  "Grab all metrics. Useful if you don't know what to monitor yet or just want to graph everything" ],
     "w|warning=s"      => [ \$warning,      "Warning  threshold or ran:ge (inclusive)" ],
     "c|critical=s"     => [ \$critical,     "Critical threshold or ran:ge (inclusive)" ],
 );
 
-@usage_order = qw/host port metrics warning critical/;
+@usage_order = qw/host port metrics all-metrics warning critical/;
 get_options();
 
 $host       = validate_hostname($host);
 $port       = validate_port($port);
 my $url     = "http://$host:$port/metrics";
-defined($metrics) or usage "no metrics specified";
-my @stats;
 my %stats;
-foreach(split(/\s*[,\s]\s*/, $metrics)){
-    /^\w+$/ or usage "invalid metrics $_ given, must be alphanumeric with underscores";
-    push(@stats, $_);
+my @stats;
+unless($all_metrics){
+    defined($metrics) or usage "no metrics specified";
+    foreach(split(/\s*[,\s]\s*/, $metrics)){
+        /^\w+$/ or usage "invalid metrics $_ given, must be alphanumeric with underscores";
+        push(@stats, $_);
+    }
+    @stats or usage "no valid metrics specified";
 }
-@stats or usage "no valid metrics specified";
 validate_thresholds();
 
 vlog2;
@@ -54,7 +60,7 @@ set_timeout();
 $status = "OK";
 
 # ============================================================================ #
-# lifted from my check_hadoop_jobtracker.pl plugin
+# lifted from my check_hadoop_jobtracker.pl plugin, modified to support $all_metrics
 vlog2 "querying $url";
 my $content = get $url;
 my ($result, $err) = ($?, $!);
@@ -69,22 +75,41 @@ unless($content){
 }
 
 sub check_stats_parsed(){
-    foreach(@stats){
-        unless(defined($stats{$_})){
-            vlog2;
-            quit "UNKNOWN", "failed to find $_ in JobTracker output";
+    if($all_metrics){
+        if(scalar keys %stats < 10){
+            quit "UNKNOWN", "<10 stats collected from /metrics page, this must be an error, try running with -vvv to see what the deal is";
         }
-        vlog2 "stats $_ = $stats{$_}";
+        foreach(sort keys %stats){
+            vlog2 "stats $_ = $stats{$_}";
+        }
+    } else {
+        foreach(@stats){
+            unless(defined($stats{$_})){
+                vlog2;
+                quit "UNKNOWN", "failed to find $_ in JobTracker output";
+            }
+            vlog2 "stats $_ = $stats{$_}";
+        }
     }
     vlog2;
 }
 
 sub parse_stats(){
+    my $jvm_seen = 0;
     foreach my $line (split("\n", $content)){
-        foreach my $stat (@stats){
-            if($line =~ /^\s*$stat\s*=\s*(\d+(?:\.\d+)?)\s*$/){
-                $stats{$stat} = $1;
-                last;
+        # TODO: support Fair Scheduler metrics for MAP and REDUCE sections later, right now only doing JVM and Mapred stats
+        $line =~ /^jvm/ and $jvm_seen = 1;
+        next unless $jvm_seen;
+        if($all_metrics){
+            if($line =~ /^\s*(\w+)\s*=\s*(\d+(?:\.\d+)?)\s*$/){
+                $stats{$1} = $2;
+            }
+        } else {
+            foreach my $stat (@stats){
+                if($line =~ /^\s*$stat\s*=\s*(\d+(?:\.\d+)?)\s*$/){
+                    $stats{$stat} = $1;
+                    last;
+                }
             }
         }
     }
@@ -96,10 +121,21 @@ vlog2 "parsing metrics from '$host:$port'";
 parse_stats();
 
 $msg = "";
-foreach(@stats){
-    $msg .= "$_=$stats{$_} ";
+if($all_metrics){
+    foreach(sort keys %stats){
+        $msg .= "$_=$stats{$_} ";
+    }
+} else {
+    foreach(@stats){
+        $msg .= "$_=$stats{$_} ";
+    }
 }
-if(scalar @stats == 1){
+if($all_metrics){
+    $msg .= "| ";
+    foreach(sort keys %stats){
+        $msg .= "$_=$stats{$_} ";
+    }
+} elsif(!$all_metrics and scalar @stats == 1){
     check_thresholds($stats{$stats[0]});
     $msg .= " | $stats[0]=$stats{$stats[0]};" . ($thresholds{warning}{upper} ? $thresholds{warning}{upper} : "") . ";" . ($thresholds{critical}{upper} ? $thresholds{critical}{upper} : "");
 } else {
