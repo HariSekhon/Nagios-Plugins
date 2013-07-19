02556b49106d6df792b5fbb4c8fc3be45aaaec23
Hari Sekhon <harisekhon@gmail.com>
2013-07-20 00:35:43 +0100

    description wording tweak

--- check_context_switches.pl
+++ check_context_switches.pl
@@ -9,7 +9,7 @@
 #  License: see accompanying LICENSE file
 #
 
-$DESCRIPTION = "Nagios Plugin to check the number of context switches on the Linux server";
+$DESCRIPTION = "Nagios Plugin to check the number of context switches on the local Linux server. Designed to be called over NRPE";
 
 $VERSION = "0.2";
 
