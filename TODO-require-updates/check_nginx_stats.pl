48f91182c81f592bce49a4c84c50859859dcf3f7
Hari Sekhon <harisekhon@gmail.com>
2013-07-22 01:57:17 +0100

    rewrite check_nginx_stats to reintegrate with my library

--- TODO-require-updates/check_nginx_stats.pl
+++ TODO-require-updates/check_nginx_stats.pl
@@ -9,33 +9,25 @@
 #  License: see accompanying LICENSE file
 #
 
-# TODO: reintegrate this with HariSekhonUtils
+$DESCRIPTION = "Nagios Plugin to check Nginx stats from the nginx stub_status page";
 
-$main::VERSION = "0.3.1";
+$VERSION = "0.4";
 
 use strict;
 use warnings;
 use Fcntl ':flock';
-use Getopt::Long qw(:config bundling);
-use LWP::UserAgent;
-use POSIX;
 BEGIN {
     use File::Basename;
-    use lib dirname(__FILE__);
+    use lib dirname(__FILE__) . "/lib";
 }
-use utils qw(%ERRORS $TIMEOUT);
-
-delete @ENV{qw(IFS CDPATH ENV BASH_ENV)};
-$ENV{'PATH'} = '/bin:/usr/bin';
+use HariSekhonUtils qw/:DEFAULT :regex/;
+use LWP::UserAgent;
+use POSIX;
 
 my $conns_sec;
 my $contents;
-my $critical;
 my $default_port = 80;
-my $default_timeout = 10;
 my $diff_secs;
-my $help;
-my $host;
 my $last_accepted = 0;
 my $last_active = 0;
 my $last_handled;
@@ -48,99 +40,42 @@ my $no_keepalives = 0;
 my $port = $default_port;
 my $requests_sec;
 my $state_file_empty = 0;
-my $timeout = $default_timeout;
 my $url;
-my $verbose = 0;
-my $version;
-my $warning;
 
-sub quit{
-    print "$_[0]: $_[1]\n";
-    exit $ERRORS{$_[0]};
-}
+%options = (
+    "H|host=s"         => [ \$host,          "Host to connect to" ],
+    "P|port=s"         => [ \$port,          "Port to connect to (defaults to $default_port)" ],
+    "u|url=s"          => [ \$url,           "Nginx Status URL (usually something like /nginx_status - must be compiled with support and enabled in the nginx config using stub_status)" ],
+    "no-keepalives"    => [ \$no_keepalives, "Use only when nginx config has 'keepalive_timeout 0' to enable an extra sanity check against the Handled/Request counts where Handled >= Requests must be true. A sanity check of Accepted >= Handled is performed regardless" ],
+    "w|warning=s"      => [ \$warning,       "Warning  threshold or ran:ge (inclusive) for Active Connections count" ],
+    "c|critical=s"     => [ \$critical,      "Critical threshold or ran:ge (inclusive) for Active Connections count" ],
+);
 
-my $progname = basename $0; 
-$progname =~ /^([\w_\.-]+)$/ or die "script name was invalid\n";
-$progname = $1;
+@usage_order = qw/host port url no-keepalives warning critical/;
 
-sub usage{
-    print "@_\n" if defined(@_);
-    print "usage: $progname -H host [ -p port ] -u '/nginx_status' -w <warning_threshold> -c <critical_threshold> [ -t secs -v ]
+get_options();
 
---host     -H    Host to connect to
---port     -p    Port to connect to (defaults to $default_port)
---url      -u    Nginx Status URL (usually something like /nginx_status - must be compiled with support and enabled on the nginx config)
---warning  -w    The warning threshold for Active Connections
---critical -c    The critical threshold for Active Connections
---no-keepalives  Use only when nginx config has 'keepalive_timeout 0' to enable an extra sanity check against the Handled/Request counts where Handled >= Requests must be true. A sanity check of Accepted >= Handled is performed regardless
---timeout  -t    Timeout in seconds (defaults to $default_timeout, min 1, max 60)
---version  -V    Show version and exit
---verbose  -v    Verbose mode
---help     -h    Print this help
-\n";
-    exit $ERRORS{"UNKNOWN"};
-}
+$host = validate_hostname($host);
+$port = validate_port($port);
+$url  = validate_url_path_suffix($url, "nginx stub");
+$url  =~ s/^\///;
+$url  = "http://$host:$port/$url";
+$url  = validate_url($url, "full");
 
-sub vlog{
-    print "@_\n" if $verbose;
-}
+validate_thresholds(undef, undef, { "simple" => "upper", "integer" => 1, "positive" => 1 } );
+vlog2;
 
-GetOptions (
-            "h|help|usage"       => \$help,
-            "H|host=s"           => \$host,
-            "p|port=i"           => \$port,
-            "u|url=s"            => \$url,
-            "w|warning=i"        => \$warning,
-            "c|critical=i"       => \$critical,
-            "t|timeout=i"        => \$timeout,
-            "no-keepalives"      => \$no_keepalives,
-            "v|verbose+"         => \$verbose,
-            "V|version"          => \$version,
-           ) or usage "invalid option specified";
-
-usage if defined($help);
-die "$progname Version $main::VERSION\n" if defined($version);
-
-defined($host)                  || usage "hostname not specified";
-$host =~ /^([\w\.-]+)$/         || die "invalid hostname given\n";
-$host = $1;
-
-defined($port)                  || usage "port not specified";
-$port  =~ /^(\d+)$/             || die "invalid port number given, must be a positive integer\n";
-$port = $1;
-($port >= 1 && $port <= 65535)  || die "invalid port number given, must be between 1-65535)\n";
-
-defined($url) or usage "url is not specified";
-$url =~ /^(\/?[\w\.\;\=\&\%\/-]+)$/ or die "Invalid URL given\n";
-$url = $1;
-
-defined($warning)       || usage "warning threshold not defined";
-defined($critical)      || usage "critical threshold not defined";
-$warning  =~ /^(\d+)$/  || usage "invalid warning threshold given, must be a positive numeric integer";
-$warning = $1;
-$critical =~ /^(\d+)$/  || usage "invalid critical threshold given, must be a positive numeric integer";
-$critical = $1;
-($critical >= $warning) || usage "critical threshold must be greater than or equal to the warning threshold";
-
-$timeout =~ /^\d+$/                 || die "timeout value must be a positive integer\n";
-($timeout >= 1 && $timeout <= 60)   || die "timeout value must 1 - 60 secs\n";
-
-$SIG{ALRM} = sub {
-    quit "UNKNOWN", "check timed out after $timeout seconds";
-};
-vlog "verbose mode on";
-vlog "setting plugin timeout to $timeout secs\n";
-alarm($timeout);
+set_timeout();
 
 my $statefh;
 my $statefile = "/tmp/$progname.$host.tmp";
 my $state_file_existed = 0;
 if(-f $statefile){
     $state_file_existed = 1;
-    vlog "opening state file '$statefile'\n";
+    vlog2 "opening state file '$statefile'\n";
     open $statefh, "+<$statefile" or quit "UNKNOWN", "Error: failed to open state file '$statefile': $!";
 } else {
-    vlog "creating state file '$statefile'\n";
+    vlog2 "creating state file '$statefile'\n";
     open $statefh, "+>$statefile" or quit "UNKNOWN", "Error: failed to create state file '$statefile': $!";
 }
 flock($statefh, LOCK_EX | LOCK_NB) or quit "UNKNOWN", "Failed to aquire a lock on state file '$statefile', another instance of this plugin was running?";
@@ -152,9 +87,9 @@ if($state_file_existed == 1){
         chomp $contents;
         if($contents =~ /^\s+$/){
             $state_file_empty = 1;
-            vlog "state file is empty";
+            vlog2 "state file is empty";
         } else {
-            vlog "checking state file against regex: '$state_regex'\n";
+            vlog2 "checking state file against regex: '$state_regex'\n";
             $contents =~ /$state_regex/ or quit "UNKNOWN", "Error: state file '$statefile' was not in the expected format, offending line was \"$_\"";
             $last_tstamp   = $1;
             $last_active   = $2;
@@ -164,17 +99,17 @@ if($state_file_existed == 1){
             $last_accepted = $6;
             $last_handled  = $7;
             $last_requests = $8;
-            vlog "last active:   $last_active";
-            vlog "last reading:  $last_reading";
-            vlog "last writing:  $last_writing";
-            vlog "last waiting:  $last_waiting";
-            vlog "last accepted: $last_accepted";
-            vlog "last_handled:  $last_handled";
-            vlog "last requests: $last_requests";
+            vlog2 "last active:   $last_active";
+            vlog2 "last reading:  $last_reading";
+            vlog2 "last writing:  $last_writing";
+            vlog2 "last waiting:  $last_waiting";
+            vlog2 "last accepted: $last_accepted";
+            vlog2 "last_handled:  $last_handled";
+            vlog2 "last requests: $last_requests";
         }
     } else {
         $state_file_empty = 1;
-        vlog "state file is empty";
+        vlog2 "state file is empty";
     }
 }
 
@@ -185,36 +120,26 @@ if($last_tstamp > 0 and $last_tstamp < $now){
 } else {
     $diff_secs = 0;
 }
-vlog "$diff_secs secs since last run\n";
-$url =~ s/^\///;
-$url = "http://$host:$port/$url";
-
-vlog "Host: $host";
-vlog "Port: $port";
-vlog "Full URL: $url\n";
+vlog2 "$diff_secs secs since last run\n";
 
 my $ua = LWP::UserAgent->new;
-$ua->agent("$progname/$main::VERSION ");
+$ua->agent("Hari Sekhon $progname $main::VERSION");
 my $req = HTTP::Request->new(GET => $url);
 
 #my $time = strftime("%F %T", localtime);
-vlog "sending request";
+vlog2 "sending request";
 my $res  = $ua->request($req);
-vlog "got response";
-my $status  = my $status_line  = $res->status_line;
-$status  =~ s/\s.*$//;
-if($status !~ /^\d+$/){
-    quit "UNKNOWN", "CODE ERROR - status code '$status' is not a number (status line was: '$status_line')";
-}
-vlog "status line: $status_line";
+vlog2 "got response";
+my $status_line  = $res->status_line;
+vlog2 "status line: $status_line";
 my $content = my $content_single_line = $res->content;
-vlog "\ncontent:\n\n$content\n";
+vlog3 "\ncontent:\n\n$content\n";
 $content_single_line =~ s/\n/ /g;
 sub not_found {
     quit "CRITICAL", "cannot find '@_' in output => '$content_single_line' from '$url'";
 }
 
-unless($status eq 200){
+unless($res->code eq 200){
     quit "CRITICAL", "'$status_line'";
 }
 if($content =~ /^\s*$/){
@@ -231,12 +156,12 @@ my $accepted = $1;
 my $handled  = $2;
 my $requests = $3;
 # Sanity checks
-vlog "performing sanity check on returned stats: Accepted >= Handled";
+vlog2 "\nperforming sanity check on returned stats: Accepted >= Handled\n";
 unless($accepted >= $handled){
     quit "CRITICAL", "handled connection count > accepted connection count, sanity check failed on nginx stats returned by server!";
 }
 if($no_keepalives){
-    vlog "performing extra sanity check on returned stats: Handled >= Requests";
+    vlog2 "performing extra sanity check on returned stats: Handled >= Requests\n";
     unless($handled >= $requests){
         quit "CRITICAL", "request count > handled connection count, sanity check failed on nginx stats returned by server!";
     }
@@ -263,23 +188,16 @@ if($diff_secs > 0){
     seek($statefh, 0, 0)  or quit "UNKNOWN", "Error: seek failed on '$statefile': $!";
     truncate($statefh, 0) or quit "UNKNOWN", "Error: failed to truncate '$statefile': $!";
 } else {
-    vlog "not updating state file since 0 secs since last run\n";
+    vlog2 "not updating state file since 0 secs since last run\n";
 }
 
 print $statefh "$now $active $reading $writing $waiting $accepted $handled $requests" if ($diff_secs > 0 or $state_file_empty or not $state_file_existed);
 
-my $msg = my $msg2 = "'Active connections' = $active;$warning;$critical, 'Connections / sec' = $conns_sec, 'Requests / sec' = $requests_sec, 'Reading' = $reading, 'Writing' = $writing, 'Accepted' = $accepted, 'Handled' = $handled, 'Requests' = $requests";
-$msg =~ s/'//g;
-$msg =~ s/;(\d+);(\d+)/ (w=$1\/c=$2)/g;
-$msg2 =~ s/,//g;
-$msg2 =~ s/\s*=\s*/=/g;
-$msg .= "| $msg2";
-
 $status = "OK";
-if($active >= $critical){
-    $status = "CRITICAL";
-} elsif($active >= $warning) {
-    $status = "WARNING";
-}
 
-quit($status, $msg);
+$msg = "Active connections = $active";
+check_thresholds($active);
+$msg .= ", Connections / sec = $conns_sec, Requests / sec = $requests_sec, Reading = $reading, Writing = $writing, Accepted = $accepted, Handled = $handled, Requests = $requests";
+$msg .= " | 'Active connections'=$active;" . (defined($thresholds{warning}{upper}) ? $thresholds{warning}{upper} : "") . ";" . (defined($thresholds{critical}{upper}) ? $thresholds{critical}{upper} : "") . ";0; 'Connections / sec'=$conns_sec 'Requests / sec'=$requests_sec 'Reading'=$reading 'Writing'=$writing 'Accepted'=$accepted 'Handled'=$handled 'Requests'=$requests";
+
+quit $status, $msg;
