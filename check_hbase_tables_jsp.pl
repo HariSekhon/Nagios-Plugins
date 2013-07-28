812c869a64421de2c0fa1623964776e9b418a877
Hari Sekhon <harisekhon@gmail.com>
2013-07-28 20:18:20 +0100

    added check_hbase_tables_jsp.pl

---
+++ check_hbase_tables_jsp.pl
@@ -0,0 +1,146 @@
+#!/usr/bin/perl -T
+# nagios: -epn
+#
+#  Author: Hari Sekhon
+#  Date: 2013-07-28 17:08:27 +0100 (Sun, 28 Jul 2013)
+#
+#  http://github.com/harisekhon
+#
+#  License: see accompanying LICENSE file
+#  
+
+$DESCRIPTION = "Nagios Plugin to check given HBase table(s) via the JSP interface on the HBase Master
+
+Checks:
+
+1. Table exists
+2. Table is enabled and available (has at least one region listed, disabled tables have no regions listed)
+
+Strongly recommended to use check_hbase_table.pl instead which uses the HBase Thrift API, it's must tighter programmatically, this is only doing a basic scrape of the HBase Master JSP which could break across releases. The only advantage this program has is that it doesn't require having an HBase Thrift Server
+
+Written and tested on CDH 4.3 (HBase 0.94.6-cdh4.3.0)";
+
+$VERSION = "0.1";
+
+use strict;
+use warnings;
+BEGIN {
+    use File::Basename;
+    use lib dirname(__FILE__) . "/lib";
+}
+use HariSekhonUtils;
+# Mojo::DOM causes this error on Mac OSX 10.8: Your vendor has not defined Time::HiRes macro CLOCK_MONOTONIC, used at (eval 11) line 1.
+#use Mojo::DOM;
+#use HTML::TreeBuilder;
+use LWP::UserAgent;
+
+my $default_port = 60010;
+$port = $default_port;
+
+my $tables;
+
+%options = (
+    "H|host=s"         => [ \$host,     "HBase Master to connect to" ],
+    "P|port=s"         => [ \$port,     "HBase Master JSP Port to connect to (defaults to $default_port)" ],
+    "T|tables=s"       => [ \$tables,   "Table(s) to check. This should be a list of user tables, not -ROOT- or .META. catalog tables which are checked additionally. If no tables are given then only -ROOT- and .META. are checked" ],
+);
+
+@usage_order = qw/host port tables/;
+get_options();
+
+$host       = validate_hostname($host);
+$port       = validate_port($port);
+my @tables = qw/-ROOT- .META./;
+push(@tables, split(/\s*,\s*/, $tables)) if defined($tables);
+@tables or usage "no valid tables specified";
+@tables = uniq_array @tables;
+my $table;
+foreach $table (@tables){
+    if($table =~ /^(-ROOT-|\.META\.)$/){
+    } else {
+        $table = isDatabaseTableName($table) || usage "invalid table name $table given";
+    }
+}
+
+vlog_options "tables", "[ " . join(" , ", @tables) . " ]";
+vlog2;
+set_timeout();
+
+my $ua = LWP::UserAgent->new;
+$ua->agent("Hari Sekhon $progname $main::VERSION");
+$ua->show_progress(1) if $debug;
+
+$status = "OK";
+
+my $url;
+my $html_tree;
+
+my @tables_ok;
+my @tables_not_found;
+my @tables_not_available;
+
+foreach $table (@tables){
+    $url = "http://$host:$port/table.jsp?name=$table";
+    vlog2 "querying HBase Master for table $table";
+    vlog3 "url: $url";
+    my $res = $ua->get($url);
+    vlog2 "got response";
+    my $status_line  = $res->status_line;
+    vlog2 "status line: $status_line";
+    my $content = my $content_single_line = $res->content;
+    vlog3 "\ncontent:\n\n$content\n";
+    $content_single_line =~ s/\n/ /g;
+    vlog2;
+
+    unless($res->code eq 200){
+        if($res->code eq 500 and $content =~ /TableNotFoundException/){
+            critical;
+            vlog2 "table not found: $table\n";
+            push(@tables_not_found, $table);
+            next;
+        } else {
+            quit "CRITICAL", "'$status_line'";
+        }
+    }
+    if($content =~ /\A\s*\Z/){
+        quit "CRITICAL", "empty body returned from '$url'";
+    }
+
+    #$dom = Mojo::DOM->new($content);
+    #$html_tree = HTML::TreeBuilder->new_from_content($content);
+    # Check we have a regionserver listed for the table, this only happens when the table exists and is available and has a region assigned to a regionserver
+    if($content =~ /<tr>            [\r\n\s]*
+                        <td>
+                            $table(?:,[^,]*,[\w\.]+)?
+                        <\/td>      [\r\n\s]*
+                        <td>        [\r\n\s]*
+                            <a\s+href="[^"]+">[^<]+<\/a>    [\r\n\s]*
+                        <\/td>/xim){
+        vlog2 "found table '$table' and available regions";
+        push(@tables_ok, $table);
+    } else {
+        critical;
+        vlog2 "table '$table' exists but no available regions";
+        push(@tables_not_available, $table);
+    }
+    vlog2;
+}
+
+$msg = "HBase ";
+
+sub print_tables($@){
+    my $str = shift;
+    my @arr = @_;
+    if(@arr){
+        @arr = uniq_array @arr;
+        plural scalar @arr;
+        $msg .= "table$plural $str: " . join(" , ", @arr) . " -- ";
+    }
+}
+
+print_tables("not found",     @tables_not_found);
+print_tables("not available", @tables_not_available);
+print_tables("ok",            @tables_ok);
+$msg =~ s/ -- $//;
+
+quit $status, $msg;
