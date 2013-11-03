86f8bb5e0c290a30e877cd6e44b5a7309fdee054
Hari Sekhon <harisekhon@gmail.com>
2013-11-03 17:06:00 +0000

    update description of all the checks this code performs

--- check_zookeeper_znode.pl
+++ check_zookeeper_znode.pl
@@ -15,14 +15,16 @@ Useful for a wide variety of checks against ZooKeeper based services such as HBa
 
 Checks:
 
-1. root znode (\"/\") exists ( we are successfully connected to ZooKeeper(s) )
+1. root znode (\"/\") exists ( we are successfully connected to ZooKeeper(s), retries all ZooKeepers )
 2. given znode exists (useful for checking ephemeral znodes eg. HBase Master is reporting alive by holding ephemeral node in ZooKeeper)
 3. given znode's literal contents, substring match (eg. server we expect is the Master. optional)
 4. given znode's contents against regex (eg. one of the servers we expect is the Master, flexible, anchoring etc. optional)
 5. given znode is not blank/empty (unless -d \"\" is intentionally specified)
-6. given znode's age against --warning/--critical thresholds (optional)
+6. given znode is ephemeral (optiona)
+7. given znode has children / no children znodes (optional. Useful when the child znodes are dynamic but you just need to check for their existence)
+8. given znode's age against --warning/--critical thresholds (optional)
 
-Checks 3-6 are skipped when specifying --null znodes
+Checks 3-5 and 8 are skipped when specifying --null znodes
 
 ================================================================================
                             Some useful examples:
