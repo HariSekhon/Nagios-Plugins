9c3c7a6cd7584d4d94d1f7079d92d5d8fcea9a9f
Hari Sekhon <harisekhon@gmail.com>
2013-11-23 23:16:46 +0000

    tightened database validation to the default database set

--- check_redis_write.pl
+++ check_redis_write.pl
@@ -93,7 +93,7 @@ if($progname eq "check_redis_write_replication.pl"){
     ($host eq $slave) and usage "cannot specify same master and slave";
 }
 if(defined($database)){
-    $database = validate_int($database, 0, 10000, "database");
+    $database = validate_int($database, 0, 15, "database");
 }
 $precision = validate_int($precision, 1, 20, "precision");
 validate_thresholds(undef, undef, { "simple" => "upper", "positive" => 1, "integer" => 0 } );
