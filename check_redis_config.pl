ccce9ee0e6b1ec0ab32e896f8ab86b8190ac67e8
Hari Sekhon <harisekhon@gmail.com>
2013-11-24 11:10:57 +0000

    added detection to config command disabling in config file

--- check_redis_config.pl
+++ check_redis_config.pl
@@ -119,6 +119,7 @@ while(<$fh>){
         if(scalar @tmp == 2){
             $config_cmd = $tmp[1];
             $config_cmd =~ s/["'`]//g;
+            quit "UNKNOWN", "config command was disabled in config file '$conf' using rename-command" unless $config_cmd;
         }
     }
     if($value =~ /^(\d+(?:\.\d+)?)([KMGTP]B)$/i){
