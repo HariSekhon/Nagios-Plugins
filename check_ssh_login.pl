a681bc6ece732819f37d9117cbdaaaec83dee61c
Hari Sekhon <harisekhon@gmail.com>
2013-12-02 16:26:11 +0000

    added support for HP iLO

--- check_ssh_login.pl
+++ check_ssh_login.pl
@@ -9,7 +9,13 @@
 #  License: see accompanying LICENSE file
 #
 
-$DESCRIPTION = "Nagios Plugin to test SSH login credentials. Originally written to verify the login credentials across Dell DRAC infrastructure";
+$DESCRIPTION = "Nagios Plugin to test SSH login credentials
+
+Originally written to verify the login credentials were set properly across Dell DRAC infrastructure
+
+Updated for HP iLO controllers for the same reason, people tend to forget and leave them with default credentials!
+
+Since Dracs and iLOs are very slow, you will need to increase the --timeout for those to something like 50-60 seconds to allow 10-12 secs each password prompt / response as it auto-calculates it to be 1/5th of global --timeout";
 
 $VERSION = "0.9.6";
 
@@ -22,6 +28,12 @@ BEGIN {
     use lib dirname(__FILE__) . "/lib";
 }
 use HariSekhonUtils;
+
+# regex to expect for successful logins
+# no space at end, disparate ssh login prompts don't always have \s$
+# these are non-greedy matches, the .* doesn't do anything
+my $shell_prompt = '.*[>$#]';
+my $login_prompt = '.*[:\?]';
 
 $port        = 22;
 $timeout_max = 130;
@@ -86,35 +98,35 @@ $SIG{__DIE__} = sub {
 #
 $ssh->run_ssh() or quit "CRITICAL", "SSH login failed with user '$user'";
 vlog2 "ssh forked\n";
-my $shell_prompt = '.*?[>$#]'; # no space at end, disparate ssh login prompts don't always have \s$
-my $login_prompt = '.*?:'; # no space at end, disparate ssh login prompts don't always have \s$
-$ssh->waitfor('.*(?::|\?|\n)\s*', $login_timeout, "-re");
+# doesn't prioritize for \n, nor does it capture .* after :
+#$ssh->waitfor('(?:.+\n|[^\n]+\?|[^\n\?]+:).*', $login_timeout, "-re");
+$ssh->waitfor($login_prompt, $login_timeout, "-re");
 my $result = $ssh->match();
-$result =~ s/^\r?\n?$//o if $result;
+$result =~ s/^\r?\n?$// if $result;
 quit "CRITICAL", "timeout after $login_timeout secs waiting for initial prompt" unless $result;
-$result =~ /Name or service not known/io and quit "CRITICAL", "DNS lookup failure on '$host'";
-$result =~ /Connection refused/io and quit "CRITICAL", "Connection refused to '$host:$port'";
+$result =~ /Name or service not known/i and quit "CRITICAL", "DNS lookup failure on '$host'";
+$result =~ /Connection refused/i and quit "CRITICAL", "Connection refused to '$host:$port'";
 vlog2 "\n\nchecking for host key prompt";
 while($result =~ /The authenticity of host .+ can't be established|key fingerprint is/){
-    $ssh->waitfor('.*(?::|\?|\n)\s*', $login_timeout, "-re");
+    $ssh->waitfor($login_prompt, $login_timeout, "-re");
     $result = $ssh->match();
-    $result =~ s/^\r?\n?$//o if $result;
+    $result =~ s/^\r?\n?$// if $result;
 }
 quit "CRITICAL", "timeout after $login_timeout secs waiting for password/host key prompt" unless $result;
-if($result and $result =~ /Are you sure you want to continue connecting \(yes\/no\)\?/smio){
+if($result and $result =~ /Are you sure you want to continue connecting \(yes\/no\)\?/smi){
     vlog2 "host key prompt detected, sending 'yes'";
     $ssh->send("yes");
     vlog2 "\nattempting to read prompt again (timeout: $login_timeout seconds)\n";
     #$result = $ssh->read_all($login_timeout);
     $ssh->waitfor('(?:[Pp]assword|[Pp]assphrase).*:\s', $login_timeout, "-re");
     $result = $ssh->match();
-    $result =~ s/^\r?\n?$//o if $result;
+    $result =~ s/^\r?\n?$// if $result;
     quit "CRITICAL", "password prompt not returned within $login_timeout seconds after accepting ssh host key" unless $result;
 }
 vlog2 "\n\nchecking for password prompt";
 if($result){
-    if($result =~ /(?:password|passphrase).*:/io){
-        vlog2 "caught password prompt, sending password '<omitted>'";
+    if($result =~ /(?:password|passphrase).*:/i){
+        vlog2 "caught password prompt, sending password";
         $ssh->send($password);
         vlog2 "\nreading password response (timeout: $login_timeout seconds)\n";
         $ssh->waitfor($login_prompt, $login_timeout, "-re");
@@ -129,24 +141,44 @@ if($result){
 $result =~ s/\s*$//;
 
 #vlog3 "result:\n\n$result";
-$result =~ s/^\s*\n//mo;
-if($result =~ /^Last\s+login.+?$/io){
+$result =~ s/^\s*\n//m;
+if($result =~ /^Last\s+login.+?$/i){
     $ssh->waitfor($shell_prompt, $login_timeout, "-re");
     $result = $ssh->match();
     defined($result) or quit "UNKNOWN", "shell prompt not received within $login_timeout secs after Last login header";
 }
-if($result =~ /(?:Mon|Tue|Wed|Thu|Fri|Sat|Sun).*$/io){
+if($result =~ /(?:Mon|Tue|Wed|Thu|Fri|Sat|Sun).*$/i){
     $ssh->waitfor($shell_prompt, $login_timeout, "-re");
     $result = $ssh->match();
     defined($result) or quit "UNKNOWN", "shell prompt not received with $login_timeout secs after Last login date header";
 }
+
+if($result =~ /logged[\s-]in/i){
+    $ssh->waitfor($shell_prompt, $login_timeout, "-re");
+    $result = $ssh->match();
+    defined($result) or quit "UNKNOWN", "shell prompt not received within $login_timeout secs after logged in header";
+}
+
+# added for HP iLO
+if($result =~ /^User:/){
+    $ssh->waitfor($shell_prompt);
+    $result = $ssh->match();
+    defined($result) or quit "UNKNOWN", "shell prompt not received with $login_timeout secs after User: response";
+    if($result =~ /$user logged-in to/i){
+        vlog2 "HP user logged-in header detected";
+        $ssh->waitfor($shell_prompt, $login_timeout, "-re");
+        $result = $ssh->match();
+        defined($result) or quit "UNKNOWN", "shell prompt not received within $login_timeout secs after 'User:' response";
+    }
+}
+
 #vlog3 "\n\nprompt: '$result'";
 
 vlog2 "\n";
-if($result =~ /(?:password|passphrase).*:/io){
+if($result =~ /(?:password|passphrase).*:/i){
     vlog2 "password prompt has been re-displayed, username/password combination has failed";
     quit "CRITICAL", "SSH login failed with user '$user'";
-} elsif($result =~ /$shell_prompt/o){
+} elsif($result =~ /$shell_prompt/){
     quit "OK", "SSH login successful with user '$user' (prompt returned: '$result')";
 } else {
     quit "CRITICAL", "SSH login failed with user '$user' (unconfirmed prompt: '$result')";
