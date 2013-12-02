4c21e331d4ee4c1e52d2c21a8e6c93f850ed197a
Hari Sekhon <harisekhon@gmail.com>
2013-12-02 16:37:06 +0000

    updated check_datameer_version.pl check_datameer_license.pl check_datameer_active_jobs.pl

--- check_datameer_version.pl
+++ check_datameer_version.pl
@@ -13,7 +13,7 @@
 
 $DESCRIPTION = "Nagios Plugin to check the Datameer version using the Datameer Rest API
 
-Tested against Datameer 2.1.x.x";
+Tested against Datameer 2.1.4.6 and 3.0.11";
 
 $VERSION = "0.1";
 
