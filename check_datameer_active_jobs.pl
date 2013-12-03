1a566ffe46cf264dcc0864be9a13f946ed207d1f
Hari Sekhon <harisekhon@gmail.com>
2013-12-03 12:17:34 +0000

    updated check_datameer_active_jobs.pl

--- check_datameer_active_jobs.pl
+++ check_datameer_active_jobs.pl
@@ -15,7 +15,7 @@ $DESCRIPTION = "Nagios Plugin to check the number of active Datameer jobs using 
 
 Tested against Datameer 2.1.4.6 and 3.0.11";
 
-$VERSION = "0.1";
+$VERSION = "0.2";
 
 use strict;
 use warnings;
@@ -25,7 +25,9 @@ BEGIN {
 }
 use HariSekhonUtils;
 use JSON::XS;
-use LWP::UserAgent;
+use LWP::Simple '$ua';
+
+$ua->agent("Hari Sekhon $progname $main::VERSION");
 
 my $default_port = 8080;
 $port = $default_port;
@@ -58,25 +60,7 @@ set_timeout();
 
 $status = "OK";
 
-my $ua = LWP::UserAgent->new;
-$ua->agent("Hari Sekhon $progname $main::VERSION");
-$ua->credentials($host, '', $user, $password);
-
-# Lifted from check_cloudera_manager_metrics.pl TODO: move to lib
-#my $content = get $url;
-vlog2 "querying $url";
-my $req = HTTP::Request->new('GET',$url);
-$req->authorization_basic($user, $password);
-my $response = $ua->request($req);
-my $content  = $response->content;
-chomp $content;
-vlog3 "returned HTML:\n\n" . ( $content ? $content : "<blank>" ) . "\n";
-vlog2 "http code: " . $response->code;
-vlog2 "message: " . $response->message;
-
-unless($response->code eq "200"){
-    quit "UNKNOWN", $response->code . " " . $response->message;
-}
+my $content = curl $url, $user, $password;
 
 my $json;
 try{
