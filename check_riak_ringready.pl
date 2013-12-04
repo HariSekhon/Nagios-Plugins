56597d3d67126a07f7ac3e5635a7fa153bc852dc
Hari Sekhon <harisekhon@gmail.com>
2013-12-04 19:03:56 +0000

    updated check_riak_ringready.pl

--- check_riak_ringready.pl
+++ check_riak_ringready.pl
@@ -31,7 +31,7 @@ $ENV{"PATH"} .= ":/usr/sbin";
 my $path = "";
 
 %options = (
-    "riak-admin-path=s"  => [ \$path, "Path to riak-admin command if differing from the default /usr/sbin" ],
+    "riak-admin-path=s"  => [ \$path, "Path to directory containing riak-admin command if differing from the default /usr/sbin" ],
 );
 
 get_options();
