d4bb9bee273989a9eb547896971645f20bde6dc0
Hari Sekhon <harisekhon@gmail.com>
2013-12-06 15:45:54 +0000

    rounded bytes to make more sense, capped heap % to 2 decimal places

--- check_hadoop_namenode.pl
+++ check_hadoop_namenode.pl
@@ -455,18 +455,18 @@ if($balance){
     if($content =~ /Heap\s+Memory\s+used\s+(\d+(?:\.\d+)?)\s+(\w+)\s+is\s+(\d+(?:\.\d+)?)%\s+of\s+Commited\s+Heap\s+Memory\s+(\d+(?:\.\d+)?)\s+(\w+)\.\s+Max\s+Heap\s+Memory\s+is\s+(\d+(?:\.\d+)?)\s+(\w+)/){
         $stats{"heap_used"}                 = $1;
         $stats{"heap_used_units"}           = $2;
-        $stats{"heap_used_of_committed_pc"} = $3; # % used of comitted
+        $stats{"heap_used_of_committed_pc"} = $3; # % used of committed, not % of total which is what we're checking
         $stats{"heap_committed"}            = $4;
         $stats{"heap_committed_units"}      = $5;
         $stats{"heap_max"}                  = $6;
         $stats{"heap_max_units"}            = $7;
-        $stats{"heap_used_bytes"}           = expand_units($stats{"heap_used"}, $stats{"heap_used_units"}, "Heap Used");
-        $stats{"heap_committed_bytes"}      = expand_units($stats{"heap_committed"}, $stats{"heap_committed_units"}, "Heap committed");
-        $stats{"heap_max_bytes"}            = expand_units($stats{"heap_max"},  $stats{"heap_max_units"},  "Heap Max" );
+        $stats{"heap_used_bytes"}           = int(expand_units($stats{"heap_used"}, $stats{"heap_used_units"}, "Heap Used"));
+        $stats{"heap_committed_bytes"}      = int(expand_units($stats{"heap_committed"}, $stats{"heap_committed_units"}, "Heap committed"));
+        $stats{"heap_max_bytes"}            = int(expand_units($stats{"heap_max"},  $stats{"heap_max_units"},  "Heap Max" ));
         vlog3 sprintf("heap used        %s %s => %s", $stats{"heap_used"}, $stats{"heap_used_units"}, $stats{"heap_used_bytes"});
         vlog3 sprintf("heap committed   %s %s => %s", $stats{"heap_committed"}, $stats{"heap_committed_units"}, $stats{"heap_committed_bytes"});
         vlog3 sprintf("heap max         %s %s => %s", $stats{"heap_max"}, $stats{"heap_max_units"}, $stats{"heap_max_bytes"});
-        $stats{"heap_used_pc_calculated"} =  $stats{"heap_used_bytes"} / $stats{"heap_max_bytes"} * 100;
+        $stats{"heap_used_pc_calculated"} =  sprintf("%.2f", $stats{"heap_used_bytes"} / $stats{"heap_max_bytes"} * 100);
         vlog3 sprintf("heap used calculated = %.2f%% (%s / %s)\n", $stats{heap_used_pc_calculated}, $stats{heap_used_bytes}, $stats{heap_max_bytes});
         # we get given the % of comitted not the % of total heap, so this is not comparable for 
         #if(abs(int($stats{"heap_used_pc_calculated"}) - $stats{"heap_used_of_comitted_pc"}) > 2){
@@ -478,7 +478,7 @@ if($balance){
     $status = "OK";
     $msg    = sprintf("Namenode Heap %.2f%% Used (%s %s used, %s %s committed, %s %s total)", $stats{"heap_used_pc_calculated"}, $stats{"heap_used"}, $stats{"heap_used_units"}, $stats{"heap_committed"}, $stats{"heap_committed_units"}, $stats{"heap_max"}, $stats{"heap_max_units"});
     check_thresholds($stats{"heap_used_pc_calculated"});
-    $msg .= " | 'Namenode Heap % Used'=$stats{heap_used_pc_calculated}%;" . ($thresholds{warning}{upper} ? $thresholds{warning}{upper} : "" ) . ";" . ($thresholds{critical}{upper} ? $thresholds{critical}{upper} : "" ) . ";0;100 'Namenode Heap Used'=$stats{heap_used_bytes}B 'NameNode Heap Committed'=$stats{heap_committed_bytes}B";
+    $msg .= " | 'Namenode Heap % Used'=$stats{heap_used_pc_calculated}%" . msg_perf_thresholds(1) . "0;100 'Namenode Heap Used'=$stats{heap_used_bytes}B 'NameNode Heap Committed'=$stats{heap_committed_bytes}B";
 ###############
 } elsif($datanode_blocks){
     $content = curl $url_live_nodes, $url_name;
