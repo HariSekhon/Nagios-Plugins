2f603d8872c2fb3f33e4fb489c0f614b7173f3c1
Hari Sekhon <harisekhon@gmail.com>
2013-12-07 14:29:33 +0000

    updated check_datameer_job_details.pl

--- check_datameer_job_details.pl
+++ check_datameer_job_details.pl
@@ -21,6 +21,8 @@ import_output_bytes
 import_dropped_splits
 import_output_partitions
 
+To find the JOB ID that you should supply to the --job-id option you should look at the Browser tab inside Datameer's web UI and right-click on the import job and then click Information. This will show you the JOB ID in the field \"ID: <number>\" (NOT \"File ID\")
+
 Tested against Datameer 3.0.11";
 
 $VERSION = "0.2";
@@ -52,26 +54,37 @@ get_options();
 ($host, $port, $user, $password) = validate_host_port_user_password($host, $port, $user, $password);
 $job_id = validate_int($job_id, "job-id", 1, 100000);
 
-my $url = "http://$host:$port/rest/job-execution/job-details/$job_id";
+my $url = "http://$host:$port/rest/job-configuration/job-history/$job_id?start=0&length=1";
 
 vlog2;
 set_timeout();
-set_http_timeout($timeout - 1);
+set_http_timeout(($timeout - 1) / 2);
 
 my $json = datameer_curl $url, $user, $password;
 
+quit "UNKNOWN", "no job runs have occurred yet for job $job_id or no job history available for that job" unless @{$json};
+
+my $job_run = @{$json}[0];
+
+defined($job_run->{"id"}) or quit "UNKNOWN", "job run id not returned by Datameer server for latest run of job $job_id";
+isInt($job_run->{"id"}) or quit "UNKNOWN", "non-integer returned for latest job run id by Datameer server (got '$$job_run->{id}')";
+
+$url = "http://$host:$port/rest/job-execution/job-details/$job_run->{id}";
+
+$job_run = datameer_curl $url, $user, $password;
+
 foreach(qw/jobStatus failureCount successCount counters startTime stopTime/){
-    defined($json->{$_}) or quit "UNKNOWN", "job $job_id '$_' field not returned by Datameer server";
+    defined($job_run->{$_}) or quit "UNKNOWN", "job $job_id '$_' field not returned by Datameer server";
 }
 foreach(qw/failureCount successCount/){
-    isInt($json->{$_})   or quit "UNKNOWN", "job $job_id '$_' returned non-integer '$json->{$_}', investigation required";
+    isInt($job_run->{$_})   or quit "UNKNOWN", "job $job_id '$_' returned non-integer '$job_run->{$_}', investigation required";
 }
 foreach(qw/IMPORT_RECORDS IMPORT_BYTES IMPORT_DROPPED_RECORDS IMPORT_PREVIEW_RECORDS IMPORT_OUTPUT_BYTES IMPORT_DROPPED_SPLITS IMPORT_OUTPUT_PARTITIONS/){
-    defined($json->{"counters"}->{$_}) or quit "UNKNOWN", "job $job_id counter '$_' not returned by Datameer server";
-    isInt(  $json->{"counters"}->{$_}) or quit "UNKNOWN", "job $job_id counter '$_' returned non-integer '$json->{counters}->{$_}', investigation required";
+    defined($job_run->{"counters"}->{$_}) or $job_run->{"counters"}->{$_} = 0;
+    isInt(  $job_run->{"counters"}->{$_}) or quit "UNKNOWN", "job $job_id counter '$_' returned non-integer '$job_run->{counters}->{$_}', investigation required";
 }
 
-my $job_status = $json->{"jobStatus"};
+my $job_status = $job_run->{"jobStatus"};
 
 $status = "UNKNOWN";
 foreach my $state (qw/CRITICAL WARNING OK/){
@@ -81,9 +94,9 @@ foreach my $state (qw/CRITICAL WARNING OK/){
     }
 }
 
-$msg = sprintf("job %d state '%s', failureCount %s, successCount %s, start time '%s', stop time '%s' |", $job_id, lc $job_status, $json->{"failureCount"}, $json->{"successCount"}, $json->{"startTime"}, $json->{"stopTime"});
+$msg = sprintf("job %d state '%s', failureCount %s, successCount %s, last start time '%s', stop time '%s' |", $job_id, lc $job_status, $job_run->{"failureCount"}, $job_run->{"successCount"}, $job_run->{"startTime"}, $job_run->{"stopTime"});
 foreach(qw/IMPORT_RECORDS IMPORT_BYTES IMPORT_DROPPED_RECORDS IMPORT_PREVIEW_RECORDS IMPORT_OUTPUT_BYTES IMPORT_DROPPED_SPLITS IMPORT_OUTPUT_PARTITIONS/){
-    $msg .= sprintf(" %s=%d", lc $_, $json->{"counters"}->{$_});
+    $msg .= sprintf(" %s=%d", lc $_, $job_run->{"counters"}->{$_});
 }
 
 quit $status, $msg;
