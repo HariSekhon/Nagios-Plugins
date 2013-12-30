e1279a06271c3fb7652381ab50ac3aafd2b51a48
Hari Sekhon <harisekhon@gmail.com>
2013-12-30 17:27:41 +0000

    added couchbase mode and couchbase replicated write mode

--- check_memcached_write.pl
+++ check_memcached_write.pl
@@ -9,17 +9,18 @@
 #  License: see accompanying LICENSE file
 #
 
-our $DESCRIPTION = "Nagios Plugin to check a Memcached server via API read/write
+our $DESCRIPTION = "Nagios Plugin to check a Memcached server via API write => read
 
 Checks:
 
 1. writes a unique ephemeral key with dynamically generated value
 2. reads back same unique key
 3. checks the returned value is identical to that written
-4. records the read/write/delete timings and total time (including tcp connection and close) to a given precision
-5. compares timing of each read/write/delete operation against warning/critical thresholds if given";
+4. deletes the unique generated key, checks deleted occurred successfully
+5. records the read/write/delete timings and total time (including tcp connection and close) to a given precision
+6. compares timing of each read/write/delete operation against warning/critical thresholds if given";
 
-$VERSION = "0.9.2";
+$VERSION = "0.10.0";
 
 use strict;
 use warnings;
@@ -30,7 +31,7 @@ BEGIN {
 use HariSekhonUtils;
 # Why am I using a socket connection instead of one of the libraries out there? Easy portability. Plus the text protocol isn't hard :)
 use IO::Socket;
-use Time::HiRes 'time';
+use Time::HiRes qw/time sleep/;
 
 set_port_default(11211);
 set_timeout_range(1, 60);
@@ -38,16 +39,40 @@ set_timeout_range(1, 60);
 my $default_precision = 5;
 my $precision = $default_precision;
 
+my $server_name           = "memcached";
+my $couchbase             = 0;
+my $couchbase_replication = 0;
+my $slave;
+my $default_slave_delay   = 0.5;
+my $slave_delay = $default_slave_delay;
+
+if($progname =~ /couchbase/i){
+    $couchbase = 1;
+    $server_name = "couchbase";
+    $DESCRIPTION =~ s/Memcached server via/Couchbase server via Memcached/;
+    env_creds("COUCHBASE");
+} else {
+    env_creds("MEMCACHED");
+}
+
 %options = (
     %hostoptions,
     "w|warning=s"   => [ \$warning,     "Warning  threshold in seconds for each read/write/delete operation (use float for milliseconds)" ],
     "c|critical=s"  => [ \$critical,    "Critical threshold in seconds for each read/write/delete operation (use float for milliseconds)" ],
     "precision=i"   => [ \$precision,   "Number of decimal places for timings (default: $default_precision)" ],
 );
+
 @usage_order = qw/host port warning critical precision/;
 
-if($progname =~ /couchbase/i){
-    $DESCRIPTION =~ s/Memcached server via/Couchbase server via Memcached/;
+if($progname eq "check_couchbase_write_replication.pl"){
+    $couchbase_replication = 1;
+    $DESCRIPTION =~ s/^2\..*$/2. reads back the same unique key from the given replication slave after a configurable delay in secs (to allow for the write to replicate to the slave)/m;
+    %options = (
+        %options,
+        "S|slave=s"       => [ \$slave,         "Couchbase slave host to read replicated write from to test replication" ],
+        "slave-delay=s"   => [ \$slave_delay,   "Delay in secs before reading the key-value pair back from the Couchbase replication slave cluster (default: $default_slave_delay)" ],
+    );
+    @usage_order = qw/host port slave slave-delay warning critical precision/;
 }
 
 get_options();
@@ -55,6 +80,13 @@ get_options();
 $host = validate_host($host);
 $port = validate_port($port);
 $precision = validate_int($precision, "precision", 1, 20);
+if($couchbase and $couchbase_replication){
+    $slave = validate_host($slave, "slave");
+    $slave_delay = validate_float($slave_delay, "slave delay", 0.02, 60); # In testing in a local Couchbase cluster 0.02 secs is the minimum time required for the replicated write check to work
+    if($slave_delay > $timeout - 1){
+        usage "slave delay cannot be set to more than (timeout - 1) secs, timeout is currently set to $timeout secs. Either increase --timeout or reduce --slave-delay";
+    }
+}
 validate_thresholds(undef, undef, { "simple" => "upper", "positive" => 1, "integer" => 0 } );
 vlog2;
 
@@ -76,28 +108,34 @@ vlog2 "setting socket timeout to $socket_timeout secs as 1/4 of timeout since th
 
 $status = "OK";
 
-vlog2 "connecting to $host:$port";
 my $start_time = time;
-my $ip = validate_resolvable($host);
-my $conn = IO::Socket::INET->new (
-                                    Proto    => "tcp",
-                                    PeerAddr => $ip,
-                                    PeerPort => $port,
-                                    Timeout  => $socket_timeout,
-                                 ) or quit "CRITICAL", "Failed to connect to '$host:$port': $!";
-my $connect_time = sprintf("%0.${precision}f", time - $start_time);
-vlog2 "OK connected in $connect_time secs\n";
-$conn->autoflush(1) or quit "UNKNOWN", "failed to set autoflush on socket: $!";
-vlog3 "set autoflush on";
+
+sub memcached_connect($){
+    my $host = shift;
+    vlog2 "connecting to $host:$port";
+    my $ip = validate_resolvable($host);
+    my $conn = IO::Socket::INET->new (
+                                        Proto    => "tcp",
+                                        PeerAddr => $ip,
+                                        PeerPort => $port,
+                                        Timeout  => $socket_timeout,
+                                     ) or quit "CRITICAL", "Failed to connect to '$host:$port': $!";
+    my $connect_time = sprintf("%0.${precision}f", time - $start_time);
+    vlog2 "OK connected in $connect_time secs\n";
+    $conn->autoflush(1) or quit "UNKNOWN", "failed to set autoflush on socket: $!";
+    vlog3 "set autoflush on\n";
+    return $conn;
+}
+my $conn        = memcached_connect($host);
+my $slave_conn  = memcached_connect($slave) if ($couchbase and $slave);
 
 # using add instead of set here to intentionally fail if the key is already present which it shouldn't be
 my $memcached_write_cmd  = "add $key $flags $timeout $bytes\r\n$value\r\n";
 my $memcached_read_cmd   = "get $key\r\n"; 
 my $memcached_delete_cmd = "delete $key\r\n";
-vlog3;
 vlog3 "sending write request: $memcached_write_cmd";
 my $write_start_time = time;
-print $conn $memcached_write_cmd or quit "CRITICAL", "failed to write memcached key/value on '$host:$port': $!";
+print $conn $memcached_write_cmd or quit "CRITICAL", "failed to write $server_name key/value on '$host:$port': $!";
 
 sub check_memcached_response($){
     my $response = shift;
@@ -112,36 +150,46 @@ sub check_memcached_response($){
         } else {
             $err_msg = "unknown error returned from";
         }
-        quit "CRITICAL", "$err_msg memcached '$host:$port': '$response'";
+        quit "CRITICAL", "$err_msg $server_name '$host:$port': '$response'";
     }
 }
 
 while (<$conn>){
     s/\r\n$//;
-    vlog3 "memcached response: $_";
+    vlog3 "$server_name response: $_";
     s/\r$//;
     check_memcached_response($_);
     if(/^STORED$/){
         last;
     } else {
-         quit "CRITICAL", "failed to store key/value to memcached on '$host:$port': $_";
+         quit "CRITICAL", "failed to store key/value to $server_name on '$host:$port': $_";
     }
 }
 my $write_time_taken = sprintf("%0.${precision}f", time - $write_start_time);
 vlog2 "write request completed in $write_time_taken secs\n";
 
-vlog3;
-vlog3 "sending read request: $memcached_read_cmd";
+if($couchbase and $slave and $slave_delay){
+    vlog2 sprintf("sleeping for %.2f secs before reading from couchbase slave", $slave_delay);
+    sleep $slave_delay;
+}
+
+my $read_conn = $conn;
+my $read_host = $host;
+if($couchbase and $slave){
+    $read_conn = $slave_conn;
+    $read_host = $slave;
+}
+vlog3 "sending read request to $read_host:$port: $memcached_read_cmd";
 my $read_start_time = time;
-print $conn $memcached_read_cmd or quit "CRITICAL", "failed to read back key/value from memcached on '$host:$port': $!";
+print $read_conn $memcached_read_cmd or quit "CRITICAL", "failed to read back key/value from $server_name on '$read_host:$port': $!";
 
 my $read_value;
 my $value_seen = 0;
 my $value_regex = "^VALUE $key $flags $bytes\$";
 vlog3 "value regex:       $value_regex\n";
-while(<$conn>){
+while(<$read_conn>){
     s/\r\n$//;
-    vlog3 "memcached response: $_";
+    vlog3 "$server_name response: $_";
     last if /END/;
     if($_ =~ /$value_regex/){
         $value_seen = 1;
@@ -154,7 +202,7 @@ my $read_time_taken  = sprintf("%0.${precision}f", time - $read_start_time);
 vlog2 "read request completed in $read_time_taken secs\n";
 
 unless(defined($read_value)){
-    quit "CRITICAL", "failed to find content back from memcached at '$host:$port', try running with -vv or -vvv to debug";
+    quit "CRITICAL", "failed to read back key from '$read_host:$port'" . ( $couchbase_replication ? sprintf(" (either replication is broken or perhaps key/value didn't replicate within %.2f secs, consider increasing --slave-delay)", $slave_delay) : "");
 }
 
 vlog2 "comparing read back value for key";
@@ -165,23 +213,24 @@ if($value eq $read_value){
 }
 
 my $delete_start_time = time;
-print $conn $memcached_delete_cmd or quit "CRITICAL", "failed to delete memcached key/value on '$host:$port': $!";
+print $conn $memcached_delete_cmd or quit "CRITICAL", "failed to delete $server_name key/value on '$host:$port': $!";
 
 while (<$conn>){
     s/\r\n$//;
-    vlog3 "memcached response: $_";
+    vlog3 "$server_name response: $_";
     s/\r$//;
     check_memcached_response($_);
     if(/^DELETED$/){
         last;
     } else {
-         quit "CRITICAL", "failed to store key/value to memcached on '$host:$port': $_";
+         quit "CRITICAL", "failed to delete key/value to $server_name on '$host:$port': $_";
     }
 }
 my $delete_time_taken = sprintf("%0.${precision}f", time - $delete_start_time);
 vlog2 "delete request completed in $delete_time_taken secs\n";
 
 close $conn;
+close $slave_conn if $slave_conn;
 vlog2 "closed connection\n";
 
 my $time_taken = time - $start_time;
