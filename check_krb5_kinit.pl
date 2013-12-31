a63b8016a879b40a58b1167bc587706cecbb0a0f
Hari Sekhon <harisekhon@gmail.com>
2013-12-31 15:56:31 +0000

    added kdc mode

--- check_krb5_kinit.pl
+++ check_krb5_kinit.pl
@@ -9,19 +9,20 @@
 #  License: see accompanying LICENSE file
 #
 
-$DESCRIPTION = "Nagios Plugin to check Kerberos is working by getting a TGT from the KDC using a keytab
+our $DESCRIPTION = "Nagios Plugin to check Kerberos is working by getting a TGT from a KDC using a keytab
 
 Create a nagios kerberos principal and export a keytab for it to use in this check
 
 Requirements:
 
-1. Kerberos KDC(s)
-2. /etc/krb5.conf
-3. nagios kerberos principal
-4. exported keytab for the nagios kerberos principal
+- Kerberos KDC
+- /etc/krb5.conf
+- nagios kerberos principal
+- exported keytab for the nagios kerberos principal
+- kinit command in standard system path
 ";
 
-$VERSION = "0.2";
+$VERSION = "0.3";
 
 use strict;
 use warnings;
@@ -33,19 +34,51 @@ use HariSekhonUtils qw/:DEFAULT :regex/;
 use File::Temp;
 
 my $default_principal = "nagios";
-my $principal = $default_principal;
+my $principal         = $default_principal;
 my $keytab;
 
-%options = (
-    "p|principal=s"    => [ \$principal,    "Kerberos principal to use (default: $default_principal)" ],
-    "k|keytab=s"       => [ \$keytab,       "Path to Keytab exported containing principal credentials. Required" ],
-);
+my $conf;
+my $kdc;
+my $realm;
+my $renewable;
 
-@usage_order = qw/principal keytab/;
+set_port_default(88);
+
+env_creds("KDC");
+
+%options = (
+    "k|keytab=s"       => [ \$keytab,       "Path to Keytab exported containing principal credentials. Required" ],
+    "p|principal=s"    => [ \$principal,    "Kerberos principal to use (default: $default_principal)" ],
+    "C|conf=s"         => [ \$conf,         "Path to krb5.conf (optional, will usually default to using /etc/krb5.conf if unspecified)" ],
+    "r|renewable"      => [ \$renewable,    "Checks we are able to retrieve a renewable TGT" ],
+);
+@usage_order = qw/host port realm keytab principal conf renewable/;
+
+if($progname eq "check_krb5_kdc.pl"){
+    $kdc = 1;
+    $DESCRIPTION =~ s/check Kerberos is working.*from a KDC/check a specific KDC is working by getting a TGT/;
+    $DESCRIPTION =~ s/- \/etc\/krb5.conf/- Kerberos Realm/;
+    delete $options{"C|conf=s"};
+    %options = (
+        %hostoptions,
+        "R|realm=s" => [ \$realm, "Kerberos Realm" ],
+        %options,
+    );
+}
+
 get_options();
 
-$principal = validate_krb5_princ($principal);
+if($kdc){
+    $host  = validate_host($host);
+    $port  = validate_port($port);
+    $realm = validate_krb5_realm($realm);
+}
 $keytab    = validate_file($keytab, undef, "keytab");
+$principal = validate_krb5_princ($principal);
+if($conf){
+    $conf      = validate_file($conf  , undef, "conf");
+    $conf =~ /(?:.*\/)?krb5.conf$/ or usage "must specify a file called krb5.conf if using --conf";
+}
 validate_thresholds();
 
 vlog2;
@@ -59,9 +92,44 @@ my $fh = File::Temp->new(TEMPLATE => "/tmp/${progname}_krb5cc_$>_XXXXXXXXXX");
 my $ticket_cache = $fh->filename;
 vlog2 "temporary ticket cache is '$ticket_cache'\n";
 
+my $fh2;
+if($host){
+    vlog2 "creating temporary krb5.conf configuration file";
+    $fh2 = File::Temp->new(TEMPLATE => "/tmp/${progname}.krb5.conf.XXXXXXXXXX");
+    my $conf_tmp = $fh2->filename;
+    vlog2 "writing conf '$conf_tmp' for realm '$realm' with kdc '$host:$port'";
+    $| = 1;
+    print $fh2 "
+[libdefaults]
+    default_realm = $realm
+[realms]
+    $realm = {
+        kdc = $host:$port
+    }
+";
+    close $fh2;
+    $conf = $conf_tmp;
+    vlog2;
+    validate_resolvable($host);
+}
+
+if($conf){
+    vlog2 "setting krb5.conf configuration file to '$conf'\n";
+    $ENV{"KRB5_CONFIG"} = $conf;
+}
+
 vlog2 "requesting TGT";
-my @output = cmd("kinit -c '$ticket_cache' -k -t '$keytab' '$principal'");
+my $cmd = "kinit -c '$ticket_cache' -k -t '$keytab' '$principal'";
+if($renewable){
+    $cmd .= " -r 1d";
+}
+my @output = cmd($cmd);
 if(@output){
+    if($output[0] =~ /Keytab contains no suitable keys/){
+        $output[0] .= " (did you generate the keytab correctly and specify the correct principal and realm including correct capitalisation?)";
+    } elsif($output[0] =~ /Cannot contact any KDC/){
+        $output[0] .= " (KDC down or specified incorrect --host/--port?)";
+    }
     quit "CRITICAL", join(" ", @output);
 }
 
@@ -70,14 +138,14 @@ vlog2 "validating TGT\n";
 foreach(@output){
     next if(
     /^Ticket\s+cache:\s+FILE:$ticket_cache\s*$/i or
-    /^Default\s+principal:\s+$principal(?:(?:\/$domain_regex)?\@$domain_regex)?\s*$/i    or
+    /^Default\s+principal:\s+$principal(?:(?:\/$domain_regex)?\@$domain_regex)?\s*$/i or
     /^\s*$/ or
     /^Valid\s+starting\s+Expires\s+Service principal\s*$/i or
     /^\s*renew until/i
     );
     /(\d{2}\/\d{2}\/\d{2}\s+\d{2}:\d{2}:\d{2})\s+(\d{2}\/\d{2}\/\d{2}\s+\d{2}:\d{2}:\d{2})\s+krbtgt\/$domain_regex\@$domain_regex/i or quit "CRITICAL", "unrecognized line: '$_'. $nagios_plugins_support_msg";
     if($1 eq $2){
-        quit "CRITICAL", "TGT start and expiry are the same";
+        quit "CRITICAL", "TGT start and expiry are the same (misconfiguration of the kerberos principal in the KDC?)";
     }
 }
 
