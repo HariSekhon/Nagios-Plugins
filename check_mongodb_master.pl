ac2f03921ce7c8c5fb0975e27d2dc41a7d9f9e3f
Hari Sekhon <harisekhon@gmail.com>
2014-03-16 15:51:58 +0000

    updated check_mongodb_master.pl

--- check_mongodb_master.pl
+++ check_mongodb_master.pl
@@ -23,29 +23,40 @@ use HariSekhonUtils;
 use Data::Dumper;
 use MongoDB::MongoClient;
 
-set_port_default(27017);
-
 env_creds("MongoDB");
+
+my $expected_master;
 
 my $ssl               = 0;
 my $sasl              = 0;
 my $sasl_mechanism    = "GSSAPI";
 
 %options = (
-    %hostoptions,
+    "H|host=s"              => [ \$host,          "MongoDB host(s) to connect to (should be from same replica set), comma separated, with optional :<port> suffixes. Tries hosts in given order from left to right to find Primary for write. Specifying any one host is sufficient as the rest will be auto-determined to find the primary (\$MONGODB_HOST, \$HOST)" ],
     %useroptions,
-    "ssl"              => [ \$ssl,           "Enable SSL, MongDB libraries must have been compiled with SSL and server must support it. Experimental" ],
-    "sasl"             => [ \$sasl,          "Enable SASL authentication, must be compiled in to the MongoDB perl driver to work. Experimental" ],
-    "sasl-mechanism=s" => [ \$sasl_mechanism, "SASL mechanism (default: GSSAPI eg Kerberos on MongoDB Enterprise 2.4+ in which case this should be run from a valid kinit session, alternative PLAIN for LDAP using user/password against MongoDB Enterprise 2.6+ which is sent in plaintext so should be used over SSL). Experimental" ],
+    "e|expected-master=s"   => [ \$expected_master, "Checks the master against a specific regex rather than the specified --host. Required if specifying more than one --host" ],
+    "ssl"                   => [ \$ssl,             "Enable SSL, MongDB libraries must have been compiled with SSL and server must support it. Experimental" ],
+    "sasl"                  => [ \$sasl,            "Enable SASL authentication, must be compiled in to the MongoDB perl driver to work. Experimental" ],
+    "sasl-mechanism=s"      => [ \$sasl_mechanism,  "SASL mechanism (default: GSSAPI eg Kerberos on MongoDB Enterprise 2.4+ in which case this should be run from a valid kinit session, alternative PLAIN for LDAP using user/password against MongoDB Enterprise 2.6+ which is sent in plaintext so should be used over SSL). Experimental" ],
 );
 
-@usage_order = qw/host port database collection user password write-concern read-concern wtimeout ssl sasl sasl-mechanism warning critical precision/;
+@usage_order = qw/host port user password expected-master ssl sasl sasl-mechanism/;
 get_options();
 
-$host       = validate_host($host);
-$port       = validate_port($port);
+defined($host) or usage "MongoDB host(s) not specified";
+my @hosts = split(",", $host);
+for(my $i=0; $i < scalar @hosts; $i++){
+    $hosts[$i] = validate_hostport(strip($hosts[$i]), "Mongo");
+}
+my $hosts  = "mongodb://" . join(",", @hosts);
+#my $hosts  = join(",", @hosts);
+vlog_options "Mongo host list", $hosts;
+if(scalar @hosts > 1){
+    $expected_master or usage "must specify --expected-master if specifying more than one host to connect to in the replica set";
+}
 $user       = validate_user($user);
 $password   = validate_password($password) if $password;
+$expected_master = validate_regex($expected_master) if $expected_master;
 grep { $sasl_mechanism eq $_ } qw/GSSAPI PLAIN/ or usage "invalid sasl-mechanism specified, must be either GSSAPI or PLAIN";
 vlog_options "ssl",  "enabled" if $ssl;
 vlog_options "sasl", "enabled" if $sasl;
@@ -56,13 +67,10 @@ set_timeout();
 
 $status = "OK";
 
-my $ip = validate_resolvable($host);
-vlog2 "resolved '$host' to '$ip'";
-
 my $client;
 try {
     $client = MongoDB::MongoClient->new(
-                                        "host"           => "$host:$port",
+                                        "host"           => $hosts,
                                         # hangs when giving only nodes of a replica set that aren't the Primary
                                         #"find_master"    => 1,
                                         "timeout"        => int($timeout * 1000 / 4), # connection timeout
@@ -73,9 +81,9 @@ try {
                                         "sasl-mechanism" => $sasl_mechanism,
                                        ) || quit "CRITICAL", "$!";
 };
-catch_quit "failed to connect to MongoDB host '$host:$port' ($ip)";
+catch_quit "failed to connect to MongoDB host '$hosts'";
 
-vlog2 "connection initiated to $host:$port\n";
+vlog2 "connection initiated to $host\n";
 
 my @dbs = $client->database_names;
 @dbs or quit "UNKNOWN", "no databases found on Mongod server, cannot call ismaster";
@@ -102,9 +110,16 @@ my $me     = $ismaster->{"me"};
 
 $msg = "master is '$master'";
 
-if($master ne $me){
-    critical;
-    $msg .= " (this host is '$me')";
+if($expected_master){
+    unless($master =~ /^$expected_master$/){
+        critical;
+        $msg .= " (expected regex: '$expected_master')";
+    }
+} else {
+    if($master ne $me){
+        critical;
+        $msg .= " (this host is '$me')";
+    }
 }
 
 quit $status, $msg;
