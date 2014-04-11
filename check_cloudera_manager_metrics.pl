34f42702fc462dbba8707267be4cdb5a9f500119
Hari Sekhon <harisekhon@gmail.com>
2014-04-11 21:26:00 +0100

    libraried out core cloudera manager bits for other impending plugins

--- check_cloudera_manager_metrics.pl
+++ check_cloudera_manager_metrics.pl
@@ -21,7 +21,7 @@ You may need to upgrade to Cloudera Manager 4.6 for the Standard Edition (free) 
 
 This is still using v1 of the API for compatability purposes";
 
-$VERSION = "0.5";
+$VERSION = "0.6";
 
 use strict;
 use warnings;
@@ -30,79 +30,39 @@ BEGIN {
     use lib dirname(__FILE__) . "/lib";
 }
 use HariSekhonUtils;
-use LWP::UserAgent;
-use JSON 'decode_json';
+use HariSekhon::ClouderaManager;
 
-my $ua = LWP::UserAgent->new;
 $ua->agent("Hari Sekhon $progname version $main::VERSION");
-
-my $protocol     = "http";
-my $api          = "/api/v1";
-my $default_port = 7180;
-$port            = $default_port;
-my $ssl_port     = 7183;
 
 my $activity;
 my $all_metrics;
-my $cluster;
-my $hostid;
 my $list_roles;
 my $metrics;
-my $nameservice;
-my $role;
-my $service;
-my $ssl_ca_path;
-my $tls = 0;
-my $tls_noverify;
-my $url;
-
 my %metric_results;
-my @metrics;
 my %metrics_found;
+my @metrics;
 my @metrics_not_found;
-
-env_creds("CM", "Cloudera Manager");
 
 %options = (
     %hostoptions,
     %useroptions,
-    "T|tls"            => [ \$tls,          "Use TLS connection to Cloudera Manager (automatically updates port to $ssl_port if still set to $default_port to save one 302 redirect round trip)" ],
-    "ssl-CA-path=s"    => [ \$ssl_ca_path,  "Path to CA certificate directory for validating SSL certificate (automatically enables --tls)" ],
-    "tls-noverify"     => [ \$tls_noverify, "Do not verify SSL certificate from Cloudera Manager (automatically enables --tls)" ],
+    %thresholdoptions,
+    %cm_options,
     "m|metrics=s"      => [ \$metrics,      "Metric(s) to fetch, comma separated (eg. dfs_capacity,dfs_capacity_used,dfs_capacity_used_non_hdfs). Thresholds may optionally be applied if a single metric is given" ],
     "a|all-metrics"    => [ \$all_metrics,  "Fetch all metrics for the given service/host/role specified by the options below. Caution, this could be a *lot* of metrics, best used to find available metrics for a given section" ],
-    "C|cluster=s"      => [ \$cluster,      "Cluster Name as shown in Cloudera Manager (eg. \"Cluster - CDH4\")" ],
-    "S|service=s"      => [ \$service,      "Service Name as shown in Cloudera Manager (eg. hdfs1, mapreduce4). Requires --cluster" ],
-    "I|hostId=s"       => [ \$hostid,       "HostId to collect metric for (eg. datanode1.domain.com)" ],
     "A|activityId=s"   => [ \$activity,     "ActivityId to collect metric for. Requires --cluster and --service" ],
-    "N|nameservice=s"  => [ \$nameservice,  "Nameservice to collect metric for (as specified in your HA configuration under dfs.nameservices). Requires --cluster and --service" ],
-    "R|roleId=s"       => [ \$role,         "RoleId to collect metric for (eg. hdfs4-NAMENODE-73d774cdeca832ac6a648fa305019cef - use --list-roleIds to find CM's role ids for a given service). Requires --cluster and --service" ],
     "list-roleIds"     => [ \$list_roles,   "List roleIds for a given cluster service. Convenience switch to find the roleId to query, prints role ids and exits immediately. Requires --cluster and --service" ],
-    %thresholdoptions,
 );
 
-@usage_order = qw/host port user password tls ssl-CA-path tls-noverify metrics all-metrics cluster service hostId activityId nameservice roleId list-roleIds warning critical/;
 get_options();
 
 $host       = validate_host($host);
 $port       = validate_port($port);
 $user       = validate_user($user);
 $password   = validate_password($password);
-$tls = 1 if(defined($ssl_ca_path) or defined($tls_noverify));
-if(defined($tls_noverify)){
-    $ua->ssl_opts( verify_hostname => 0 );
-    $tls = 1;
-}
-if(defined($ssl_ca_path)){
-    $ssl_ca_path = validate_directory($ssl_ca_path, undef, "SSL CA directory", "no vlog");
-    $ua->ssl_opts( SSL_ca_path => $ssl_ca_path );
-    $tls = 1;
-}
-if($tls){
-    vlog_options "TLS enabled",  "true";
-    vlog_options "SSL CA Path",  $ssl_ca_path  if defined($ssl_ca_path);
-    vlog_options "TLS noverify", $tls_noverify ? "true" : "false";
-}
+
+cm_tls();
+
 if($all_metrics){
     vlog_options "metrics", "ALL";
 } elsif($list_roles){
@@ -184,58 +144,7 @@ if($list_roles){
     $url =~ s/\?$//;
 }
 
-# Doesn't work
-#$ua->credentials("$host:$port", "Cloudera Manager", $user, $password);
-#$ua->credentials($host, "Cloudera Manager", $user, $password);
-$ua->show_progress(1) if $debug;
-if($tls){
-    $protocol = "https";
-    if($port == 7180){
-        vlog2 "overriding default http port 7180 to default tls port 7183";
-        $port = $ssl_port;
-    }
-}
-$host = validate_resolvable($host);
-my $url_prefix = "$protocol://$host:$port";
-$url = "$url_prefix$url";
-vlog2 "querying $url";
-my $req = HTTP::Request->new('GET',$url);
-$req->authorization_basic($user, $password);
-my $response = $ua->request($req);
-my $content  = $response->content;
-chomp $content;
-vlog3 "returned HTML:\n\n" . ( $content ? $content : "<blank>" ) . "\n";
-vlog2 "http code: " . $response->code;
-vlog2 "message: " . $response->message; 
-if(!$response->is_success){
-    my $err = "failed to query Cloudera Manager at '$url_prefix': " . $response->code . " " . $response->message;
-    if($content =~ /"message"\s*:\s*"(.+)"/){
-        $err .= ". Message returned by CM: $1";
-    }
-    if($response->message =~ /Can't verify SSL peers without knowing which Certificate Authorities to trust/){
-        $err .= ". Do you need to use --ssl-CA-path or --tls-noverify?";
-    }
-    quit "CRITICAL", $err;
-}
-unless($content){
-    quit "CRITICAL", "blank content returned by Cloudera Manager at '$url_prefix'";
-}
-
-vlog2 "parsing output from Cloudera Manager\n";
-
-# give a more user friendly message than the decode_json's die 'malformed JSON string, neither array, object, number, string or atom, at character offset ...'
-#isJson() used recursive regex which broke older clients
-# is_valid_json give ugly errors
-#try{
-#    is_valid_json($content) or quit "CRITICAL", "invalid json returned by Cloudera Manager at '$url_prefix', did you try to connect to the SSL port without --tls?";
-#};
-my $json;
-try{
-    $json = decode_json $content;
-};
-catch{
-    quit "invalid json returned by Cloudera Manager at '$url_prefix', did you try to connect to the SSL port without --tls?";
-};
+cm_query();
 
 if($list_roles){
     my @role_list;
