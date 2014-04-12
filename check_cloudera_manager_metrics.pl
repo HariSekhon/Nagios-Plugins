db795ab17ccf6cdc65f016b59d26584b47c2c43f
Hari Sekhon <harisekhon@gmail.com>
2014-04-12 17:15:09 +0100

    refactored out more code to lib HariSekhon::ClouderaManager

--- check_cloudera_manager_metrics.pl
+++ check_cloudera_manager_metrics.pl
@@ -21,7 +21,7 @@ You may need to upgrade to Cloudera Manager 4.6 for the Standard Edition (free) 
 
 This is still using v1 of the API for compatability purposes";
 
-$VERSION = "0.6";
+$VERSION = "0.7";
 
 use strict;
 use warnings;
@@ -34,9 +34,7 @@ use HariSekhon::ClouderaManager;
 
 $ua->agent("Hari Sekhon $progname version $main::VERSION");
 
-my $activity;
 my $all_metrics;
-my $list_roles;
 my $metrics;
 my %metric_results;
 my %metrics_found;
@@ -48,10 +46,10 @@ my @metrics_not_found;
     %useroptions,
     %thresholdoptions,
     %cm_options,
+    %cm_options_list,
     "m|metrics=s"      => [ \$metrics,      "Metric(s) to fetch, comma separated (eg. dfs_capacity,dfs_capacity_used,dfs_capacity_used_non_hdfs). Thresholds may optionally be applied if a single metric is given" ],
     "a|all-metrics"    => [ \$all_metrics,  "Fetch all metrics for the given service/host/role specified by the options below. Caution, this could be a *lot* of metrics, best used to find available metrics for a given section" ],
     "A|activityId=s"   => [ \$activity,     "ActivityId to collect metric for. Requires --cluster and --service" ],
-    "list-roleIds"     => [ \$list_roles,   "List roleIds for a given cluster service. Convenience switch to find the roleId to query, prints role ids and exits immediately. Requires --cluster and --service" ],
 );
 
 get_options();
@@ -61,9 +59,11 @@ $port       = validate_port($port);
 $user       = validate_user($user);
 $password   = validate_password($password);
 
+validate_cm_cluster_options();
+
 if($all_metrics){
     vlog_options "metrics", "ALL";
-} elsif($list_roles){
+} elsif(listing_cm_components()){
 } else {
     defined($metrics) or usage "no metrics specified";
     foreach(split(",", $metrics)){
@@ -75,86 +75,29 @@ if($all_metrics){
     @metrics = sort @metrics;
     vlog_options "metrics", "[ " . join(" ", @metrics) . " ]"; 
 }
-defined($hostid and ($cluster or $service or $activity or $nameservice or $role)) and usage "cannot specify both --hostId and --cluster/service/roleId type metrics at the same time";
-if(defined($cluster) and defined($service)){
-    $cluster    =~ /^\s*([\w\s\.-]+)\s*$/ or usage "Invalid cluster name given, may only contain alphanumeric, space, dash, dots or underscores";
-    $cluster    = $1;
-    $service    =~ /^\s*([\w-]+)\s*$/ or usage "Invalid service name given, must be alphanumeric with dashes";
-    $service    = $1;
-    vlog_options "cluster", $cluster;
-    vlog_options "service", $service;
-    $url = "$api/clusters/$cluster/services/$service";
-    if(defined($activity)){
-        $activity =~ /^\s*([\w-]+)\s*$/ or usage "Invalid activity given, must be alphanumeric with dashes";
-        $activity = $1;
-        vlog_options "activity", $activity;
-        $url .= "/activities/$activity";
-    } elsif(defined($nameservice)){
-        $nameservice =~ /^\s*([\w-]+)\s*$/ or usage "Invalid nameservice given, must be alphanumeric with dashes";
-        $nameservice = $1;
-        vlog_options "nameservice", $nameservice;
-        $url .= "/nameservices/$nameservice";
-    } elsif(defined($role)){
-        $role =~ /^\s*([\w-]+-\w+-\w+)\s*$/ or usage "Invalid role id given, expected in format such as <service>-<role>-<hexid> (eg hdfs4-NAMENODE-73d774cdeca832ac6a648fa305019cef). Use --list-roleIds to see available roles + IDs for a given cluster service";
-        $role = $1;
-        vlog_options "roleId", $role;
-        $url .= "/roles/$role";
-    }
-} elsif(defined($hostid)){
-    $hostid = isHostname($hostid) || usage "invalid host id given";
-    vlog_options "hostId", "$hostid";
-    $url .= "$api/hosts/$hostid";
-} else {
-    usage "must specify the type of metric to be collected using one of the following combinations:
 
---cluster --service
---cluster --service --activityId
---cluster --service --nameservice
---cluster --service --roleId
---hostId
-";
-}
-if($list_roles){
-    unless(defined($cluster) and defined($service)){
-        usage "must define cluster and service to be able to list roles";
-    }
-    $url = "$api/clusters/$cluster/services/$service";
-}
 validate_thresholds();
 
 vlog2;
 set_timeout();
 
 $status = "OK";
-if($list_roles){
-    $url .= "/roles";
-} else {
-    $url .= "/metrics?";
-    if($debug){
-        $url .= "view=full&"
-    }
-    if(not $all_metrics){
-        foreach(@metrics){
-            $url .= "metrics=$_&";
-        }
-        $url =~ s/\&$//;
-    }
-    $url =~ s/\?$//;
+
+list_cm_components();
+
+$url .= "/metrics?";
+if($debug){
+    $url .= "view=full&"
 }
+if(not $all_metrics){
+    foreach(@metrics){
+        $url .= "metrics=$_&";
+    }
+    $url =~ s/\&$//;
+}
+$url =~ s/\?$//;
 
 cm_query();
-
-if($list_roles){
-    my @role_list;
-    foreach(@{$json->{"items"}}){
-        if(defined($_->{"name"})){
-            push(@role_list, $_->{"name"});
-        } else {
-            code_error "no 'name' field returned in item from role listing from Cloudera Manager at '$url_prefix', check -vvv to see the output returned by CM";
-        }
-    }
-    usage "no checks performed, roles available for cluster '$cluster', service '$service':\n\n" . join("\n", @role_list);
-}
 
 unless(@{$json->{"items"}}){
     quit "CRITICAL", "no matching metrics returned by Cloudera Manager '$url_prefix'";
@@ -165,7 +108,7 @@ my $context = 0;
 my %metrics_contexts;
 foreach(@{$json->{"items"}}){
     foreach my $field (qw/name data/){
-        defined($_->{$field}) or quit "UNKNOWN", "no '$field' field returned item collection from Cloudera Manager, run with -vvv to see the (malformed?) json returned";
+        defined($_->{$field}) or quit "UNKNOWN", "no '$field' field returned item collection from Cloudera Manager. $nagios_plugins_support_msg_api";
     }
     if(defined($_->{"data"}[-1])){
         if(defined($metric_results{$_->{"name"}})){
