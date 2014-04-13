1cad051798b8dfb2e31b9c975cf497514981c539
Hari Sekhon <harisekhon@gmail.com>
2014-04-14 00:11:38 +0100

    added option to Cloudera Manager Mgmt services healthSummary

--- check_cloudera_manager_service_state.pl
+++ check_cloudera_manager_service_state.pl
@@ -21,7 +21,7 @@ This is still using v1 of the API for compatability purposes
 
 Tested on Cloudera Manager 5.0.0";
 
-$VERSION = "0.1";
+$VERSION = "0.2";
 
 use strict;
 use warnings;
@@ -34,18 +34,21 @@ use HariSekhon::ClouderaManager;
 
 $ua->agent("Hari Sekhon $progname version $main::VERSION");
 
+my $cm_mgmt = 0;
+
 %options = (
     %hostoptions,
     %useroptions,
     %cm_options,
     %cm_options_list,
+    "CM-mgmt"       =>  [ \$cm_mgmt,    "Cloudera Manager Management service state" ],
 );
 
 delete $options{"I|hostId=s"};
 delete $options{"A|activityId=s"};
 delete $options{"N|nameservice=s"};
 
-@usage_order = qw/host port user password tls ssl-CA-path tls-noverify cluster service roleId list-activities list-clusters list-hosts list-nameservices list-roles list-services/;
+@usage_order = qw/host port user password tls ssl-CA-path tls-noverify CM-mgmt cluster service roleId list-activities list-clusters list-hosts list-nameservices list-roles list-services/;
 
 get_options();
 
@@ -61,13 +64,24 @@ $status = "OK";
 
 list_cm_components();
 
-validate_cm_cluster_options();
+if($cm_mgmt){
+    $url .= "$api/cm/service";
+    if($cluster or $service or $role or $hostid){
+        usage "cannot mix --cluster/--service/--role/--host and --CM-mgmt";
+    }
+} else {
+    validate_cm_cluster_options();
+}
 
 cm_query();
 
 my $state;
-if($cluster and $service){
-    $msg = "cluster '$cluster' service '$service'";
+if(($cluster and $service) or $cm_mgmt){
+    if($cm_mgmt){
+        $msg = "Cloudera Manager Mgmt service";
+    } else {
+        $msg = "cluster '$cluster' service '$service'";
+    }
     if($role){
         check_cm_field("roleState");
         check_cm_field("type");
