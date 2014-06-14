c03333ce5af0ea745688692a657bdd3942166a49
Hari Sekhon <harisekhon@gmail.com>
2014-06-14 22:32:06 +0100

    updated check_ibm_biginsights_hdfs_space.pl

--- check_ibm_biginsights_hdfs_space.pl
+++ check_ibm_biginsights_hdfs_space.pl
@@ -41,8 +41,8 @@ $host       = validate_host($host);
 $port       = validate_port($port);
 $user       = validate_user($user);
 $password   = validate_password($password);
-validate_ssl();
 validate_thresholds();
+validate_ssl();
 
 vlog2;
 set_timeout();
