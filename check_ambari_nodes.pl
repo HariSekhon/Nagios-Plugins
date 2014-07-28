a204cff87f3e674e31c6399d8d5bfc61a7ed0bc3
Hari Sekhon <harisekhon@gmail.com>
2014-07-28 16:48:01 +0100

    put status before state since it's more generic, then more specific message can follow

--- check_ambari_nodes.pl
+++ check_ambari_nodes.pl
@@ -69,7 +69,7 @@ if($node){
     # state appears to be just a string, whereas status is the documented state type HEALTHY/UNHEALTHY/UNKNOWN to check according to API docs. However I've just discovered that state can stay HEALTHY and status UNHEALTHY
     my $node_state  = get_field("Hosts.host_state");
     my $node_status = get_field("Hosts.host_status");
-    $msg = "node '$node' state: " . ($node_state eq "HEALTHY" ? "healthy" : $node_state) . ", status: " . ($node_status eq "HEALTHY" ? "healthy" : $node_status);
+    $msg = "node '$node' status: " . ($node_status eq "HEALTHY" ? "healthy" : $node_status) . ", state: " . ($node_state eq "HEALTHY" ? "healthy" : $node_state);
     if($node_status eq "HEALTHY"){
         # ok
     } elsif($node_status eq "UNHEALTHY"){
@@ -85,7 +85,7 @@ if($node){
     if(@items){
         my @nodes;
         foreach (@items){
-            push(@nodes, get_field2($_, "Hosts.host_name") . " (state=" . get_field2($_, "Hosts.host_state") . "/status=" . get_field2($_, "Hosts.host_status") . ")");
+            push(@nodes, get_field2($_, "Hosts.host_name") . " (status=" . get_field2($_, "Hosts.host_status") . "/state=" . get_field2($_, "Hosts.host_state") . ")");
         }
         my $num_nodes = scalar @nodes;
         plural $num_nodes;
