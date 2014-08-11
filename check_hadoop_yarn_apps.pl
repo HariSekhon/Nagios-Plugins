d55e9cbff411b56c3482f4e86131f88ddba4fbad
Hari Sekhon <harisekhon@gmail.com>
2014-08-11 16:44:40 +0100

    added queue support as we're only checking the root queue by default

--- check_hadoop_yarn_apps.pl
+++ check_hadoop_yarn_apps.pl
@@ -15,7 +15,7 @@ Optional thresholds on running yarn apps to aid in capacity planning
 
 Tested on Hortonworks HDP 2.1 (Hadoop 2.4.0.2.1.1.0-385)";
 
-$VERSION = "0.2";
+$VERSION = "0.3";
 
 use strict;
 use warnings;
@@ -34,8 +34,11 @@ set_port_default(8088);
 
 env_creds(["HADOOP_YARN_RESOURCE_MANAGER", "HADOOP"], "Yarn Resource Manager");
 
+my $queue = "root";
+
 %options = (
     %hostoptions,
+    "Q|queue=s"      =>  [ \$queue, "Queue to output stats for (default: root)" ],
     %thresholdoptions,
 );
 
@@ -75,41 +78,53 @@ my $found_mbean = 0;
 #       Hadoop:service=ResourceManager,name=RpcActivityForPort8141
 #       Hadoop:service=ResourceManager,name=RpcActivityForPort8030
 #       Hadoop:service=ResourceManager,name=JvmMetrics
+my $apps_submitted = 0;
+my $apps_running   = 0;
+my $apps_pending   = 0;
+my $apps_completed = 0;
+my $apps_killed    = 0;
+my $apps_failed    = 0;
+my $active_users   = 0;
+my $active_apps    = 0;
+
+my $mbean_name = "Hadoop:service=ResourceManager,name=QueueMetrics";
+$mbean_name .= ",q0=$queue";
 foreach(@beans){
-    next unless get_field2($_, "name") eq "Hadoop:service=ResourceManager,name=QueueMetrics,q0=root";
+    vlog2 Dumper($_) if get_field2($_, "name") =~ /QueueMetrics/;
+    next unless get_field2($_, "name") =~ /^$mbean_name$/;
     $found_mbean++;
-    my $apps_submitted = get_field2_int($_, "AppsSubmitted");
-    my $apps_running   = get_field2_int($_, "AppsRunning");
-    my $apps_pending   = get_field2_int($_, "AppsPending");
-    my $apps_completed = get_field2_int($_, "AppsCompleted");
-    my $apps_killed    = get_field2_int($_, "AppsKilled");
-    my $apps_failed    = get_field2_int($_, "AppsFailed");
-    my $active_users   = get_field2_int($_, "ActiveUsers");
-    my $active_apps    = get_field2_int($_, "ActiveApplications");
-    $msg  = "yarn apps: ";
-    $msg .= "$apps_running running";
-    check_thresholds($apps_running);
-    $msg .= ", ";
-    $msg .= "$apps_pending pending, ";
-    $msg .= "$active_apps active, ";
-    $msg .= "$apps_submitted submitted, ";
-    $msg .= "$apps_completed completed, ";
-    $msg .= "$apps_killed killed, ";
-    $msg .= "$apps_failed failed. ";
-    $msg .= "$active_users active users";
-    $msg .= " | ";
-    $msg .= "'apps running'=$apps_running";
-    msg_perf_thresholds();
-    $msg .= " ";
-    $msg .= "'apps pending'=$apps_pending ";
-    $msg .= "'apps active'=$active_apps ";
-    $msg .= "'apps submitted'=${apps_submitted}c ";
-    $msg .= "'apps completed'=${apps_completed}c ";
-    $msg .= "'apps killed'=${apps_killed}c ";
-    $msg .= "'apps failed'=${apps_failed}c ";
-    $msg .= "'active users'=$active_users";
+    $apps_submitted = get_field2_int($_, "AppsSubmitted");
+    $apps_running   = get_field2_int($_, "AppsRunning");
+    $apps_pending   = get_field2_int($_, "AppsPending");
+    $apps_completed = get_field2_int($_, "AppsCompleted");
+    $apps_killed    = get_field2_int($_, "AppsKilled");
+    $apps_failed    = get_field2_int($_, "AppsFailed");
+    $active_users   = get_field2_int($_, "ActiveUsers");
+    $active_apps    = get_field2_int($_, "ActiveApplications");
     last;
 }
+$msg  = "yarn apps for queue '$queue': ";
+$msg .= "$apps_running running";
+check_thresholds($apps_running);
+$msg .= ", ";
+$msg .= "$apps_pending pending, ";
+$msg .= "$active_apps active, ";
+$msg .= "$apps_submitted submitted, ";
+$msg .= "$apps_completed completed, ";
+$msg .= "$apps_killed killed, ";
+$msg .= "$apps_failed failed. ";
+$msg .= "$active_users active users";
+$msg .= " | ";
+$msg .= "'apps running'=$apps_running";
+msg_perf_thresholds();
+$msg .= " ";
+$msg .= "'apps pending'=$apps_pending ";
+$msg .= "'apps active'=$active_apps ";
+$msg .= "'apps submitted'=${apps_submitted}c ";
+$msg .= "'apps completed'=${apps_completed}c ";
+$msg .= "'apps killed'=${apps_killed}c ";
+$msg .= "'apps failed'=${apps_failed}c ";
+$msg .= "'active users'=$active_users";
 
 quit "UNKNOWN", "failed to find mbean. $nagios_plugins_support_msg_api" unless $found_mbean;
 
