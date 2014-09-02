042d892e47f82498598922ea048c8f0b8a9ec8d1
Hari Sekhon <harisekhon@gmail.com>
2014-09-02 12:21:42 +0100

    added check_datastax_opscenter_cassandra_space_used.pl

---
+++ check_datastax_opscenter_cassandra_space_used.pl
@@ -0,0 +1,92 @@
+#!/usr/bin/perl -T
+# nagios: -epn
+#
+#  Author: Hari Sekhon
+#  Date: started 2013
+#
+#  http://github.com/harisekhon
+#
+#  License: see accompanying LICENSE file
+#
+
+# http://www.datastax.com/documentation/opscenter/5.0/api/docs/cluster_info.html#storage-capacity
+
+$DESCRIPTION = "Nagios Plugin to check Cassandra storage capacity % used via DataStax OpsCenter Rest API
+
+Tested on DataStax OpsCenter 5.0.0";
+
+$VERSION = "0.1";
+
+use strict;
+use warnings;
+BEGIN {
+    use File::Basename;
+    use lib dirname(__FILE__) . "/lib";
+}
+use HariSekhonUtils;
+use LWP::Simple '$ua';
+
+$ua->agent("Hari Sekhon $progname version $main::VERSION");
+
+set_port_default(8888);
+set_threshold_defaults(80, 90);
+
+env_creds("DataStax OpsCenter");
+
+my $cluster;
+
+%options = (
+    %hostoptions,
+    %useroptions,
+    "C|cluster=s"   =>  [ \$cluster, "Cluster name to check" ],
+    %thresholdoptions,
+);
+splice @usage_order, 6, 0, 'cluster';
+
+get_options();
+
+$host       = validate_host($host);
+$port       = validate_port($port);
+$user       = validate_user($user);
+$password   = validate_password($password);
+$cluster or usage "cluster not defined";
+$cluster =~ /^[A-Za-z0-9]+$/ or usage "invalid cluster name given, must be alphanumeric";
+
+validate_thresholds(1, 1, { "simple" => "upper", "integer" => 0, "positive" => 1, "min" => 0, "max" => 100 });
+
+vlog2;
+set_timeout();
+set_http_timeout($timeout-1);
+
+$ua->show_progress(1) if $debug;
+
+$status = "OK";
+
+my $url = "http://$host:$port/$cluster/storage-capacity";
+
+my $content = curl $url, "DataStax OpsCenter", $user, $password;
+try{
+    $json = isJson($content);
+};
+catch {
+    quit "CRITICAL", "invalid json returned by DataStax OpsCenter at $url";
+};
+
+my $free_gb         = get_field_int("free_gb");
+my $used_gb         = get_field_int("used_gb");
+my $reporting_nodes = get_field_int("reporting_nodes");
+
+vlog2 "free_gb: $free_gb";
+vlog2 "used_gb: $used_gb";
+
+my $total_gb = $free_gb + $used_gb;
+vlog2 "total_gb: $total_gb";
+
+my $pc_used = sprintf("%.2f", $used_gb / $total_gb * 100);
+
+$msg = "$pc_used% space used in cassandra cluster '$cluster' [" . human_units($used_gb * 1024 * 1024 * 1024) . "/" . human_units($total_gb * 1024 * 1024 * 1024) . "] across $reporting_nodes reporting nodes | '% space used'=$pc_used%";
+msg_perf_thresholds();
+$msg .= " 'space used'=${used_gb}GB 'space free'=${free_gb}GB 'space total'=${total_gb}GB 'reporting nodes'=$reporting_nodes";
+
+vlog2;
+quit $status, $msg;
