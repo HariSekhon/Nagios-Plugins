cb5830f0d79ea7a4ad378a7e54064df781f018c8
Hari Sekhon <harisekhon@gmail.com>
2014-10-05 18:24:48 +0100

    added check_mapr_disk_balancer.pl

---
+++ check_mapr_disk_balancer.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl -T
+# nagios: -epn
+#
+#  Author: Hari Sekhon
+#  Date: 2014-09-15 23:54:05 +0100 (Mon, 15 Sep 2014)
+#
+#  https://github.com/harisekhon/nagios-plugins
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  vim:ts=4:sts=4:sw=4:et
+
+$DESCRIPTION = "Nagios Plugin to check the status of the MapR-FS Balancer and output balance metrics";
+
+$VERSION = "0.1";
+
+use strict;
+use warnings;
+BEGIN {
+    use File::Basename;
+    use lib dirname(__FILE__) . "/lib";
+}
+use HariSekhonUtils;
+
+my $maprcli = "maprcli";
+
+%options = (
+    "m|maprcli=s"   => [ \$maprcli, "Path to 'maprcli' command if not in \$PATH ($ENV{PATH})" ],
+);
+splice @usage_order, 6, 0, qw/maprcli/;
+
+get_options();
+
+$maprcli = validate_program_path($maprcli, "maprcli");
+
+vlog2;
+set_timeout();
+
+$status = "OK";
+
+my $cmd = "$maprcli dump balancermetrics -json";
+my @output = cmd($cmd, 1);
+$json = join(" ", @output);
+$json = isJson($json) or quit "UNKNOWN", "invalid json returned by command '$cmd'";
+
+my $balancer_status     = get_field("status");
+my $total               = get_field_int("total");
+my $numContainersMoved  = get_field_int("data.0.numContainersMoved");
+my $numMBMoved          = get_field_int("data.0.numMBMoved");
+my $timeOfLastMove      = get_field("data.0.timeOfLastMove", 1);
+
+critical unless $balancer_status eq "OK";
+
+$msg = "balancer status '$balancer_status': total = $total, containers moved = $numContainersMoved, volume moved = " . human_units($numMBMoved * 1024 * 1024);
+$msg .= ", time of last move = '$timeOfLastMove'" if $timeOfLastMove;
+$msg .= " | total=${total} containers_moved=${numContainersMoved}c volume_moved=${numMBMoved}MB";
+
+vlog2;
+quit $status, $msg;
