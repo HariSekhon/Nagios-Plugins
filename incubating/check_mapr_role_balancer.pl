a6636a27ead10b2debfc7ab3a5c5339c9cbe759b
Hari Sekhon <harisekhon@gmail.com>
2014-10-05 18:46:22 +0100

    updated incubating/check_mapr_role_balancer.pl

--- incubating/check_mapr_role_balancer.pl
+++ incubating/check_mapr_role_balancer.pl
@@ -10,7 +10,7 @@
 #
 #  vim:ts=4:sts=4:sw=4:et
 
-$DESCRIPTION = "Nagios Plugin to check the status of the MapR-FS Role Balancer via the maprcli command. Call over NRPE.";
+$DESCRIPTION = "Nagios Plugin to check the status of the MapR Role Balancer via the maprcli command. Call over NRPE.";
 
 $VERSION = "0.1";
 
