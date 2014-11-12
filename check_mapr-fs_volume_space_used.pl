325b8e1c4cfbc3c2ff15e7831a8bb383d29e4772
Hari Sekhon <harisekhon@gmail.com>
2014-11-12 00:54:13 +0000

    abstracted out volume code, removed extra total and fixed total perfdata

--- check_mapr-fs_volume_space_used.pl
+++ check_mapr-fs_volume_space_used.pl
@@ -30,28 +30,23 @@ use HariSekhon::MapR;
 $ua->agent("Hari Sekhon $progname version $main::VERSION");
 
 my $logical_used;
-my $list_volumes;
-my $volume_name;
 
 %options = (
     %mapr_options,
     %mapr_option_cluster,
-    "L|volume=s"      =>  [ \$volume_name,    "Volume name to check (returns all volumes by default)" ],
+    %mapr_option_volume,
     "logical-used"    =>  [ \$logical_used,   "Check logical space used instead of total space used (ie excluded snapshots from the total)" ],
-    "list-volumes"    =>  [ \$list_volumes,   "List volume names and mount points. Convenience switch to find what to supply to --volume" ],
     %thresholdoptions,
 );
-splice @usage_order, 6, 0, qw/volume cluster logical-used list-volumes/;
+splice @usage_order, 6, 0, qw/volume cluster logical-used list-volumes list-clusters/;
 
 get_options();
 
 validate_mapr_options();
 list_clusters();
 $cluster = validate_cluster($cluster) if $cluster;
-if($volume_name){
-    $volume_name =~ /^([A-Za-z0-9\._-]+)$/ or usage "invalid volume name specified";
-    $volume_name = $1;
-}
+list_volumes();
+$volume = validate_volume($volume) if $volume;
 validate_thresholds(0, 0, { "simple" => "upper", "integer" => 1, "positive" => 1});
 
 vlog2;
@@ -60,11 +55,9 @@ set_timeout();
 $status = "OK";
 
 my $url = "/volume/list";
-$url .= "?" if($cluster or ($volume_name and not $list_volumes));
+$url .= "?" if($cluster or $volume);
 $url .= "cluster=$cluster&" if $cluster;
-if(not $list_volumes){
-    $url .= "filter=[volumename==$volume_name]" if $volume_name;
-}
+$url .= "filter=[volumename==$volume]" if $volume;
 
 $json = curl_mapr $url, $user, $password;
 
@@ -74,9 +67,7 @@ my %vols;
 my $found = 0;
 foreach(@data){
     my $vol = get_field2($_, "volumename");
-    unless($list_volumes){
-        next if($volume_name and $volume_name ne $vol);
-    }
+    next if($volume and $volume ne $vol);
     $found++;
     $vols{$vol}{"logicalUsed"} = get_field2($_, "logicalUsed");
     $vols{$vol}{"totalused"}   = get_field2($_, "totalused");
@@ -84,16 +75,9 @@ foreach(@data){
     isFloat($vols{$vol}{"logicalUsed"}) or quit "UNKNOWN", "invalid non-float returned for logical used MB '$vols{$vol}{logicalUsed}' for volume '$vol' by MCS API. $nagios_plugins_support_msg_api";
     isFloat($vols{$vol}{"totalused"})   or quit "UNKNOWN", "invalid non-float returned for total used MB '$vols{$vol}{totalused}' for volume '$vol' by MCS API. $nagios_plugins_support_msg_api";
 }
-if($list_volumes){
-    print "MapR-FS volumes:\n\n";
-    printf("%-30s %s\n\n", "Name", "Mount Point");
-    foreach my $vol (sort keys %vols){
-        printf("%-30s %s\n", $vol, $vols{$vol}{"mount"});
-    }
-    exit $ERRORS{"UNKNOWN"};
-} elsif(not $found){
-    if($volume_name){
-        quit "UNKNOWN", "volume with name '$volume_name' was not found, check you've supplied the correct name, see --list-volumes";
+if(not $found){
+    if($volume){
+        quit "UNKNOWN", "volume with name '$volume' was not found, check you've supplied the correct name, see --list-volumes";
     } else {
         quit "UNKNOWN", "no volumes found! See --list-volumes or -vvv. $nagios_plugins_support_msg_api";
     }
@@ -108,7 +92,7 @@ foreach my $vol (sort keys %vols){
     #$msg .= " logical=$vols{$vol}{logicalUsed}";
     check_thresholds($vols{$vol}{"logicalUsed"}) if $logical_used;
     $msg .= " total=" . human_units($vols{$vol}{totalused}, "MB", 1);
-    $msg .= " total=$vols{$vol}{totalused}";
+    #$msg .= " total=$vols{$vol}{totalused}";
     check_thresholds($vols{$vol}{"totalused"}) unless $logical_used;
     $msg .= ", ";
 }
@@ -117,7 +101,7 @@ $msg .= " |";
 foreach my $vol (sort keys %vols){
     $msg .= " 'volume $vol logical used'=$vols{$vol}{logicalUsed}MB";
     msg_perf_thresholds() if $logical_used;
-    $msg .= " 'volume $vol total used'=$vols{$vol}{logicalUsed}MB";
+    $msg .= " 'volume $vol total used'=$vols{$vol}{totalused}MB";
     msg_perf_thresholds() unless $logical_used;
 }
 
