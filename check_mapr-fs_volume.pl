865b1b35f43eb458f1a40f3666ab492e68a5a922
Hari Sekhon <harisekhon@gmail.com>
2014-11-12 00:48:04 +0000

    abstracted volume code to HariSekhon::MapR

--- check_mapr-fs_volume.pl
+++ check_mapr-fs_volume.pl
@@ -30,8 +30,6 @@ use HariSekhon::MapR;
 $ua->agent("Hari Sekhon $progname version $main::VERSION");
 
 my $logical_used;
-my $list_volumes;
-my $volume_name;
 
 my $ignore_mounted;
 my $ignore_readonly;
@@ -40,24 +38,21 @@ my $ignore_needsGfsck;
 %options = (
     %mapr_options,
     %mapr_option_cluster,
-    "L|volume=s"         =>  [ \$volume_name,       "Volume name to check (returns all volumes by default)" ],
+    %mapr_option_volume,
     "logical-used"       =>  [ \$logical_used,      "Check logical space used instead of total space used (ie excluded snapshots from the total)" ],
-    "list-volumes"       =>  [ \$list_volumes,      "List volume names and mount points. Convenience switch to find what to supply to --volume" ],
     "ignore-mounted"     =>  [ \$ignore_mounted,    "Ignore mounted" ],
     "ignore-readonly"    =>  [ \$ignore_mounted,    "Ignore read only" ],
     "ignore-needsGfsck"  =>  [ \$ignore_needsGfsck, "Ignore needs Gfsck" ],
 );
-splice @usage_order, 6, 0, qw/volume cluster ignore-mounted ignore-readonly ignore-needsGfsck list-volumes/;
+splice @usage_order, 6, 0, qw/volume cluster ignore-mounted ignore-readonly ignore-needsGfsck list-volumes list-clusters/;
 
 get_options();
 
 validate_mapr_options();
 list_clusters();
 $cluster = validate_cluster($cluster) if $cluster;
-if($volume_name){
-    $volume_name =~ /^([A-Za-z0-9\._-]+)$/ or usage "invalid volume name specified";
-    $volume_name = $1;
-}
+list_volumes();
+$volume  = validate_volume($volume) if $volume;
 validate_thresholds(0, 0, { "simple" => "upper", "integer" => 1, "positive" => 1});
 
 vlog2;
@@ -67,9 +62,7 @@ $status = "OK";
 
 my $url = "/volume/list?";
 $url .= "cluster=$cluster&" if $cluster;
-if(not $list_volumes){
-    $url .= "filter=[volumename==$volume_name]&" if $volume_name;
-}
+$url .= "filter=[volumename==$volume]&" if $volume;
 $url .= "columns=volumename,mountdir,readonly,needsGfsck,mounted";
 
 $json = curl_mapr $url, $user, $password;
@@ -80,26 +73,16 @@ my %vols;
 my $found = 0;
 foreach(@data){
     my $vol = get_field2($_, "volumename");
-    unless($list_volumes){
-        next if($volume_name and $volume_name ne $vol);
-    }
-
+    next if($volume and $volume ne $vol);
     $found++;
     $vols{$vol}{"mount"}       = get_field2($_, "mountdir");
     $vols{$vol}{"mounted"}     = get_field2($_, "mounted");
     $vols{$vol}{"needsGfsck"}  = get_field2($_, "needsGfsck");
     $vols{$vol}{"readonly"}    = get_field2($_, "readonly");
 }
-if($list_volumes){
-    print "MapR-FS volumes:\n\n";
-    printf("%-30s %s\n\n", "Name", "Mount Point");
-    foreach my $vol (sort keys %vols){
-        printf("%-30s %s\n", $vol, $vols{$vol}{"mount"});
-    }
-    exit $ERRORS{"UNKNOWN"};
-} elsif(not $found){
-    if($volume_name){
-        quit "UNKNOWN", "volume with name '$volume_name' was not found, check you've supplied the correct name, see --list-volumes";
+if(not $found){
+    if($volume){
+        quit "UNKNOWN", "volume with name '$volume' was not found, check you've supplied the correct name, see --list-volumes";
     } else {
         quit "UNKNOWN", "no volumes found! See --list-volumes or -vvv. $nagios_plugins_support_msg_api";
     }
