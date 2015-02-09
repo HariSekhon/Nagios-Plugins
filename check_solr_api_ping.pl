207134d20f483423a0b11fd44a5d8294e7ae3345
Hari Sekhon <harisekhon@gmail.com>
2015-02-09 22:32:09 +0000

    added collection support since ping api if unique to each colletion

--- check_solr_api_ping.pl
+++ check_solr_api_ping.pl
@@ -15,7 +15,7 @@ Optional warning/critical thresholds apply to query response time (QTime field)
 
 Tested on Solr 3.1, 3.6.2 and Solr / SolrCloud 4.x";
 
-$VERSION = "0.1";
+$VERSION = "0.2";
 
 use strict;
 use warnings;
@@ -34,14 +34,19 @@ my $api_ping;
 
 %options = (
     %solroptions,
+    %solroptions_collection,
     %thresholdoptions,
 );
-splice @usage_order, 6, 0, qw/api-ping/;
+splice @usage_order, 6, 0, qw/api-ping collection/;
 
 get_options();
 
 $host       = validate_host($host);
 $port       = validate_port($port);
+unless(defined($collection)){
+    $collection = "collection1";
+}
+$collection = validate_collection($collection);
 validate_thresholds(0, 0, { 'simple' => 'upper', 'positive' => 1, 'integer' => 1 });
 validate_ssl();
 
@@ -50,7 +55,9 @@ set_timeout();
 
 $status = "OK";
 
-$url = "$solr_admin/ping?distrib=false";
+list_solr_collections();
+
+$url = "solr/$collection/admin/ping?distrib=false";
 
 $json = curl_solr $url;
 
@@ -58,7 +65,7 @@ my $qstatus = get_field("status");
 unless($qstatus eq "OK"){
     critical;
 }
-$msg .= "Solr API ping " . ( $verbose ? "(" . get_field("responseHeader.params.q") . ") " : "") . "returned $qstatus, query time ${query_time}ms";
+$msg .= "Solr API ping of collection '$collection' " . ( $verbose ? "(" . get_field("responseHeader.params.q") . ") " : "") . "returned $qstatus, query time ${query_time}ms";
 check_thresholds($query_time);
 $msg .= " | ";
 
