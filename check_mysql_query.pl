cf69970d5292f0187b45e4be431e8046e919fbc0
Hari Sekhon <harisekhon@gmail.com>
2015-02-12 22:14:12 +0000

    tweaked description

--- check_mysql_query.pl
+++ check_mysql_query.pl
@@ -13,7 +13,7 @@ $DESCRIPTION = "Nagios Plugin to check arbitrary MySQL queries against regex mat
 
 It looks like a similar plugin has now been added to the standard Nagios Plugins collection, although this one still has more features.
 
-Only the first row return is considered for the result so your SQL query should take that in to account.
+Only the first row returned is considered for the result so your SQL query should take that in to account.
 
 DO NOT ADD a semi-colon to the end of your query in Nagios, although this plugin can handle this fine and it works on the command line, in Nagios the command will end up being prematurely terminated and result in a null critical error that is hard to debug and that this plugin cannot catch since it's raised by the shell before this plugin is executed
 ";
