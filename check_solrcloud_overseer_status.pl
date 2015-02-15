ac8257cbe7388e9ca94b0c3769d932c485d59bdc
Hari Sekhon <harisekhon@gmail.com>
2015-02-15 16:44:36 +0000

    added check_solrcloud_overseer_status.pl

---
+++ check_solrcloud_overseer_status.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl -T
+# nagios: -epn
+#
+#  Author: Hari Sekhon
+#  Date: 2014-06-07 18:29:01 +0100 (Sat, 07 Jun 2014)
+#
+#  http://github.com/harisekhon
+#
+#  License: see accompanying LICENSE file
+#
+
+$DESCRIPTION = "Nagios Plugin to check Solr / SolrCloud Overseer via SolrCloud Collections API
+
+See also adjacent plugin check_solrcloud_overseer_zookeeper.pl which does the same but via ZooKeeper
+
+Tested on SolrCloud 4.x
+";
+
+our $VERSION = "0.1";
+
+use strict;
+use warnings;
+BEGIN {
+    use File::Basename;
+    use lib dirname(__FILE__) . "/lib";
+}
+use HariSekhonUtils qw/:DEFAULT :time/;
+use HariSekhon::Solr;
+
+$ua->agent("Hari Sekhon $progname $main::VERSION");
+
+%options = (
+    %solroptions,
+    %thresholdoptions,
+);
+splice @usage_order, 4, 0, qw//;
+
+get_options();
+
+$host = validate_host($host);
+$port = validate_port($port);
+validate_thresholds();
+validate_ssl();
+
+vlog2;
+set_timeout();
+
+$status = "OK";
+
+$json = curl_solr "solr/admin/collections?action=OVERSEERSTATUS&distrib=true";
+
+my $overseer = get_field("leader");
+
+$msg .= "SolrCloud overseer leader is '$overseer', query time ${query_qtime}ms";
+check_thresholds($query_time);
+$msg .= ", query QTime ${query_qtime}ms";
+$msg .= sprintf(' | query_time=%dms', $query_time);
+msg_perf_thresholds();
+$msg .= sprintf(' query_QTime=%sms', $query_time, $query_qtime);
+
+quit $status, $msg;
