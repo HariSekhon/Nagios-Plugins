a855f663446d138813501fa2a2c4a43e0ba34719
Hari Sekhon <harisekhon@gmail.com>
2015-02-19 21:31:28 +0000

    added support for alternative solr http contexts

--- check_solr_api_ping.pl
+++ check_solr_api_ping.pl
@@ -17,7 +17,7 @@ Configurable warning/critical thresholds apply to this API call's millisecond ti
 
 Tested on Solr 3.1, 3.6.2 and Solr / SolrCloud 4.x";
 
-$VERSION = "0.3";
+$VERSION = "0.3.1";
 
 use strict;
 use warnings;
@@ -37,9 +37,10 @@ my $api_ping;
 %options = (
     %solroptions,
     %solroptions_collection,
+    %solroptions_context,
     %thresholdoptions,
 );
-splice @usage_order, 6, 0, qw/collection/;
+splice @usage_order, 6, 0, qw/collection http-context list-collections/;
 
 get_options();
 
@@ -48,9 +49,10 @@ $port       = validate_port($port);
 unless(defined($collection)){
     $collection = "collection1";
 }
-$collection = validate_collection($collection);
-validate_thresholds(0, 0, { 'simple' => 'upper', 'positive' => 1, 'integer' => 1 });
+$collection   = validate_collection($collection);
+$http_context = validate_solr_context($http_context);
 validate_ssl();
+validate_thresholds(0, 0, { 'simple' => 'upper', 'positive' => 1, 'integer' => 1 });
 
 vlog2;
 set_timeout();
@@ -59,7 +61,7 @@ $status = "OK";
 
 list_solr_collections();
 
-$url = "solr/$collection/admin/ping?distrib=false";
+$url = "$http_context/$collection/admin/ping?distrib=false";
 
 $json = curl_solr $url;
 
@@ -75,4 +77,5 @@ $msg .= sprintf('query_time=%dms', $query_time);
 msg_perf_thresholds();
 $msg .= sprintf(' query_QTime=%dms', $query_qtime);
 
+vlog2;
 quit $status, $msg;
