92935aa2e1e9c9f4e89f7b2098f037ead3b02128
Hari Sekhon <harisekhon@gmail.com>
2015-03-20 10:38:17 +0000

    added perfdata, set active shard thresholds to be lower

--- check_elasticsearch_cluster_status_nodes_shards.pl
+++ check_elasticsearch_cluster_status_nodes_shards.pl
@@ -22,7 +22,7 @@ check_elasticsearch_cluster_shards.pl
 
 Tested on Elasticsearch 0.90.1, 1.2.1, 1.4.4";
 
-$VERSION = "0.2";
+$VERSION = "0.3";
 
 use strict;
 use warnings;
@@ -47,13 +47,13 @@ my $unassigned_shard_thresholds = "0,1";
 %options = (
     %hostoptions,
     "C|cluster-name=s"          =>  [ \$cluster,                            "Cluster name to expect (optional). Cluster name is used for auto-discovery and should be unique to each cluster in a single network" ],
-    "n|nodes=s"                 =>  [ \$node_thresholds,                    "Node thresholds (inclusive, optional)" ],
-    "d|data-nodes=s"            =>  [ \$data_node_thresholds,               "Data Node thresholds (inclusive, optional)" ],
-    "active-primary-shards=s"   =>  [ \$active_primary_shard_thresholds,    "Active Primary Shards thresholds (inclusive, optional)" ],
-    "active-shards=s"           =>  [ \$active_shard_thresholds,            "Active Shards thresholds (inclusive, optional)" ],
-    "relocating-shards=s"       =>  [ \$relocating_shard_thresholds,        "Relocating Shards thresholds (inclusive, optional)" ],
-    "initializing-shards=s"     =>  [ \$initializing_shard_thresholds,      "Initializing Shards thresholds (inclusive, optional)" ],
-    "unassigned-shards=s"       =>  [ \$unassigned_shard_thresholds,        "Unassigned Shards thresholds (inclusive, default w,c: 0,1)" ],
+    "n|nodes=s"                 =>  [ \$node_thresholds,                    "Node lower thresholds (inclusive, optional)" ],
+    "d|data-nodes=s"            =>  [ \$data_node_thresholds,               "Data Node lower thresholds (inclusive, optional)" ],
+    "active-primary-shards=s"   =>  [ \$active_primary_shard_thresholds,    "Active Primary Shards lower thresholds (inclusive, optional)" ],
+    "active-shards=s"           =>  [ \$active_shard_thresholds,            "Active Shards lower thresholds (inclusive, optional)" ],
+    "relocating-shards=s"       =>  [ \$relocating_shard_thresholds,        "Relocating Shards upper thresholds (inclusive, optional)" ],
+    "initializing-shards=s"     =>  [ \$initializing_shard_thresholds,      "Initializing Shards upper thresholds (inclusive, optional)" ],
+    "unassigned-shards=s"       =>  [ \$unassigned_shard_thresholds,        "Unassigned Shards upper thresholds (inclusive, default w,c: 0,1)" ],
 );
 splice @usage_order, 6, 0, qw/cluster-name nodes data-nodes active-primary-shards active-shards relocating-shards initializing-shards unassigned-shards/;
 
@@ -66,8 +66,8 @@ my $options_upper = { "simple" => "upper", "integer" => 1, "positive" => 1 };
 my $options_lower = { "simple" => "lower", "integer" => 1, "positive" => 1 };
 validate_thresholds(undef, undef, $options_lower, "nodes",                  $node_thresholds);
 validate_thresholds(undef, undef, $options_lower, "data nodes",             $data_node_thresholds);
-validate_thresholds(undef, undef, $options_upper, "active primary shards",  $active_primary_shard_thresholds);
-validate_thresholds(undef, undef, $options_upper, "active shards",          $active_shard_thresholds);
+validate_thresholds(undef, undef, $options_lower, "active primary shards",  $active_primary_shard_thresholds);
+validate_thresholds(undef, undef, $options_lower, "active shards",          $active_shard_thresholds);
 validate_thresholds(undef, undef, $options_upper, "relocating shards",      $relocating_shard_thresholds);
 validate_thresholds(undef, undef, $options_upper, "initializing shards",    $initializing_shard_thresholds);
 validate_thresholds(undef,     1, $options_upper, "unassigned shards",      $unassigned_shard_thresholds);
@@ -85,33 +85,42 @@ check_string($cluster_name, $cluster);
 
 $msg .= check_elasticsearch_status(get_field("status"));
 
+my $msg2 = "";
+
 my $nodes = get_field_int("number_of_nodes");
 $msg .= ", nodes: $nodes";
 check_thresholds($nodes, 0, "nodes");
+$msg2 .= " 'nodes'=$nodes" . msg_perf_thresholds(1, 1, "nodes");
 
 my $data_nodes = get_field_int("number_of_data_nodes");
 $msg .= ", data nodes: $data_nodes";
 check_thresholds($data_nodes, 0, "data nodes");
+$msg2 .= " 'data nodes'=$data_nodes" . msg_perf_thresholds(1, 1, "data nodes");
 
 my $active_primary_shards = get_field_int("active_primary_shards");
 $msg .= ", active primary shards: $active_primary_shards";
 check_thresholds($active_primary_shards, 0, "active primary shards");
+$msg2 .= " 'active primary shards'=$active_primary_shards" . msg_perf_thresholds(1, 1, "active primary shards");
 
 my $active_shards = get_field_int("active_shards");
 $msg .= ", active shards: $active_shards";
 check_thresholds($active_shards, 0, "active shards");
+$msg2 .= " 'active shards'=$active_shards" . msg_perf_thresholds(1, 1, "active shards");
 
 my $relocating_shards = get_field_int("relocating_shards");
 $msg .= ", relocating shards: $relocating_shards";
 check_thresholds($relocating_shards, 0, "relocating shards");
+$msg2 .= " 'relocating shards'=$relocating_shards" . msg_perf_thresholds(1, 0, "relocating shards");
 
 my $initializing_shards = get_field_int("initializing_shards");
 $msg .= ", inititializing shards: $initializing_shards";
 check_thresholds($initializing_shards, 0, "initializing shards");
+$msg2 .= " 'initializing shards'=$initializing_shards" . msg_perf_thresholds(1, 0, "initializing shards");
 
 my $unassigned_shards = get_field_int("unassigned_shards");
 $msg .= ", unassigned shards: $unassigned_shards";
 check_thresholds($unassigned_shards, 0, "unassigned shards");
+$msg2 .= " 'unassigned shards'=$unassigned_shards" . msg_perf_thresholds(1, 0, "unassigned shards");
 
 my $timed_out = get_field("timed_out");
 #$timed_out = ( $timed_out ? "true" : "false" );
@@ -120,6 +129,8 @@ if($timed_out){
     $msg .= ", TIMED OUT: TRUE";
     #check_string($timed_out, "false");
 }
+
+$msg .= " |$msg2";
 
 vlog2;
 quit $status, $msg;
