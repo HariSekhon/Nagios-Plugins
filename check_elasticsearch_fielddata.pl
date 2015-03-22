cbaddf7b3e669bf681cd2d920030f737de6352d2
Hari Sekhon <harisekhon@gmail.com>
2015-03-22 21:48:52 +0000

    added support for specifying node hostname and output hostname instead of IP

--- check_elasticsearch_fielddata.pl
+++ check_elasticsearch_fielddata.pl
@@ -16,7 +16,7 @@ Optional --warning/--critical threshold ranges may be applied to the volume in b
 
 Tested on Elasticsearch 1.2.1 and 1.4.4";
 
-$VERSION = "0.1";
+$VERSION = "0.2";
 
 use strict;
 use warnings;
@@ -29,22 +29,26 @@ use HariSekhon::Elasticsearch;
 
 $ua->agent("Hari Sekhon $progname version $main::VERSION");
 
-my $node_ip;
-my $list_node_ips;
+my $node;
+my $list_nodes;
 
 %options = (
     %hostoptions,
-    "I|node-ip=s"   => [ \$node_ip,       "IP address of node for which to check fielddata volume" ],
-    "list-node-ips" => [ \$list_node_ips, "List node IPs" ],
+    "N|node=s"    => [ \$node,       "Node hostname or IP address of node for which to check fielddata volume" ],
+    "list-nodes"  => [ \$list_nodes, "List nodes" ],
     %thresholdoptions,
 );
-push(@usage_order, qw/node-ip list-node-ips/);
+push(@usage_order, qw/node list-nodes/);
 
 get_options();
 
 $host  = validate_host($host);
 $port  = validate_port($port);
-$node_ip = validate_ip($node_ip, "node") unless $list_node_ips;
+unless($list_nodes){
+    defined($node) or usage "node not defined, see --list-nodes to see which nodes are available to specify";
+    $node = (isIP($node) or isHostname($node)) or usage "invalid node specified, must be IP or hostname of node according to --list-nodes";
+    vlog_options "node", $node;
+}
 validate_thresholds(0, 0);
 
 vlog2;
@@ -52,34 +56,39 @@ set_timeout();
 
 $status = "OK";
 
-my $url = "/_cat/fielddata?h=ip,total&bytes=b";
+my $url = "/_cat/fielddata?h=host,ip,total&bytes=b";
 my $content = curl_elasticsearch_raw $url;
 
 my @parts;
+my $node_hostname;
 my $ip;
 my $bytes;
-print "Node IPs:\n\n" if $list_node_ips;
+print "Nodes:\n\n" if $list_nodes;
 foreach(split(/\n/, $content)){
     @parts = split(/\s+/, $content);
-    defined($parts[1]) or quit "UNKNOWN", "failed to find 2nd field in result. $nagios_plugins_support_msg_api";
-    isIP($parts[0]) or quit "UNKNOWN", "returned non-IP for 1st field in result. $nagios_plugins_support_msg_api";
-    $ip = $parts[0];
-    if($list_node_ips){
-        print "$ip\n";
+    defined($parts[2]) or quit "UNKNOWN", "failed to find 3rd field in result. $nagios_plugins_support_msg_api";
+    isIP($parts[1]) or quit "UNKNOWN", "returned non-IP for 1st field in result. $nagios_plugins_support_msg_api";
+    $ip = $parts[1];
+    isHostname($parts[0]) or quit "UNKNOWN", "invalid hostname returned for 1st field in result. $nagios_plugins_support_msg_api";
+    $node_hostname = $parts[0];
+    if($list_nodes){
+        print "$node_hostname\t$ip\n";
         next;
     }
-    if($ip eq $node_ip){
-        isInt($parts[1]) or quit "UNKNOWN", "returned non-integer for 2nd field in result. $nagios_plugins_support_msg_api";
-        $bytes = $parts[1];
+    if($node eq $ip or $node eq $node_hostname){
+        isInt($parts[2]) or quit "UNKNOWN", "returned non-integer for 2nd field in result. $nagios_plugins_support_msg_api";
+        $bytes = $parts[2];
     }
 }
-exit $ERRORS{"UNKNOWN"} if($list_node_ips);
+exit $ERRORS{"UNKNOWN"} if($list_nodes);
 
-$ip eq $node_ip or quit "UNKNOWN", "failed to find node with IP '$node_ip' in result from Elasticsearch";
+($node eq $ip or $node eq $node_hostname) or quit "UNKNOWN", "failed to find node '$node' in result from Elasticsearch";
 
 $msg = "elasticsearch ";
 
-$msg .= "total fielddata on node '$node_ip' = " . human_units($bytes);
+$msg .= "total fielddata on node '$node_hostname'";
+$msg .= " ($ip)" if $verbose;
+$msg .= " = " . human_units($bytes);
 check_thresholds($bytes);
 
 $msg .= " | total_fielddata=$bytes";
