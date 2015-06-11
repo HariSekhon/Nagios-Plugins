48c3c8a8ca75d3f14f6179e5d8c2ec70d62df170
Hari Sekhon <harisekhon@gmail.com>
2015-06-11 13:12:28 +0000

    updated tests/travis.sh

--- tests/travis.sh
+++ tests/travis.sh
@@ -211,7 +211,9 @@ echo "
 # ============================================================================ #
 "
 
-curl -XPUT localhost:8098/types/myType/buckets/myBucket/keys/myKey -d 'hari'
+# don't use new bucket types yet
+#curl -XPUT localhost:8098/types/myType/buckets/myBucket/keys/myKey -d 'hari'
+curl -XPUT localhost:8098/buckets/myBucket/keys/myKey -d 'hari'
 # RIAK_HOST obtained via .travis.yml
 # needs sudo
 #perl -T $I_lib ./check_riak_diag.pl
