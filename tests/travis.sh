80c8671c7401a9a2b03f1f30cb92a97249b1d735
Hari Sekhon <harisekhon@gmail.com>
2015-06-11 10:33:41 +0000

    tried adding jars from lib to CLASSPATH

--- tests/travis.sh
+++ tests/travis.sh
@@ -51,6 +51,9 @@ ps -ef|grep -i cassandra
 # CASSANDRA_HOST and CASSANDRA_CONF obtained via .travis.yml
 export CASSANDRA_CONF="${CASSANDRA_CONF:-/usr/local/cassandra/conf}"
 export CLASSPATH="${CLASSPATH:-.}" # workaround for nodetool "You must set the CASSANDRA_CONF and CLASSPATH vars"
+for x in /usr/local/cassandra/lib/*.jar; do
+    export CLASSPATH="CLASSPATH:$x"
+done
 perl -T $I_lib ./check_cassandra_balance.pl
 hr
 perl -T $I_lib ./check_cassandra_heap.pl -vvv
