3fbe434941c5c28e33d049b36d1ed3cc7404b9f8
Hari Sekhon <harisekhon@gmail.com>
2015-06-12 16:05:03 +0000

    added recursion of stats for disks and connected nodes etc

--- check_riak_stats.pl
+++ check_riak_stats.pl
@@ -9,16 +9,20 @@
 #  License: see accompanying LICENSE file
 #  
 
-$DESCRIPTION = "Nagios Plugin to check Riak's /stats metrics
+$DESCRIPTION = "Nagios Plugin to check Riak's stats and other settings exposed via the Basho Stats API such as Riak component versions
+
+This is even how check_riak_version.pl is implemented through this plugin for Riak core, and you can explicitly query other components via this plugin, use --all -vv to get a convenient listing one per line of all the available stats.
 
 Checks:
 
 1. fetches one or more stats
 2. checks stat's value against expected regex (optional)
 3. checks stat's value against warning/critical range thresholds (optional)
-   raises warning/critical if the value is outside thresholds or not a floating point number";
+   raises warning/critical if the value is outside thresholds or not a floating point number
 
-$VERSION = "0.7.1";
+Tested on Riak 1.x, 2.0.0, 2.1.1";
+
+$VERSION = "0.8.0";
 
 use strict;
 use warnings;
@@ -27,6 +31,7 @@ BEGIN {
     use lib dirname(__FILE__) . "/lib";
 }
 use HariSekhonUtils;
+use Data::Dumper;
 use LWP::Simple '$ua';
 use JSON::XS;
 
@@ -35,42 +40,48 @@ set_port_default(8098);
 env_creds("Riak");
 
 my $metrics;
-my $all_metrics;
+my $all_stats;
 my $expected;
 
 %options = (
     %hostoptions,
-    "m|metrics=s"         => [ \$metrics,           "Metric(s) to collect, comma separated. Output in the order specified for convenience. Optional thresholds will only be applied when a single metrics is given" ],
-    "a|all-metrics"       => [ \$all_metrics,       "Grab all metrics. Useful if you don't know what to monitor yet or just want to graph everything" ],
-    "e|expected=s"        => [ \$expected,          "Expected regex for metric if one metric is given. Checked before range thresholds. Optional" ],
+    "s|stat=s"            => [ \$metrics,           "Stat(s) to collect, comma separated. Output in the order specified for convenience. Optional thresholds will only be applied when a single stat is specified" ],
+    "a|all"               => [ \$all_stats,         "Grab all stats. Useful if you don't know what to monitor yet or just want to graph everything" ],
+    "e|expected=s"        => [ \$expected,          "Expected regex for stat if one stat is specified. Checked before range thresholds. Optional" ],
     "w|warning=s"         => [ \$warning,           "Warning  threshold or ran:ge (inclusive). Optional" ],
     "c|critical=s"        => [ \$critical,          "Critical threshold or ran:ge (inclusive). Optional" ],
 );
+@usage_order = qw/host port stat all expected warning critical/;
 
-@usage_order = qw/host port metrics all-metrics expected warning critical/;
+if($progname =~ /version/){
+    delete $options{"s|stat=s"};
+    delete $options{"a|all"};
+    delete $options{"w|warning=s"};
+    delete $options{"c|critical=s"};
+    $metrics = "riak_core_version";
+}
 get_options();
 
 $host       = validate_host($host);
-$host       = validate_resolvable($host);
 $port       = validate_port($port);
-my $url     = "http://$host:$port/stats";
 my %stats;
 my @stats;
-if($all_metrics){
-    defined($metrics) and usage "cannot specify --all-metrics and specific --metrics at the same time!";
+if($all_stats){
+    defined($metrics) and usage "cannot specify --all and specific --stats at the same time!";
 } else {
-    defined($metrics) or usage "no metrics specified";
+    defined($metrics) or usage "no stat(s) specified";
     foreach my $metric (split(/\s*[,\s]\s*/, $metrics)){
-        $metric =~ /^[A-Z]+[\w:]*[A-Z0-9]+$/i or usage "invalid metrics '$metric' given, must be alphanumeric, may contain underscores and colons in middle";
+        $metric =~ /^[A-Z]+[\w\.:]*[A-Z0-9]+$/i or usage "invalid stats '$metric' given, must be alphanumeric, may contain underscores, dots and colons in middle";
         grep(/^$metric$/, @stats) or push(@stats, $metric);
+        vlog_options "stat", $metric;
     }
-    @stats or usage "no valid metrics specified";
+    @stats or usage "no valid stats specified";
 }
 if(defined($expected)){
-    ($all_metrics or scalar @stats != 1) and usage "can only specify expected value when giving a single stat";
+    ($all_stats or scalar @stats != 1) and usage "can only specify expected value when giving a single stat";
     $expected = validate_regex($expected);
 }
-if(($all_metrics or scalar @stats != 1) and ($warning or $critical)){
+if(($all_stats or scalar @stats != 1) and ($warning or $critical)){
     usage "can only specify warning/critical thresholds when giving a single stat";
 }
 validate_thresholds(undef, undef, { "simple" => "upper", "positive" => 0, "integer" => 0 } );
@@ -83,50 +94,59 @@ $ua->show_progress(1) if $debug;
 
 $status = "OK";
 
+$host = validate_resolvable($host);
+my $url = "http://$host:$port/stats";
+
 # ============================================================================ #
-# lifted from my check_hadoop_jobtracker.pl plugin, modified to support $all_metrics
-my $content = curl $url,"Riak node $host";
+my $content = curl $url, "Riak node $host";
 
 sub check_stats_parsed(){
-    if($all_metrics){
+    if($all_stats){
+        vlog2 "checking all stats found at least 10 stats";
         if(scalar keys %stats < 10){
             quit "UNKNOWN", "<10 stats collected from /stats page, this must be an error, try running with -vvv to see what the deal is";
         }
-        foreach(sort keys %stats){
-            vlog2 "stats $_ = $stats{$_}";
-        }
+        #foreach(sort keys %stats){
+        #    vlog2 "stats $_ = '$stats{$_}'";
+        #}
     } else {
-        foreach(@stats){
-            unless(defined($stats{$_})){
+        foreach my $requested_stat (@stats){
+            vlog2 "checking $requested_stat was found";
+            unless(defined($stats{$requested_stat})){
                 vlog2;
-                quit "UNKNOWN", "failed to find $_ in output from '$host:$port'";
+                my $err = "failed to find $requested_stat in output from '$host:$port'";
+                if(grep { $_ =~ /^\Q$requested_stat\E\./ } keys %stats){
+                    $err .= ", but subkey(s) for $requested_stat were found, try -vv to see what was collected to specify a subnode if it's an array or hash";
+                }
+                quit "UNKNOWN", $err;
             }
-            vlog2 "stats $_ = $stats{$_}";
+            #vlog2 "stat found $requested_stat = '$stats{$requested_stat}'";
         }
     }
     vlog2;
 }
 
-# TODO: could improve to make this recursive for embedded arrays / hashes but not needed right now
-sub riakStatToString($){
-    my $var = $_[0];
+# To check prototype before calling recursively
+sub processStat($$);
+sub processStat($$){
+    my $name = shift;
+    my $var  = shift;
+    vlog3 "processing $name";
     if(isArray($var)){
-        return "[" . join(",", @{$var}) . "]";
-    } elsif(isHash($var)){
-        my $str = "{";
-        foreach(sort keys %{$var}){
-            $str .= "$_=$var->$_,";
+        if(scalar @{$var} > 0){
+            foreach(my $i=0; $i < scalar @{$var}; $i++){
+                processStat("$name.$i", $$var[$i]);
+            }
+        } else {
+            processStat("$name.0", "");
         }
-        $str =~ s/,$/}/;
-        return $str;
-    # TODO: for some reason values such as 1.0.3 from basho_stats_version are not determined by ref used in isScalar so having to assume Scalar instead
-    #} elsif(isScalar($var)) {
-    #    return $var;
-    #} else {
-    #    quit "UNKNOWN", "could not determine if var '$var' is array, hash or scalar in riakStatToString()";
-    #}
+    } elsif(isHash($var)){
+        foreach my $key (keys %{$var}){
+            processStat("$name.$key", $$var{$key});
+        }
     } else {
-        return $var;
+        vlog2 "collected stat  $name='$var'";
+        $stats{$name} = $var;
     }
 }
 
@@ -140,17 +160,18 @@ sub parse_stats(){
         quit "CRITICAL", "invalid json returned by '$host:$port'";
     };
     if($debug){
-        use Data::Dumper;
         print Dumper($json);
     }
     foreach my $stat (sort keys %{$json}){
         # This can be a float, a string, an array etc
-        if($all_metrics){
-            $stats{$stat} = riakStatToString($json->{$stat});
+        if($all_stats){
+            processStat($stat, $json->{$stat});
         } else {
             foreach my $metric (@stats){
-                if($metric eq $stat){
-                    $stats{$metric} = riakStatToString($json->{$stat});
+                my $metric2 = $metric;
+                $metric2 =~ s/\..*$//;
+                if($metric2 eq $stat){
+                    processStat($stat, $json->{$stat});
                     last;
                 }
             }
@@ -160,11 +181,11 @@ sub parse_stats(){
 }
 # ============================================================================ #
 
-vlog2 "parsing metrics from '$url'";
+vlog2 "parsing stats from '$url'";
 parse_stats();
 
 $msg = "";
-if($all_metrics){
+if($all_stats){
     foreach(sort keys %stats){
         $msg .= "$_=$stats{$_} ";
     }
@@ -175,7 +196,7 @@ if($all_metrics){
 }
 
 my $value;
-if($all_metrics){
+if($all_stats){
     my $metrics_found=0;
     foreach(sort keys %stats){
         isFloat($stats{$_}) or next;
@@ -186,7 +207,7 @@ if($all_metrics){
         isFloat($stats{$_}) or next;
         $msg .= "$_=$stats{$_} ";
     }
-} elsif(!$all_metrics and scalar @stats == 1){
+} elsif(!$all_stats and scalar @stats == 1){
     $msg =~ s/ $//;
     $value = $stats{$stats[0]};
     if(defined($expected)){
@@ -211,11 +232,13 @@ if($all_metrics){
     }
     my ($threshold_ok, $threshold_msg);
     if($isFloat){
-        ($threshold_ok, $threshold_msg) = check_thresholds($value, 1);
-        if(!$threshold_ok){
-            $msg .= " $threshold_msg";
-        }
-        $msg .= " | $stats[0]=$value" . msg_perf_thresholds(1);
+        #($threshold_ok, $threshold_msg) = check_thresholds($value, 1);
+        #if(!$threshold_ok){
+        #    $msg .= " $threshold_msg";
+        #}
+        check_thresholds($value);
+        $msg .= " | $stats[0]=$value";
+        msg_perf_thresholds();
     }
 } else {
     my $metrics_found=0;
