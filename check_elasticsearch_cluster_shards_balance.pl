ad8b464e78468477e7fd5f6c907f3597384b01e5
Hari Sekhon <harisekhon@gmail.com>
2015-07-03 19:23:36 +0100

    updated check_elasticsearch_cluster_shards_balance.pl

--- check_elasticsearch_cluster_shards_balance.pl
+++ check_elasticsearch_cluster_shards_balance.pl
@@ -51,8 +51,6 @@ set_timeout();
 
 $status = "OK";
 
-list_elasticsearch_nodes();
-
 # This looks like it's fields might have changed in 1.6
 my $url = "/_cat/allocation?h=shards,host,ip,node";
 $url .= "&v" if $verbose > 2;
