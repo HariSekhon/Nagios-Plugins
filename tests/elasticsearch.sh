978ca311894fbd6090c5b495c64288f36078a66d
Hari Sekhon <harisekhon@gmail.com>
2015-10-02 09:39:58 +0100

    setting thresholds higher as occasionally travis tests run on legacy infrastructure which alerts on disk 85% full, breaking the build

--- tests/elasticsearch.sh
+++ tests/elasticsearch.sh
@@ -86,7 +86,7 @@ perl -T $I_lib ./check_elasticsearch_master_node.pl -v
 hr
 perl -T $I_lib ./check_elasticsearch_nodes.pl -v -w 1
 hr
-perl -T $I_lib ./check_elasticsearch_node_disk_percent.pl -N 127.0.0.1 -v
+perl -T $I_lib ./check_elasticsearch_node_disk_percent.pl -N 127.0.0.1 -v -w 90 -c 95
 hr
 perl -T $I_lib ./check_elasticsearch_node_shards.pl -N 127.0.0.1 -v
 hr
