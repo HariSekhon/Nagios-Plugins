233b347766b303b22d8fcebd15ed607438a5e814
Hari Sekhon <harisekhon@gmail.com>
2015-12-14 02:38:38 +0000

    added syntax.sh

---
+++ tests/syntax.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-05-25 01:38:24 +0100 (Mon, 25 May 2015)
+#
+#  https://github.com/harisekhon/nagios-plugins
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  http://www.linkedin.com/in/harisekhon
+#
+
+set -eu
+srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "================================================================================"
+echo "                              Syntax Checks"
+echo "================================================================================"
+
+cd "$srcdir/..";
+
+. ./tests/utils.sh
+
+for x in *.pl */*.pl; do
+    isExcluded "$x" && continue
+    #printf "%-50s" "$x:"
+    #$perl -TWc $I_lib ./$x
+    $perl -Tc $I_lib ./$x
+done
+echo "================================================================================"
+echo "                  All Perl programs passed syntax check"
+echo "================================================================================"
+echo
+echo
