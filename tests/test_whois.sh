15b658d4e7c3ee978ae6653e4c84830465de1c07
Hari Sekhon <harisekhon@gmail.com>
2016-01-23 11:09:10 +0000

    increased whois timeouts as they often fail from Travis

--- tests/test_whois.sh
+++ tests/test_whois.sh
@@ -195,7 +195,7 @@ for domain in $domains; do
     printf "%-20s  " "$domain:"
     # don't want people with 25 days left on their domains raising errors here, setting thresholds lower to always pass
     set +eo pipefail
-    output=`$perl -T $I_lib ./check_whois.pl -d $domain -w 10 -c 2 -vvv`
+    output=`$perl -T $I_lib ./check_whois.pl -d $domain -w 10 -c 2 -t 20 -vvv`
     result=$?
     echo "$output"
     if [ $result -ne 0 -a $result -eq 3 ]; then
@@ -209,7 +209,7 @@ done
 #echo "Testing Domains excluding expiry:"
 #for domain in $domains_noexpiry; do
 #    set +eo pipefail
-#    output=`$perl -T $I_lib ./check_whois.pl -d $domain -w 10 -c 2 --no-expiry`
+#    output=`$perl -T $I_lib ./check_whois.pl -d $domain -w 10 -c 2 -t 20 --no-expiry`
 #    result=$?
 #    echo "$output"
 #    if [ $result -ne 0 -a $result -eq 3 ]; then
@@ -223,7 +223,7 @@ echo "Testing Domains excluding nameservers:"
 for domain in $domains_no_nameservers; do
     [ "$(($RANDOM % 20))" = 0 ] || continue
     set +eo pipefail
-    output=`$perl -T $I_lib ./check_whois.pl -d $domain -w 10 -c 2 --no-nameservers -vvv`
+    output=`$perl -T $I_lib ./check_whois.pl -d $domain -w 10 -c 2 --no-nameservers -t 20 -vvv`
     result=$?
     echo "$output"
     if [ $result -ne 0 -a $result -eq 3 ]; then
