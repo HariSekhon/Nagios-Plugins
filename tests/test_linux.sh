2820e6414dfbbbb61ad04bdf47a696f1391b52c4
Hari Sekhon <harisekhon@gmail.com>
2016-02-12 23:15:25 +0000

    updated test_linux.sh

--- tests/test_linux.sh
+++ tests/test_linux.sh
@@ -21,13 +21,74 @@ cd "$srcdir/..";
 
 . ./tests/utils.sh
 
-[ `uname -s` = "Linux" ] || exit 0
+#[ `uname -s` = "Linux" ] || exit 0
 
 echo "
 # ============================================================================ #
 #                                   L i n u x
 # ============================================================================ #
 "
+
+export DOCKER_CONTAINER="nagios-plugins"
+
+export MNTDIR="/tmp/nagios-plugins"
+
+if ! is_docker_available; then
+    echo 'WARNING: Docker not found, skipping Linux checks!!!'
+    exit 0
+fi
+
+docker_run_test(){
+    local cmd="$@"
+    docker exec "$DOCKER_CONTAINER" $MNTDIR/$*
+}
+
+#trap "docker rm -f $DOCKER_CONTAINER &>/dev/null" SIGINT SIGTERM EXIT
+
+echo "Setting up test Linux container"
+if ! docker ps | tee /dev/stderr | grep -q "[[:space:]]$DOCKER_CONTAINER$"; then
+    echo "Starting Docker Linux test container"
+    docker run -d --name "$DOCKER_CONTAINER" -v "$srcdir/..":"$MNTDIR" harisekhon/nagios-plugins tail -f /dev/null
+    docker exec "$DOCKER_CONTAINER" yum makecache fast
+    docker exec "$DOCKER_CONTAINER" yum install -y net-tools
+else
+        echo "Docker Linux test container already running"
+fi
+
+docker_run_test check_linux_auth.pl -u root -g root -v
+hr
+docker_run_test check_linux_context_switches.pl || : ; sleep 1; docker_run_test check_linux_context_switches.pl -w 10000 -c 50000
+hr
+docker_run_test check_linux_duplicate_IDs.pl
+hr
+#docker_run_test check_linux_interface.pl -i eth0 -v -e -d Full
+hr
+docker_run_test check_linux_load_normalized.pl
+hr
+docker_run_test check_linux_ram.py -v -w 20% -c 10%
+hr
+docker_run_test check_linux_system_file_descriptors.pl
+hr
+docker_run_test check_linux_timezone.pl -T UTC -Z /usr/share/zoneinfo/UTC -A UTC -v
+hr
+docker_run_test check_yum.pl -C
+hr
+docker_run_test check_yum.pl -C --all-updates || :
+hr
+docker_run_test check_yum.py -C
+hr
+docker_run_test check_yum.py -C --all-updates || :
+hr
+echo
+echo -n "Deleting container "
+docker rm -f "$DOCKER_CONTAINER"
+echo; echo
+
+# ============================================================================ #
+#                                     E N D
+# ============================================================================ #
+# old local checks don't run on Mac
+exit 0
 
 $perl -T $I_lib ./check_linux_timezone.pl -T UTC -Z /usr/share/zoneinfo/UTC -A UTC
 hr
