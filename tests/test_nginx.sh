3fdc3d48e544144205e129d05c76a80c1764a96b
Hari Sekhon <harisekhon@gmail.com>
2016-02-13 21:40:28 +0000

    added tests/test_nginx.sh, tests/nginx.conf.d.default.conf

---
+++ tests/test_nginx.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-05-25 01:38:24 +0100 (Mon, 25 May 2015)
+#
+#  https://github.com/harisekhon/nagios-plugins
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -eu
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$srcdir/..";
+
+. ./tests/utils.sh
+
+echo "
+# ============================================================================ #
+#                                   N g i n x
+# ============================================================================ #
+"
+
+export NGINX_HOST="${NGINX_HOST:-${HOST:-localhost}}"
+
+export DOCKER_CONTAINER="nagios-plugins-nginx"
+
+if ! is_docker_available; then
+    echo 'WARNING: Docker not found, skipping Nginx checks!!!'
+    exit 0
+fi
+
+echo "Setting up test Nginx container"
+if ! docker ps | tee /dev/stderr | grep -q "[[:space:]]$DOCKER_CONTAINER$"; then
+    docker rm -f "$DOCKER_CONTAINER" &>/dev/null || :
+    echo "Starting Docker Nginx test container"
+    docker create --name "$DOCKER_CONTAINER" -p 80:80 nginx
+    docker cp "$srcdir/nginx.conf.d.default.conf" "$DOCKER_CONTAINER":/etc/nginx/conf.d/default.conf
+    docker start "$DOCKER_CONTAINER"
+    sleep 1
+else
+    echo "Docker Nginx test container already running"
+fi
+
+hr
+$perl -T $I_lib ./check_nginx_stats.pl -H "$NGINX_HOST" -u /status
+hr
+echo
+echo -n "Deleting container "
+docker rm -f "$DOCKER_CONTAINER"
+echo; echo
