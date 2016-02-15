0b66c609d05c06b3396581bd65aa5bb320f10fb1
Hari Sekhon <harisekhon@gmail.com>
2016-02-15 00:34:07 +0000

    updated check_tachyon.py

--- check_tachyon.py
+++ check_tachyon.py
@@ -23,6 +23,8 @@ Queries the WebUI and displays the version and uptime
 Optional --warn-on-recent-start raises WARNING if started within the last 30 mins in order to catch crashes that may
 have been restarted by a supervisor process
 
+Tested on Tachyon 0.8.2
+
 """
 
 from __future__ import absolute_import
