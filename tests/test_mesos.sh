477f42e3b3699e9f6dc8ee4b354bf7a7841346f5
Hari Sekhon <harisekhon@gmail.com>
2016-02-29 11:13:13 +0000

    Update test_mesos.sh

--- tests/test_mesos.sh
+++ tests/test_mesos.sh
@@ -81,7 +81,7 @@ $perl -T $I_lib ./check_mesos_metrics.pl -P 5051 -v
 hr
 $perl -T $I_lib ./check_mesos_master_metrics.pl -v
 hr
-$perl -T $I_lib ./check_mesos_slave.py -v -s "$(./check_mesos_slave.py -l | awk '/=/{print $1}')"
+./check_mesos_slave.py -v -s "$(./check_mesos_slave.py -l | awk '/=/{print $1}')"
 hr
 $perl -T $I_lib ./check_mesos_slave_metrics.pl -v
 hr
