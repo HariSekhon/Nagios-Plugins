b3ab1f42100881c8865211115eb2c0979f3c9600
Hari Sekhon <harisekhon@gmail.com>
2016-03-10 18:22:07 +0000

    updated check_consul_write.py

--- check_consul_write.py
+++ check_consul_write.py
@@ -31,16 +31,15 @@ from __future__ import print_function
 import os
 import sys
 import traceback
-try:
-    import requests
-except ImportError as _:
-    print(traceback.format_exc(), end='')
-    sys.exit(4)
+# try:
+#     import requests
+# except ImportError as _:
+#     print(traceback.format_exc(), end='')
+#     sys.exit(4)
 libdir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'pylib'))
 sys.path.append(libdir)
 try:
     # pylint: disable=wrong-import-position
-    from harisekhon.utils import log, isStr, CriticalError
     from harisekhon.nagiosplugin import KeyWriteNagiosPlugin
     from harisekhon import RequestHandler
     from check_consul_key import ConsulKeyCheck
@@ -58,13 +57,13 @@ class ConsulWriteCheck(ConsulKeyCheck, KeyWriteNagiosPlugin):
         url = 'http://%(host)s:%(port)s/v1/kv/%(key)s' % self.__dict__
         RequestHandler.check_response_code = \
             self.check_response_code("failed to write Consul key '{0}'".format(self.key))
-        req = RequestHandler.put(url, self._write_value)
+        RequestHandler.put(url, self._write_value)
 
     def delete(self):
         url = 'http://%(host)s:%(port)s/v1/kv/%(key)s' % self.__dict__
         RequestHandler.check_response_code = \
             self.check_response_code("failed to delete Consul key '{0}'".format(self.key))
-        req = RequestHandler.delete(url)
+        RequestHandler.delete(url)
 
 
 if __name__ == '__main__':
