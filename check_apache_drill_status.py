669104a0f921030928af0ed0fab5343771a154c1
Hari Sekhon <harisekhon@gmail.com>
2016-03-20 22:41:30 +0000

    integrated with StatusNagiosPlugin class

--- check_apache_drill_status.py
+++ check_apache_drill_status.py
@@ -24,14 +24,13 @@ from __future__ import division
 from __future__ import print_function
 #from __future__ import unicode_literals
 
-#import logging
+import logging
 import os
 import re
 import sys
 import traceback
 try:
     from bs4 import BeautifulSoup
-    import requests
 except ImportError:
     print(traceback.format_exc(), end='')
     sys.exit(4)
@@ -40,56 +39,47 @@ sys.path.append(libdir)
 try:
     # pylint: disable=wrong-import-position
     from harisekhon.utils import log, qquit
-    from harisekhon.utils import validate_host, validate_port, support_msg
-    from harisekhon import NagiosPlugin
+    from harisekhon.utils import support_msg
+    from harisekhon import StatusNagiosPlugin
+    from harisekhon import RequestHandler
 except ImportError as _:
     print(traceback.format_exc(), end='')
     sys.exit(4)
 
 __author__ = 'Hari Sekhon'
-__version__ = '0.1'
+__version__ = '0.2'
 
 
-class CheckApacheDrillStatus(NagiosPlugin):
+class CheckApacheDrillStatus(StatusNagiosPlugin):
 
     def __init__(self):
         # Python 2.x
         super(CheckApacheDrillStatus, self).__init__()
         # Python 3.x
         # super().__init__()
+        self.name = 'Apache Drill'
+        self.default_port = 8047
 
-    def add_options(self):
-        self.add_hostoption(name='Apache Drill', default_host='localhost', default_port=8047)
+    def get_status(self):
+        url = 'http://{0}:{1}/status'.format(self.host, self.port)
+        req = RequestHandler().get(url)
+        status = self.parse(req)
+        return status
 
-    def run(self):
-        self.no_args()
-        host = self.get_opt('host')
-        port = self.get_opt('port')
-        validate_host(host)
-        validate_port(port)
-
-        url = 'http://%(host)s:%(port)s/status' % locals()
-        log.debug('GET %s' % url)
-        try:
-            req = requests.get(url)
-        except requests.exceptions.RequestException as _:
-            qquit('CRITICAL', _)
-        log.debug("response: %s %s" % (req.status_code, req.reason))
-        log.debug("content:\n{0}\n{1}\n{2}".format('='*80, req.content.strip(), '='*80))
-        if req.status_code != 200:
-            qquit('CRITICAL', "%s %s" % (req.status_code, req.reason))
+    def parse(self, req):
         soup = BeautifulSoup(req.content, 'html.parser')
-        #if log.isEnabledFor(logging.DEBUG):
-        #     log.debug("BeautifulSoup prettified:\n{0}\n{1}".format(soup.prettify(), '='*80))
+        # if log.isEnabledFor(logging.DEBUG):
+        #     log.debug("BeautifulSoup prettified:\n%s\n%s", soup.prettify(), '='*80)
+        status = None
         try:
-            status = soup.find('div', { 'class': 'alert alert-success'}).get_text().strip()
+            status = soup.find('div', {'class': 'alert alert-success'}).get_text().strip()
         except (AttributeError, TypeError):
             qquit('UNKNOWN', 'failed to parse Apache Drill status page. %s' % support_msg())
-        self.msg = "Apache Drill status = '{0}'".format(status)
         if re.match('Running!', status):
             self.ok()
         else:
             self.critical()
+        return status
 
 
 if __name__ == '__main__':
