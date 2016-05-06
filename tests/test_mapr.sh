6446a9f9d395d32751df7d99d3c24fff66522d72
Hari Sekhon <harisekhon@gmail.com>
2016-05-06 15:20:42 +0100

    added test_mapr.sh

---
+++ tests/test_mapr.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2016-05-06 12:12:15 +0100 (Fri, 06 May 2016)
+#
+#  https://github.com/harisekhon/nagios-plugins
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$srcdir/.."
+
+. "$srcdir/utils.sh"
+
+echo "
+# ============================================================================ #
+#                                    M a p R
+# ============================================================================ #
+"
+
+if [ -z "$MAPR_HOST" ]; then
+    echo "WARNING: MapR host not detected, skipping MapR checks"
+    exit 0
+fi
+
+hr
+# TODO: add checks
+#$perl -T $I_lib 
+#hr
+#$perl -T $I_lib 
+hr
+echo; echo
