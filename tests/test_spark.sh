162ec682bd03076fb0f46e55bcefc3914c383626
Hari Sekhon <harisekhon@gmail.com>
2016-05-09 16:16:59 +0100

    updated test_spark.sh

--- tests/test_spark.sh
+++ tests/test_spark.sh
@@ -4,7 +4,7 @@
 #  Author: Hari Sekhon
 #  Date: 2016-02-10 23:18:47 +0000 (Wed, 10 Feb 2016)
 #
-#  https://github.com/harisekhon
+#  https://github.com/harisekhon/nagios-plugins
 #
 #  License: see accompanying Hari Sekhon LICENSE file
 #
@@ -36,32 +36,15 @@ echo "using docker address '$SPARK_HOST'"
 export SPARK_MASTER_PORT="${SPARK_MASTER_PORT:-8080}"
 export SPARK_WORKER_PORT="${SPARK_WORKER_PORT:-8081}"
 
-export DOCKER_CONTAINER="nagios-plugins-spark"
+export DOCKER_IMAGE="harisekhon/spark"
+export DOCKER_CONTAINER="nagios-plugins-spark-test"
 
-if ! is_docker_available; then
-    echo 'WARNING: Docker not found, skipping spark checks!!!'
-    exit 0
-fi
-
-startupwait=10
-is_travis && let startupwait+=20
+startupwait=15
 
 hr
 echo "Setting up Spark test container"
 hr
-# reuse container it's faster
-#docker rm -f "$DOCKER_CONTAINER" &>/dev/null
-#sleep 1
-if ! docker ps | tee /dev/stderr | grep -q "[[:space:]]$DOCKER_CONTAINER$"; then
-    docker rm -f "$DOCKER_CONTAINER" &>/dev/null || :
-    echo "Starting Docker Spark test container"
-    # need tty for sudo which spark-start.sh local uses while ssh'ing localhost
-    docker run -d -t --name "$DOCKER_CONTAINER" -p $SPARK_MASTER_PORT:$SPARK_MASTER_PORT -p $SPARK_WORKER_PORT:$SPARK_WORKER_PORT harisekhon/spark
-    echo "waiting $startupwait seconds for Spark Master + Worker to become responsive..."
-    sleep $startupwait
-else
-    echo "Docker Spark test container already running"
-fi
+launch_container "$DOCKER_IMAGE" "$DOCKER_CONTAINER" $SPARK_MASTER_PORT $SPARK_WORKER_PORT
 
 hr
 $perl -T $I_lib ./check_spark_cluster.pl -c 1: -v
@@ -72,7 +55,4 @@ $perl -T $I_lib ./check_spark_cluster_memory.pl -w 80 -c 90 -v
 hr
 $perl -T $I_lib ./check_spark_worker.pl -w 80 -c 90 -v
 hr
-echo
-echo -n "Deleting container "
-docker rm -f "$DOCKER_CONTAINER"
-echo; echo
+delete_container
