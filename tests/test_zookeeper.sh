0ed4f1a898750c7ffbf7c8c20be1c71b25954669
Hari Sekhon <harisekhon@gmail.com>
2016-05-10 14:04:51 +0100

    updated tests/test_zookeeper.sh

--- tests/test_zookeeper.sh
+++ tests/test_zookeeper.sh
@@ -31,31 +31,6 @@ ZOOKEEPER_HOST="${DOCKER_HOST:-${ZOOKEEPER_HOST:-${HOST:-localhost}}}"
 ZOOKEEPER_HOST="${ZOOKEEPER_HOST##*/}"
 ZOOKEEPER_HOST="${ZOOKEEPER_HOST%%:*}"
 export ZOOKEEPER_HOST
-echo "using docker address '$ZOOKEEPER_HOST'"
-
-# XXX: make sure to keep this aligned with Makefile to pull down correct zookeeper version
-#ZOOKEEPER_VERSION=3.4.6
-#zookeeper="zookeeper-$ZOOKEEPER_VERSION"
-#TAR="$zookeeper.tgz"
-
-#if ! [ -e "$TAR" ]; then
-#    echo "fetching zookeeper tarball '$TAR'"
-#    wget "http://www.us.apache.org/dist/zookeeper/zookeeper-$zookeeper_VERSION/$TAR"
-#    echo
-#fi
-
-#if ! [ -d "$zookeeper" ]; then
-#    echo "unpacking zookeeper"
-#    tar zxf "$TAR"
-#    echo
-#fi
-
-#if [ -n "$zookeeper_built" -a -z "$(netstat -an | grep [:.]2181)" ]; then
-#    cp -vf "$zookeeper/conf/zoo_sample.cfg" "$zookeeper/conf/zoo.cfg"
-#
-#    "$zookeeper/bin/zkServer.sh" start &
-#    sleep 10
-#fi
 
 export DOCKER_IMAGE="harisekhon/zookeeper"
 export DOCKER_IMAGE2="harisekhon/nagios-plugins"
@@ -63,31 +38,21 @@ export DOCKER_CONTAINER="nagios-plugins-zookeeper"
 export DOCKER_CONTAINER2="nagios-plugins"
 export MNTDIR="/nagios-plugins-tmp"
 
-if ! is_docker_available; then
-    echo 'WARNING: Docker not found, skipping ZooKeeper checks!!!'
-    exit 0
-fi
-
 docker_run_test(){
     docker exec -ti "$DOCKER_CONTAINER2" $MNTDIR/$@
 }
 
-startupwait=5
-is_travis && let startupwait+=20
+startupwait=10
 
 echo "Setting up ZooKeeper test container"
-docker rm -f "$DOCKER_CONTAINER" &>/dev/null || :
-docker rm -f "$DOCKER_CONTAINER2" &>/dev/null || :
-echo "Starting Docker ZooKeeper test container"
-docker run -d --name "$DOCKER_CONTAINER" -p 2181:2181 -p 3181:3181 -p 4181:4181 "$DOCKER_IMAGE"
+launch_container "$DOCKER_IMAGE" "$DOCKER_CONTAINER" 2181 3181 4181
 docker cp "$DOCKER_CONTAINER":/zookeeper/conf/zoo.cfg .
 hr
 echo "Setting up nagios-plugins test container with zkperl library"
-docker run -d --name "$DOCKER_CONTAINER2" --link "$DOCKER_CONTAINER:zookeeper" -v "$PWD":"$MNTDIR" "$DOCKER_IMAGE2" tail -f /dev/null
+DOCKER_OPTS="--link $DOCKER_CONTAINER:zookeeper -v $PWD:$MNTDIR"
+DOCKER_CMD="tail -f /dev/null"
+launch_container "$DOCKER_IMAGE2" "$DOCKER_CONTAINER2"
 docker cp zoo.cfg "$DOCKER_CONTAINER2":"$MNTDIR/"
-hr
-echo "waiting $startupwait seconds for ZooKeeper to start up"
-sleep $startupwait
 hr
 $perl -T $I_lib ./check_zookeeper.pl -s -w 10 -c 20 -v
 hr
@@ -98,22 +63,5 @@ hr
 docker_run_test check_zookeeper_znode.pl -H zookeeper -P 2181 -z / -v -n --child-znodes
 hr
 
-#echo
-#hr
-#$perl -T $I_lib ./check_zookeeper.pl -s -w 10 -c 20 -v
-#hr
-#$perl -T $I_lib ./check_zookeeper_config.pl -C "$zookeeper/conf/zoo.cfg" -v
-#hr
-#if [ -n "$zookeeper_built" ]; then
-#    $perl -T $I_lib ./check_zookeeper_child_znodes.pl -z / --no-ephemeral-check -v
-#    hr
-#    $perl -T $I_lib ./check_zookeeper_znode.pl -z / -v -n --child-znodes
-#    hr
-#fi
-
-echo
-echo -n "Deleting container "
-docker rm -f "$DOCKER_CONTAINER"
-echo -n "Deleting container "
-docker rm -f "$DOCKER_CONTAINER2"
-echo; echo
+delete_container "$DOCKER_CONTAINER2"
+delete_container "$DOCKER_CONTAINER"
