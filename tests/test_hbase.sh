9ccae57514107aa99b783b4944478c0443777be8
Hari Sekhon <harisekhon@gmail.com>
2016-05-12 12:40:21 +0100

    updated test_hbase.sh

--- tests/test_hbase.sh
+++ tests/test_hbase.sh
@@ -16,11 +16,13 @@
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 
-srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+srcdir2="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd "$srcdir/.."
+cd "$srcdir2/.."
 
-. "$srcdir/utils.sh"
+. "$srcdir2/utils.sh"
+
+srcdir="$srcdir2"
 
 echo "
 # ============================================================================ #
@@ -32,6 +34,8 @@ HBASE_HOST="${DOCKER_HOST:-${HBASE_HOST:-${HOST:-localhost}}}"
 HBASE_HOST="${HBASE_HOST##*/}"
 HBASE_HOST="${HBASE_HOST%%:*}"
 export HBASE_HOST
+export HBASE_STARGATE_PORT=8080
+#export HBASE_THRIFT_PORT=9090
 
 export DOCKER_IMAGE="harisekhon/hbase-dev"
 export DOCKER_CONTAINER="nagios-plugins-hbase-test"
@@ -43,21 +47,33 @@ echo "Setting up HBase test container"
 hr
 launch_container "$DOCKER_IMAGE" "$DOCKER_CONTAINER" 2181 8080 8085 9090 9095 16000 16010 16201 16301
 
-# set up test table
-# needs to pick up JAVA_HOME from shell
-#docker exec -i "$DOCKER_CONTAINER" /bin/bash <<EOF
-#/hbase/bin/hbase shell <<EOF2
-#list
-#EOF2
-#EOF
+echo "setting up test tables"
+uniq_val=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n1 || :)
+docker exec -i "$DOCKER_CONTAINER" /bin/bash <<EOF
+export JAVA_HOME=/usr
+/hbase/bin/hbase shell <<EOF2
+create 't1', 'cf1', { REGION_REPLICATION => 1 }
+create 't2', 'cf2', { REGION_REPLICATION => 1 }
+disable 't2'
+put 't1', 'r1', 'cf1:q1', '$uniq_val'
+put 't1', 'r2', 'cf1:q2', 'test'
+list
+EOF2
+EOF
 
 hr
 # TODO: add $HOST env support
 $perl -T $I_lib ./check_hbase_regionservers.pl -H $HBASE_HOST -P 8080
 hr
+$perl -T $I_lib ./check_hbase_cell.pl -T t1 -R r1 -C cf1:q1 -e "$uniq_val"
+hr
+$perl -T $I_lib ./check_hbase_cell_stargate.pl -T t1 -R r1 -C cf1:q1 -e "$uniq_val"
+hr
+$perl -T $I_lib ./check_hbase_cell_thrift.pl -T t1 -R r1 -C cf1:q1 -e "$uniq_val"
+hr
 # TODO: enable no tables CRITICAL check, then create table then re-check
-$perl -T $I_lib ./check_hbase_tables.pl || :
-$perl -T $I_lib ./check_hbase_tables_thrift.pl || :
+$perl -T $I_lib ./check_hbase_tables.pl
+$perl -T $I_lib ./check_hbase_tables_thrift.pl
 # XXX: needs updates
 #$perl -T $I_lib ./check_hbase_tables_stargate.pl
 #$perl -T $I_lib ./check_hbase_tables_jsp.pl
@@ -66,11 +82,14 @@ $perl -T $I_lib ./check_hbase_tables_thrift.pl || :
 #hr
 #$perl -T $I_lib 
 hr
-if is_zookeeper_built; then
-    $perl -T $I_lib ./check_hbase_unassigned_regions_znode.pl
-    hr
-else
-    echo "ZooKeeper not built - skipping ZooKeeper checks"
-fi
+# Use Docker hbase-dev, zookeeper will have been built
+#if is_zookeeper_built; then
+#    $perl -T $I_lib ./check_hbase_unassigned_regions_znode.pl
+#    hr
+#else
+#    echo "ZooKeeper not built - skipping ZooKeeper checks"
+#fi
+docker exec -ti "$DOCKER_CONTAINER" -v "$srcdir/..":/pl /pl/check_hbase_unassigned_regions_znode.pl -H localhost
+hr
 
 delete_container
