4b47706b9632fd6bca3c4a35e62ffddd315cb0fe
Hari Sekhon <harisekhon@gmail.com>
2016-05-20 13:16:10 +0100

    updated test_solrcloud.sh

--- tests/test_solrcloud.sh
+++ tests/test_solrcloud.sh
@@ -34,8 +34,10 @@ SOLR_HOST="${SOLR_HOST##*/}"
 export SOLR_HOST="${SOLR_HOST%%:*}"
 export ZOOKEEPER_HOST="$SOLR_HOST"
 
-export DOCKER_IMAGE="harisekhon/solrcloud-dev:4.10"
+export DOCKER_IMAGE="harisekhon/solrcloud-dev"
 export DOCKER_CONTAINER="nagios-plugins-solrcloud-test"
+
+export SOLR_VERSIONS="4.10 5.5 6.0"
 
 export MNTDIR="/pl"
 
@@ -52,34 +54,43 @@ docker_exec(){
     docker exec -ti "$DOCKER_CONTAINER" $MNTDIR/$@
 }
 
-echo "Setting up SolrCloud docker test container"
-DOCKER_OPTS="-v $srcdir/..:$MNTDIR"
-launch_container "$DOCKER_IMAGE" "$DOCKER_CONTAINER" 8983 8984 9983
+test_solrcloud(){
+    local version="$1"
+    echo "Setting up SolrCloud $version docker test container"
+    DOCKER_OPTS="-v $srcdir/..:$MNTDIR"
+    launch_container "$DOCKER_IMAGE:$version" "$DOCKER_CONTAINER" 8983 8984 9983
 
-hr
-# docker is running slow
-$perl -T $I_lib ./check_solrcloud_cluster_status.pl -v -t 60
-hr
-docker_exec check_solrcloud_cluster_status_zookeeper.pl -H localhost -P 9983 -b / -v
-hr
-# FIXME: doesn't pick up collection from env
-docker_exec check_solrcloud_config_zookeeper.pl -H localhost -P 9983 -b / -C collection1 -d /solr/node1/solr/collection1/conf -v
-hr
-# FIXME: why is only 1 node up instead of 2
-$perl -T $I_lib ./check_solrcloud_live_nodes.pl -w 1 -c 1 -t 60 -v
-hr
-docker_exec check_solrcloud_live_nodes_zookeeper.pl -H localhost -P 9983 -b / -w 1 -c 1 -v
-hr
-# docker is running slow
-$perl -T $I_lib ./check_solrcloud_overseer.pl -t 60 -v
-hr
-docker_exec check_solrcloud_overseer_zookeeper.pl -H localhost -P 9983 -b / -v
-hr
-docker_exec check_solrcloud_server_znode.pl -H localhost -P 9983 -z /live_nodes/$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$DOCKER_CONTAINER"):8983_solr -v
-hr
-# FIXME: second node not up
-#docker_exec check_solrcloud_server_znode.pl -H localhost -P 9983 -z /live_nodes/$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$DOCKER_CONTAINER"):8984_solr -v
-hr
-docker_exec check_zookeeper_config.pl -H localhost -P 9983 -C /solr/node1/solr/zoo.cfg --no-warn-extra -v
-hr
-delete_container
+    hr
+    # docker is running slow
+    $perl -T $I_lib ./check_solrcloud_cluster_status.pl -v -t 60
+    hr
+    docker_exec check_solrcloud_cluster_status_zookeeper.pl -H localhost -P 9983 -b / -v
+    hr
+    # FIXME: doesn't pick up collection from env
+    docker_exec check_solrcloud_config_zookeeper.pl -H localhost -P 9983 -b / -C collection1 -d /solr/node1/solr/collection1/conf -v
+    hr
+    # FIXME: why is only 1 node up instead of 2
+    $perl -T $I_lib ./check_solrcloud_live_nodes.pl -w 1 -c 1 -t 60 -v
+    hr
+    docker_exec check_solrcloud_live_nodes_zookeeper.pl -H localhost -P 9983 -b / -w 1 -c 1 -v
+    hr
+    # docker is running slow
+    $perl -T $I_lib ./check_solrcloud_overseer.pl -t 60 -v
+    hr
+    docker_exec check_solrcloud_overseer_zookeeper.pl -H localhost -P 9983 -b / -v
+    hr
+    docker_exec check_solrcloud_server_znode.pl -H localhost -P 9983 -z /live_nodes/$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$DOCKER_CONTAINER"):8983_solr -v
+    hr
+    # FIXME: second node not up
+    #docker_exec check_solrcloud_server_znode.pl -H localhost -P 9983 -z /live_nodes/$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$DOCKER_CONTAINER"):8984_solr -v
+    hr
+    docker_exec check_zookeeper_config.pl -H localhost -P 9983 -C /solr/node1/solr/zoo.cfg --no-warn-extra -v
+    hr
+    delete_container
+    hr
+    echo
+}
+
+for version in $SOLR_VERSIONS; do
+    test_solrcloud $version
+done
