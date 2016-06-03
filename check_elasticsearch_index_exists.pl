e0c6669366ed02e742435d3951a762b6f8ef3f07
Hari Sekhon <harisekhon@gmail.com>
2016-06-03 13:06:08 +0100

    updated check_elasticsearch_index_exists.pl

--- check_elasticsearch_index_exists.pl
+++ check_elasticsearch_index_exists.pl
@@ -12,7 +12,7 @@
 
 $DESCRIPTION = "Nagios Plugin to check a given index exists in Elasticsearch
 
-Tested on Elasticsearch 1.2.1, 1.4.0, 1.4.4";
+Tested on Elasticsearch 1.2.1, 1.4.0, 1.4.4, 1.4.5, 1.5.2, 1.6.2, 2.0.2, 2.2.2";
 
 $VERSION = "0.1";
 
