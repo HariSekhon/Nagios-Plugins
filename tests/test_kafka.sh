96d3c4e97614469130a2c2c378575c45d481a449
Hari Sekhon <harisekhon@gmail.com>
2016-06-03 16:39:44 +0100

    updated test_kafka.sh

--- tests/test_kafka.sh
+++ tests/test_kafka.sh
@@ -38,6 +38,9 @@ export KAFKA_PORT="${KAFKA_PORT:-9092}"
 # TODO: latest container 2.11_0.10 doesn't work yet, no leader takes hold
 #export KAFKA_VERSIONS="2.11_0.10 2.11_0.10 latest"
 export KAFKA_VERSIONS="2.10_0.8 2.11_0.8 2.10_0.9 2.11_0.9"
+if is_travis; then
+    export KAFKA_VERSIONS="2.10_0.8"
+fi
 
 export DOCKER_IMAGE="harisekhon/kafka"
 export DOCKER_CONTAINER="nagios-plugins-kafka-test"
