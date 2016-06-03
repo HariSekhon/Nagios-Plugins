fc902c3952b62b83595e91f7ae1138fbfa0b6f20
Hari Sekhon <harisekhon@gmail.com>
2016-06-03 13:06:31 +0100

    updated check_elasticsearch_index_stats.pl

--- check_elasticsearch_index_stats.pl
+++ check_elasticsearch_index_stats.pl
@@ -20,7 +20,7 @@ $DESCRIPTION = "Nagios Plugin to check the stats for a given Elasticsearch index
 
 use -vv to see a convenient list of stats one per line to select from.
 
-Tested on Elasticsearch 1.2.1, 1.4.0, 1.4.4";
+Tested on Elasticsearch 1.2.1, 1.4.0, 1.4.4, 1.4.5, 1.5.2, 1.6.2, 2.0.2, 2.2.2";
 
 $VERSION = "0.2";
 
