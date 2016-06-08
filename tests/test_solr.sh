8e34b301f91c11622c4c2816d226cf383db7ef56
Hari Sekhon <harisekhon@gmail.com>
2016-06-08 12:35:35 +0100

    updated test_solr.sh

--- tests/test_solr.sh
+++ tests/test_solr.sh
@@ -27,7 +27,7 @@ echo "
 # ============================================================================ #
 "
 
-export SOLR_VERSIONS="${@:-3.1 3.6 4.10 5.5 6.0 latest}"
+export SOLR_VERSIONS="${@:-latest 3.1 3.6 4.10 5.5 6.0}"
 
 SOLR_HOST="${DOCKER_HOST:-${SOLR_HOST:-${HOST:-localhost}}}"
 SOLR_HOST="${SOLR_HOST##*/}"
