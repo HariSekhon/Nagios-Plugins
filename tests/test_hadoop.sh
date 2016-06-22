1ec631d870690d57545e12d2f804bd8e1acb5e23
Hari Sekhon <harisekhon@gmail.com>
2016-06-22 16:04:30 +0100

    updated test_hadoop.sh

--- tests/test_hadoop.sh
+++ tests/test_hadoop.sh
@@ -75,27 +75,27 @@ EOF
     if [ "$version" = "latest" ]; then
         local version=".*"
     fi
-    $perl -T $I_lib ./check_hadoop_yarn_resource_manager_version.pl -v -e "$version"
-    $perl -T $I_lib ./check_hadoop_hdfs_datanode_version.pl -N $(docker exec "$DOCKER_CONTAINER" hostname) -v -e "$version"
+    $perl -T ./check_hadoop_yarn_resource_manager_version.pl -v -e "$version"
+    $perl -T ./check_hadoop_hdfs_datanode_version.pl -N $(docker exec "$DOCKER_CONTAINER" hostname) -v -e "$version"
     hr
     docker_exec check_hadoop_balance.pl -w 5 -c 10 --hadoop-bin /hadoop/bin/hdfs --hadoop-user root -t 60
     hr
-    $perl -T $I_lib ./check_hadoop_checkpoint.pl
+    $perl -T ./check_hadoop_checkpoint.pl
     hr
     # XXX: requires updates for 2.7
-    #$perl -T $I_lib ./check_hadoop_datanode_blockcount.pl -H $HADOOP_HOST
+    #$perl -T ./check_hadoop_datanode_blockcount.pl -H $HADOOP_HOST
     hr
-    $perl -T $I_lib ./check_hadoop_datanode_jmx.pl --all-metrics
+    $perl -T ./check_hadoop_datanode_jmx.pl --all-metrics
     hr
     # XXX: requires updates for 2.7
-    #$perl -T $I_lib ./check_hadoop_datanodes_block_balance.pl -H $HADOOP_HOST -w 5 -c 10 -vvv
-    #$perl -T $I_lib ./check_hadoop_datanodes_blockcounts.pl -H $HADOOP_HOST -vvv
+    #$perl -T ./check_hadoop_datanodes_block_balance.pl -H $HADOOP_HOST -w 5 -c 10 -vvv
+    #$perl -T ./check_hadoop_datanodes_blockcounts.pl -H $HADOOP_HOST -vvv
     hr
-    $perl -T $I_lib ./check_hadoop_datanodes.pl
+    $perl -T ./check_hadoop_datanodes.pl
     hr
     # would be much higher on a real cluster, no defaults as much be configured based on NN heap
     # XXX: 404
-    #$perl -T $I_lib ./check_hadoop_hdfs_blocks.pl -w 100 -c 200
+    #$perl -T ./check_hadoop_hdfs_blocks.pl -w 100 -c 200
     hr
     # run inside Docker container so it can resolve redirect to DN
     docker_exec check_hadoop_hdfs_file_webhdfs.pl -H localhost -p /tmp/test.txt --owner root --group supergroup --replication 1 --size 8 --last-accessed 600 --last-modified 600 --blockSize 134217728
@@ -108,58 +108,58 @@ EOF
     # run inside Docker container so it can resolve redirect to DN
     docker_exec check_hadoop_hdfs_write_webhdfs.pl -H localhost
     hr
-    $perl -T $I_lib ./check_hadoop_jmx.pl -P 8042 -a
+    $perl -T ./check_hadoop_jmx.pl -P 8042 -a
     hr
-    $perl -T $I_lib ./check_hadoop_jmx.pl -P 8088 -a
+    $perl -T ./check_hadoop_jmx.pl -P 8088 -a
     hr
-    $perl -T $I_lib ./check_hadoop_jmx.pl -P 50070 -a
+    $perl -T ./check_hadoop_jmx.pl -P 50070 -a
     hr
-    $perl -T $I_lib ./check_hadoop_jmx.pl -P 50075 -a
+    $perl -T ./check_hadoop_jmx.pl -P 50075 -a
     hr
-    $perl -T $I_lib ./check_hadoop_namenode_heap.pl
+    $perl -T ./check_hadoop_namenode_heap.pl
     hr
     # XXX: fix required for non-integer? Happens just after booting Hadoop docker image
-    #$perl -T $I_lib ./check_hadoop_namenode_heap.pl --non-heap -vvv
+    #$perl -T ./check_hadoop_namenode_heap.pl --non-heap -vvv
     hr
-    $perl -T $I_lib ./check_hadoop_namenode_jmx.pl --all-metrics
+    $perl -T ./check_hadoop_namenode_jmx.pl --all-metrics
     hr
     # all the hadoop namenode checks need updating
     # 404 not found
-    #$perl -T $I_lib ./check_hadoop_namenode.pl -b -w 5 -c 10
+    #$perl -T ./check_hadoop_namenode.pl -b -w 5 -c 10
     hr
-    $perl -T $I_lib ./check_hadoop_namenode_safemode.pl
+    $perl -T ./check_hadoop_namenode_safemode.pl
     hr
     set +o pipefail
-    $perl -T $I_lib ./check_hadoop_namenode_security_enabled.pl | grep -Fx "CRITICAL: namenode security enabled 'false'"
+    $perl -T ./check_hadoop_namenode_security_enabled.pl | grep -Fx "CRITICAL: namenode security enabled 'false'"
     set -o pipefail
     hr
-    $perl -T $I_lib ./check_hadoop_namenode_state.pl
+    $perl -T ./check_hadoop_namenode_state.pl
     hr
-    $perl -T $I_lib ./check_hadoop_replication.pl
+    $perl -T ./check_hadoop_replication.pl
     hr
-    $perl -T $I_lib ./check_hadoop_yarn_app_stats.pl
+    $perl -T ./check_hadoop_yarn_app_stats.pl
     hr
-    $perl -T $I_lib ./check_hadoop_yarn_app_stats_queue.pl
+    $perl -T ./check_hadoop_yarn_app_stats_queue.pl
     hr
-    $perl -T $I_lib ./check_hadoop_yarn_metrics.pl
+    $perl -T ./check_hadoop_yarn_metrics.pl
     hr
-    $perl -T $I_lib ./check_hadoop_yarn_node_manager.pl
+    $perl -T ./check_hadoop_yarn_node_manager.pl
     hr
-    $perl -T $I_lib ./check_hadoop_yarn_node_managers.pl -w 1 -c 1
+    $perl -T ./check_hadoop_yarn_node_managers.pl -w 1 -c 1
     hr
-    $perl -T $I_lib ./check_hadoop_yarn_node_manager_via_rm.pl --node $(docker ps | awk "/$DOCKER_CONTAINER/{print \$1}")
+    $perl -T ./check_hadoop_yarn_node_manager_via_rm.pl --node $(docker ps | awk "/$DOCKER_CONTAINER/{print \$1}")
     hr
-    $perl -T $I_lib ./check_hadoop_yarn_queue_capacity.pl
-    $perl -T $I_lib ./check_hadoop_yarn_queue_capacity.pl --queue default
+    $perl -T ./check_hadoop_yarn_queue_capacity.pl
+    $perl -T ./check_hadoop_yarn_queue_capacity.pl --queue default
     hr
-    $perl -T $I_lib ./check_hadoop_yarn_queue_state.pl
-    $perl -T $I_lib ./check_hadoop_yarn_queue_state.pl --queue default
+    $perl -T ./check_hadoop_yarn_queue_state.pl
+    $perl -T ./check_hadoop_yarn_queue_state.pl --queue default
     hr
-    $perl -T $I_lib ./check_hadoop_yarn_resource_manager_heap.pl
+    $perl -T ./check_hadoop_yarn_resource_manager_heap.pl
     # non integer NonHeapMemoryUsage
-    #$perl -T $I_lib ./check_hadoop_yarn_resource_manager_heap.pl --non-heap
+    #$perl -T ./check_hadoop_yarn_resource_manager_heap.pl --non-heap
     hr
-    $perl -T $I_lib ./check_hadoop_yarn_resource_manager_state.pl
+    $perl -T ./check_hadoop_yarn_resource_manager_state.pl
     hr
     delete_container
 }
