ea52bdc016f980ff9fa2fa3255fe5176a235e34e
Hari Sekhon <harisekhon@gmail.com>
2016-07-31 09:03:12 +0100

    updated check_linux_auth.pl

--- check_linux_auth.pl
+++ check_linux_auth.pl
@@ -14,7 +14,7 @@ $DESCRIPTION = "Nagios Plugin to check authentication mechanisms are working by 
 - certain users/groups are present
 - there are no duplicate UID/GIDs
 - groups.allow contains the expected groups and no others
- 
+
 Useful for checking that AD integrated Linux servers are still able to authenticate AD users
 ";
 # - TODO: system-auth-ac matches the expected checksum
