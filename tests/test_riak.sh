936bfa3c0ffdb3fb8169ee99a15006352700d0f8
Hari Sekhon <harisekhon@gmail.com>
2016-08-04 15:44:27 +0100

    updated test_riak.sh

--- tests/test_riak.sh
+++ tests/test_riak.sh
@@ -27,7 +27,7 @@ echo "
 # ============================================================================ #
 "
 
-export RIAK_VERSIONS="${@:-latest 1.4 2.0 2.1}"
+export RIAK_VERSIONS="${@:-${RIAK_VERSIONS:-latest 1.4 2.0 2.1}}"
 
 # RIAK_HOST no longer obtained via .travis.yml, some of these require local riak-admin tool so only makes more sense to run all tests locally
 RIAK_HOST="${DOCKER_HOST:-${RIAK_HOST:-${HOST:-localhost}}}"
