7d91db4686fcc1f9808d3ce4ebf6f5315220dd2d
Hari Sekhon <harisekhon@gmail.com>
2016-08-04 15:38:00 +0100

    updated test_consul.sh

--- tests/test_consul.sh
+++ tests/test_consul.sh
@@ -28,7 +28,7 @@ echo "
 # ============================================================================ #
 "
 
-export CONSUL_VERSIONS="${@:-latest 0.6}"
+export CONSUL_VERSIONS="${@:-${CONSUL_VERSIONS:-latest 0.1 0.2 0.3 0.4 0.5 0.6 0.7}}"
 
 CONSUL_HOST="${DOCKER_HOST:-${CONSUL_HOST:-${HOST:-localhost}}}"
 CONSUL_HOST="${CONSUL_HOST##*/}"
