80e3c01777b4ccc1e97b3938808f6443b7a7d928
Hari Sekhon <harisekhon@gmail.com>
2016-09-15 10:39:39 +0200

    found bug in JMX, changed to parse HMaster UI instead

--- check_hbase_regions_stuck_in_transition.py
+++ check_hbase_regions_stuck_in_transition.py
@@ -25,11 +25,13 @@ from __future__ import division
 from __future__ import print_function
 #from __future__ import unicode_literals
 
-import json
+#import logging
+#import json
 import os
 import sys
 import traceback
 try:
+    from bs4 import BeautifulSoup
     import requests
 except ImportError:
     print(traceback.format_exc(), end='')
@@ -39,7 +41,7 @@ libdir = os.path.join(srcdir, 'pylib')
 sys.path.append(libdir)
 try:
     # pylint: disable=wrong-import-position
-    from harisekhon.utils import log, qquit, support_msg_api, isList, isInt
+    from harisekhon.utils import log, qquit, isInt, support_msg
     from harisekhon.utils import validate_host, validate_port
     from harisekhon import NagiosPlugin
 except ImportError as _:
@@ -47,7 +49,7 @@ except ImportError as _:
     sys.exit(4)
 
 __author__ = 'Hari Sekhon'
-__version__ = '0.1'
+__version__ = '0.2'
 
 
 class CheckHbaseRegionsStuckInTransition(NagiosPlugin):
@@ -58,7 +60,6 @@ class CheckHbaseRegionsStuckInTransition(NagiosPlugin):
         # Python 3.x
         # super().__init__()
         self.msg = 'msg not defined'
-        self.regions_stuck_in_transition = None
 
     def add_options(self):
         self.add_hostoption(name='HBase Master', default_host='localhost', default_port=16010)
@@ -70,9 +71,11 @@ class CheckHbaseRegionsStuckInTransition(NagiosPlugin):
         validate_host(host)
         validate_port(port)
 
-        # can also see this on the page
+        # observed bug in HDP 2.3 (HBase 1.1.2) where the JMX metric from HMaster UI /jmx is displaying 0 for beans [ { "name":"Hadoop:service=HBase,name=Master,sub=AssignmentManger", ..., "ritCountOverThreshold" : 0 }
+        #url = 'http://%(host)s:%(port)s/jmx' % locals()
+        # could get info from flat txt debug page but it doesn't contain the summary count, would have to parse and more likely to miss data due to free form than BeautifulSoup
         #url = 'http://%(host)s:%(port)s/dump' % locals()
-        url = 'http://%(host)s:%(port)s/jmx' % locals()
+        url = 'http://%(host)s:%(port)s/master-status' % locals()
         log.debug('GET %s' % url)
         try:
             req = requests.get(url)
@@ -82,38 +85,69 @@ class CheckHbaseRegionsStuckInTransition(NagiosPlugin):
         log.debug("content:\n%s\n%s\n%s", '='*80, req.content.strip(), '='*80)
         if req.status_code != 200:
             qquit('CRITICAL', "%s %s" % (req.status_code, req.reason))
-        self.parse(req.content)
-        if self.regions_stuck_in_transition == 0:
+        regions_stuck_in_transition = self.parse(req.content)
+        if regions_stuck_in_transition is None:
+            qquit('UNKNOWN', 'parse error - failed to find number for regions stuck in transition')
+        if not isInt(regions_stuck_in_transition):
+            qquit('UNKNOWN', 'parse error - got non-integer for regions stuck in transition when parsing HMaster UI')
+        if regions_stuck_in_transition == 0:
             self.ok()
         else:
             self.critical()
         self.msg = '{0} regions stuck in transition (ie. transitioning longer than HBase threshold)'\
-                   .format(self.regions_stuck_in_transition)
-        self.msg += " | regions_stuck_in_transition={0};0;0".format(self.regions_stuck_in_transition)
+                   .format(regions_stuck_in_transition)
+        self.msg += " | regions_stuck_in_transition={0};0;0".format(regions_stuck_in_transition)
 
-    def parse(self, content):
-        # collect lines after 'Regions-in-transition' if parsing /dump
+    # parsing for /jmx
+#    def parse(self, content):
+#        jsondict = None
+#        try:
+#            jsondict = json.loads(content)
+#        except ValueError:
+#            qquit('UNKNOWN', 'failed to parse returned json')
+#        try:
+#            _ = jsondict['beans']
+#            if not isList(_):
+#                qquit('UNKNOWN', 'beans is not a list! ' + support_msg_api())
+#            for section in _:
+#                if section['name'] == 'Hadoop:service=HBase,name=Master,sub=AssignmentManger':
+#                    regions_stuck_in_transition = section['ritCountOverThreshold']
+#                    if not isInt(regions_stuck_in_transition):
+#                        qquit('UNKNOWN', 'non-integer parsed for ritCountOverThreshold' + support_msg_api())
+#                    regions_stuck_in_transition = int(regions_stuck_in_transition)
+#                    return regions_stuck_in_transition
+#        except (KeyError, ValueError) as _:
+#            qquit('UNKNOWN', 'failed to parse JMX data' + support_msg_api())
+
+    @staticmethod
+    def parse(content):
+        # could also collect lines after 'Regions-in-transition' if parsing /dump
         # sample:
         # hbase:meta,,1.1588230740 state=PENDING_OPEN, \
         # ts=Tue Nov 24 08:26:45 UTC 2015 (1098s ago), server=amb2.service.consul,16020,1448353564099
-        jsondict = None
+        soup = BeautifulSoup(content, 'html.parser')
+        #if log.isEnabledFor(logging.DEBUG):
+        #    log.debug("BeautifulSoup prettified:\n%s\n%s", soup.prettify(), '='*80)
+        regions_stuck_in_transition = None
         try:
-            jsondict = json.loads(content)
-        except ValueError:
-            qquit('UNKNOWN', 'failed to parse returned json')
-        try:
-            _ = jsondict['beans']
-            if not isList(_):
-                qquit('UNKNOWN', 'beans is not a list! ' + support_msg_api())
-            for section in _:
-                if section['name'] == 'Hadoop:service=HBase,name=Master,sub=AssignmentManger':
-                    self.regions_stuck_in_transition = section['ritCountOverThreshold']
-                    if not isInt(self.regions_stuck_in_transition):
-                        qquit('UNKNOWN', 'non-integer parsed for ritCountOverThreshold' + support_msg_api())
-                    self.regions_stuck_in_transition = int(self.regions_stuck_in_transition)
-                    break
-        except (KeyError, ValueError) as _:
-            qquit('UNKNOWN', 'failed to parse JMX data' + support_msg_api())
+            headings = soup.findAll('h2')
+            for heading in headings:
+                log.debug("checking heading '%s'", heading)
+                if heading.get_text() == "Regions in Transition":
+                    log.debug('found Regions in Transition section header')
+                    table = heading.find_next('table')
+                    log.debug('checking first following table')
+                    for row in table.findChildren('tr'):
+                        for col in row.findChildren('td'):
+                            if 'Regions in Transition for more than ' in col.get_text():
+                                log.debug('found Regions in Transition for more than ... getting next td')
+                                next_sibling = col.findNext('td')
+                                regions_stuck_in_transition = next_sibling.get_text().strip()
+                                return regions_stuck_in_transition
+            qquit('UNKNOWN', 'parse error - failed to find table data for regions stuck in transition')
+        except (AttributeError, TypeError):
+            qquit('UNKNOWN', 'failed to parse HBase Master UI status page. %s' % support_msg())
+
 
 if __name__ == '__main__':
     CheckHbaseRegionsStuckInTransition().main()
