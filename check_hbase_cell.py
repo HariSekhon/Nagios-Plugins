dde95c447eee1afbf79c2801f2ad83f5ba89fec0
Hari Sekhon <harisekhon@gmail.com>
2016-10-13 11:34:38 +0100

    updated check_hbase_cell.py

--- check_hbase_cell.py
+++ check_hbase_cell.py
@@ -18,6 +18,14 @@
 
 Nagios Plugin to check a specific HBase table's cell value via the Thrift API
 
+1. checks HBase table is enabled
+2. reads latest HBase cell value for the given table, row key and column family:qualifier
+3. checks cell's returned value against expected regex (optional)
+4. checks cell's returned value against warning/critical range thresholds (optional)
+   raises warning/critical if the value is outside thresholds or not a floating point number
+5. outputs the query time to a given precision for reporting and graphing
+6. optionally outputs the cell's value for graphing purposes
+
 Tested on Apache HBase 1.0.3, 1.1.6, 1.2.2
 
 """
@@ -58,7 +66,7 @@ except ImportError as _:
     sys.exit(4)
 
 __author__ = 'Hari Sekhon'
-__version__ = '0.2'
+__version__ = '0.3'
 
 
 class CheckHBaseCell(NagiosPlugin):
@@ -75,6 +83,7 @@ class CheckHBaseCell(NagiosPlugin):
         self.row = None
         self.column = None
         self.expected = None
+        self.precision = 4
         self.graph = False
         self.units = None
         self.list_tables = False
@@ -88,6 +97,8 @@ class CheckHBaseCell(NagiosPlugin):
         self.add_opt('-C', '--column', help='Column family:qualifier to query')
         self.add_opt('-e', '--expected', help='Expected regex for the cell\'s value. Optional')
         self.add_thresholds()
+        self.add_opt('-p', '--precision', default=4, metavar='int',
+                     help='Precision for query timing in decimal places (default: 4)')
         self.add_opt('-g', '--graph', action='store_true', help="Graph the cell's value. Optional, use only if a " +
                      "floating point number is normally returned for it's values, otherwise will print NaN " +
                      "(Not a Number). The reason this is not determined automatically is because keys that change " +
@@ -103,6 +114,7 @@ class CheckHBaseCell(NagiosPlugin):
         self.row = self.get_opt('row')
         self.column = self.get_opt('column')
         self.expected = self.get_opt('expected')
+        self.precision = self.get_opt('precision')
         self.graph = self.get_opt('graph')
         self.units = self.get_opt('units')
         validate_host(self.host)
@@ -120,21 +132,26 @@ class CheckHBaseCell(NagiosPlugin):
         self.validate_thresholds(optional=True, positive=False)
 
     def run(self):
-        self.connect()
+        connect_time = self.connect()
         if self.list_tables:
             tables = self.get_tables()
             print('HBase Tables:\n\n' + '\n'.join(tables))
             sys.exit(ERRORS['UNKNOWN'])
-        self.check_cell()
+        (value, query_time) = self.check_cell()
+        self.output_perfdata(value, connect_time, query_time)
 
     def connect(self):
         log.info('connecting to HBase Thrift Server at %s:%s', self.host, self.port)
         try:
+            start = time.time()
             self.conn = happybase.Connection(host=self.host, port=self.port, timeout=10 * 1000)  # ms
+            connect_time = time.time() - start
+            log.info('connected in %s secs', connect_time)
         except socket.timeout as _:
             qquit('CRITICAL', _)
         except ThriftException as _:
             qquit('CRITICAL', _)
+        return connect_time
 
     def get_tables(self):
         try:
@@ -159,6 +176,7 @@ class CheckHBaseCell(NagiosPlugin):
             start = time.time()
             cells = table.cells(self.row, self.column, versions=1)
             query_time = time.time() - start
+            log.info('queried in %s secs', query_time)
             log.info('finished, closing connection')
             self.conn.close()
         except HBaseIOError as _:
@@ -194,19 +212,23 @@ class CheckHBaseCell(NagiosPlugin):
             if not re.search(self.expected, value):
                 qquit('CRITICAL', "cell value '{0}' (expected regex '{1}') for {2}".format(value, self.expected,
                                                                                            cell_info))
+        return (value, query_time)
+
+    def output_perfdata(self, value, connect_time, query_time):
         if isFloat(value):
             log.info('value is float, checking thresholds')
             self.check_thresholds(value)
         self.msg += ' | '
         if self.graph:
             if isFloat(value):
-                self.msg += 'value={0} '.format(value)
+                self.msg += 'value={0}'.format(value)
                 if self.units:
                     self.msg += str(self.units)
                 self.msg += self.get_perf_thresholds()
             else:
-                self.msg += 'value=NaN '
-        self.msg += 'query_time={0}s'.format(query_time)
+                self.msg += 'value=NaN'
+        self.msg += ' connect_time={0:0.{2}f}s query_time={1:0.{2}f}s'.format(connect_time,
+                                                                              query_time, self.precision)
 
 
 if __name__ == '__main__':
