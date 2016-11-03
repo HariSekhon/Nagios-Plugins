48bc872d4f7758e44420d7a53c98da68330606ba
Hari Sekhon <harisekhon@gmail.com>
2016-11-03 15:41:19 +0000

    updated check_zaloni_bedrock_workflow.py

--- check_zaloni_bedrock_workflow.py
+++ check_zaloni_bedrock_workflow.py
@@ -122,8 +122,10 @@ class CheckZaloniBedrockWorkflow(NagiosPlugin):
             pass
         else:
             self.usage('must specify either --name or --id or use --list to find them')
-        validate_float(max_age, 'max age', 1)
-        validate_float(max_runtime, 'max runtime', 1)
+        if max_age is not None:
+            validate_float(max_age, 'max age', 1)
+        if max_runtime is not None:
+            validate_float(max_runtime, 'max runtime', 1)
 
         self.url_base = 'http://%(host)s:%(port)s/bedrock-app/services/rest' % locals()
         # auth first, get JSESSIONID cookie
