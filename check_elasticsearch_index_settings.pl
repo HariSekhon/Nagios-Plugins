f8c89d030e270f9d8b49f0969ae9fe1803cedc08
Hari Sekhon <harisekhon@gmail.com>
2016-11-15 14:53:53 +0000

    updated check_elasticsearch_index_settings.pl

--- check_elasticsearch_index_settings.pl
+++ check_elasticsearch_index_settings.pl
@@ -16,7 +16,7 @@ Checks any arbitrary setting key (eg. index.refresh_interval) against a given ex
 
 Caveats: since Elasticsearch doesn't output settings which have default values, there is no way to determine whether a given arbitrary key is in it's default setting or if the key is simply not a valid setting that will never show up.
 
-Tested on Elasticsearch 1.2.1, 1.4.0, 1.4.4, 1.4.5, 1.5.2, 1.6.2, 1.7.5, 2.0.2, 2.2.2, 2.3.3";
+Tested on Elasticsearch 1.2.1, 1.4.0, 1.4.4, 1.4.5, 1.5.2, 1.6.2, 1.7.5, 2.0.2, 2.2.2, 2.3.3, 5.0.0";
 
 $VERSION = "0.6";
 
