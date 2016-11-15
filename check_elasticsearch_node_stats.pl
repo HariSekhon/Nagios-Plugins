9b47d1ed0b0f8516b7a538bfe42be18a74e2815a
Hari Sekhon <harisekhon@gmail.com>
2016-11-15 15:34:50 +0000

    updated check_elasticsearch_node_stats.pl

--- check_elasticsearch_node_stats.pl
+++ check_elasticsearch_node_stats.pl
@@ -22,6 +22,8 @@ Should specify an Elasticsearch node name rather than a hostname/FQDN/IP (see --
 
 For a convenient list of all stats one per line use -vv
 
+If using the node name be aware that the Elasticsearch API truncates the node name to 7 characters so you should only supply the first 7 chars to work around this for now - see https://github.com/elastic/elasticsearch/issues/21565
+
 Tested on Elasticsearch 1.4.0, 1.4.4, 1.4.5, 1.5.2, 1.6.2, 1.7.5, 2.0.2, 2.2.2, 2.3.3, 5.0.0";
 
 $VERSION = "0.1";
@@ -43,7 +45,7 @@ my $expected_value;
 %options = (
     %hostoptions,
     %elasticsearch_node,
-    "K|key=s"   => [ \$keys,            "Stat Key(s) to fetch (eg. indices.docs.count, http.current_open, fs.total.available_in_bytes). Multiple keys may be comma separated. Optional, all stats will be printed if no specific stat(s) requested" ],
+    "K|key=s" => [ \$keys, "Stat Key(s) to fetch (eg. indices.docs.count, http.current_open, fs.total.available_in_bytes). Multiple keys may be comma separated. Optional, all stats will be printed if no specific stat(s) requested" ],
     %thresholdoptions,
 );
 
