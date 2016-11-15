76447a5f96e31a9a5ac543f4a8264e95b0a46f02
Hari Sekhon <harisekhon@gmail.com>
2016-11-15 14:54:16 +0000

    updated check_elasticsearch_node_disk_percent.pl

--- check_elasticsearch_node_disk_percent.pl
+++ check_elasticsearch_node_disk_percent.pl
@@ -20,7 +20,7 @@ For regular deployments with one Elasticsearch instance per server it's perfectl
 
 For convenience --node defaults to same as --host, which may not match if you're specifying a short hostname for --host and elasticsearch is reporting an FQDN, in which case you should specify the node explicitly as shown by the output of --list-nodes.
 
-Tested on Elasticsearch 1.4.0, 1.4.4, 1.4.5, 1.5.2, 1.6.2, 1.7.5, 2.0.2, 2.2.2, 2.3.3";
+Tested on Elasticsearch 1.4.0, 1.4.4, 1.4.5, 1.5.2, 1.6.2, 1.7.5, 2.0.2, 2.2.2, 2.3.3, 5.0.0";
 
 $VERSION = "0.2";
 
