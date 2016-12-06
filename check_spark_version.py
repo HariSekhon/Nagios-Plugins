16ff235ea2f6fe8d7ec9e0ad8cd69d79296753e3
Hari Sekhon <harisekhon@gmail.com>
2016-12-06 17:02:41 +0000

    updated check_spark_version.py

--- check_spark_version.py
+++ check_spark_version.py
@@ -27,7 +27,7 @@ Tested on Spark 1.3, 1.4, 1.5, 1.6
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-#from __future__ import unicode_literals
+from __future__ import unicode_literals
 
 import logging
 import os
