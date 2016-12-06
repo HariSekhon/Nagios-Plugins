525b697c39c3383ef802a2cc2a4501b4e0017f1e
Hari Sekhon <harisekhon@gmail.com>
2016-12-06 13:30:50 +0000

    updated check_blue_talon_policy_deployment.py

--- check_blue_talon_policy_deployment.py
+++ check_blue_talon_policy_deployment.py
@@ -134,9 +134,9 @@ class CheckBlueTalonPolicyDeploymentAge(NagiosPlugin):
                                      support_msg=support_msg_api()))
         timedelta = datetime.now() - last_deploy_datetime
         mins = int(int(timedelta.seconds) / 60)
-        self.msg = "{software} last deployment was {mins} mins ago at '{timestamp}'".format(software=self.software,
-                                                                                            timestamp=timestamp,
-                                                                                            mins=mins)
+        self.msg = "{software} last deployment was at '{timestamp}', {mins} mins ago".format(software=self.software,
+                                                                                             timestamp=timestamp,
+                                                                                             mins=mins)
         self.check_thresholds(mins)
         if self.verbose:
             self.msg += " by user '{userid}', host = '{hostname}', description = '{description}'"\
