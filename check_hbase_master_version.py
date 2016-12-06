252b5b71f792e8483cd3e12ff9137ae180447f34
Hari Sekhon <harisekhon@gmail.com>
2016-12-06 17:01:28 +0000

    updated check_hbase_master_version.py

--- check_hbase_master_version.py
+++ check_hbase_master_version.py
@@ -28,7 +28,7 @@ Tested on Apache HBase 1.0.3, 1.1.6, 1.2.2
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-#from __future__ import unicode_literals
+from __future__ import unicode_literals
 
 import logging
 import os
