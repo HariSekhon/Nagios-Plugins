a73b37361b057100d98b7f2c0e693bc6221758a2
Hari Sekhon <harisekhon@gmail.com>
2016-12-06 17:11:56 +0000

    added check_attivio_system_health.py

---
+++ check_attivio_system_health.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2016-12-06 16:37:08 +0000 (Tue, 06 Dec 2016)
+#
+#  https://github.com/harisekhon/nagios-plugins
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+"""
+
+Nagios Plugin to check Attivio AIE system health by parsing its system status page
+
+Tested on Attivio 5.1.8
+
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import logging
+import os
+import sys
+import traceback
+try:
+    import requests
+    #from requests.auth import HTTPBasicAuth
+    from bs4 import BeautifulSoup
+except ImportError:
+    print(traceback.format_exc(), end='')
+    sys.exit(4)
+srcdir = os.path.abspath(os.path.dirname(__file__))
+libdir = os.path.join(srcdir, 'pylib')
+sys.path.append(libdir)
+try:
+    # pylint: disable=wrong-import-position
+    from harisekhon.utils import log, log_option, qquit, support_msg_api
+    from harisekhon.utils import validate_host, validate_port
+    from harisekhon import NagiosPlugin
+except ImportError as _:
+    print(traceback.format_exc(), end='')
+    sys.exit(4)
+
+__author__ = 'Hari Sekhon'
+__version__ = '0.1'
+
+
+class CheckAttivioSystemHealth(NagiosPlugin):
+
+    def __init__(self):
+        # Python 2.x
+        super(CheckAttivioSystemHealth, self).__init__()
+        # Python 3.x
+        # super().__init__()
+        self.software = 'Attivio AIE'
+        self.default_host = 'localhost'
+        self.default_port = 17000
+        self.host = self.default_host
+        self.port = self.default_port
+        self.protocol = 'http'
+        self.msg = '{0} version unknown - no message defined'.format(self.software)
+
+    def add_options(self):
+        self.add_hostoption(name=self.software, default_host=self.default_host, default_port=self.default_port)
+        # no authentication is required to access Attivio's AIE system status page
+        #self.add_useroption(name=self.software, default_user=self.default_user)
+        self.add_opt('-S', '--ssl', action='store_true', help='Use SSL')
+
+    def process_options(self):
+        self.host = self.get_opt('host')
+        self.port = self.get_opt('port')
+        validate_host(self.host)
+        validate_port(self.port)
+        ssl = self.get_opt('ssl')
+        log_option('ssl', ssl)
+        if ssl:
+            self.protocol = 'https'
+
+    def run(self):
+        log.info('querying %s', self.software)
+        url = '{protocol}://{host}:{port}/admin/systemstatus'\
+              .format(host=self.host, port=self.port, protocol=self.protocol)
+        log.debug('GET %s', url)
+        try:
+            req = requests.get(url)
+            #req = requests.get(url, auth=HTTPBasicAuth(self.user, self.password))
+        except requests.exceptions.RequestException as _:
+            errhint = ''
+            if 'BadStatusLine' in str(_.message):
+                errhint = ' (possibly connecting to an SSL secured port without using --ssl?)'
+            elif self.protocol == 'https' and 'unknown protocol' in str(_.message):
+                errhint = ' (possibly connecting to a plain HTTP port with the -S / --ssl switch enabled?)'
+            qquit('CRITICAL', str(_) + errhint)
+        log.debug("response: %s %s", req.status_code, req.reason)
+        log.debug("content:\n%s\n%s\n%s", '='*80, req.content.strip(), '='*80)
+        if req.status_code != 200:
+            qquit('CRITICAL', '{0}: {1}'.format(req.status_code, req.reason))
+        soup = BeautifulSoup(req.content, 'html.parser')
+        if log.isEnabledFor(logging.DEBUG):
+            log.debug("BeautifulSoup prettified:\n{0}\n{1}".format(soup.prettify(), '='*80))
+        try:
+            if soup.find('div', id='syshealthstatus').find('div', id='syshealthok'):
+                qquit('OK', 'system health = OK')
+            qquit('CRITICAL', 'system health != OK')
+        except (AttributeError, TypeError) as _:
+            qquit('UNKNOWN', 'error parsing output from {software}: {exception}: {error}. {support_msg}'\
+                             .format(software=self.software,
+                                     exception=type(_).__name__,
+                                     error=_,
+                                     support_msg=support_msg_api()))
+
+
+if __name__ == '__main__':
+    CheckAttivioSystemHealth().main()
