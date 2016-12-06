0385724ac7536d616079268c43fbcfab9c90e03f
Hari Sekhon <harisekhon@gmail.com>
2016-12-06 17:02:47 +0000

    updated check_tachyon_version.py

--- check_tachyon_version.py
+++ check_tachyon_version.py
@@ -28,7 +28,7 @@ Tested on Tachyon 0.7.1, 0.8.2
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-#from __future__ import unicode_literals
+from __future__ import unicode_literals
 
 import logging
 import os
