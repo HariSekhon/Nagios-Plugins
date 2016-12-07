94283b863a408f5422655ce43eb471be4b07e5e8
Hari Sekhon <harisekhon@gmail.com>
2016-12-07 18:23:22 +0000

    added test_blue_talon.sh

---
+++ tests/test_blue_talon.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-05-25 01:38:24 +0100 (Mon, 25 May 2015)
+#
+#  https://github.com/harisekhon/nagios-plugins
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$srcdir/..";
+
+. ./tests/utils.sh
+
+echo "
+# ============================================================================ #
+#                              B l u e   T a l o n
+# ============================================================================ #
+"
+
+export BLUE_TALON_HOST="${BLUE_TALON_HOST:-trial.bluetalon.com}"
+export BLUE_TALON_PORT=443
+export BLUE_TALON_USER=btadminuser
+export BLUE_TALON_PASSWORD=P@ssw0rd
+
+./check_blue_talon_masking_functions.py -Sv
+hr
+./check_blue_talon_policies.py -Sv
+hr
+./check_blue_talon_policy_deployment.py -Sv
+hr
+./check_blue_talon_resource_domains.py -Sv
+hr
+./check_blue_talon_resources.py -Sv
+hr
+./check_blue_talon_rules.py -Sv
+hr
+./check_blue_talon_user_domains.py -Sv
+hr
+./check_blue_talon_version.py -Sv
+hr
+echo
