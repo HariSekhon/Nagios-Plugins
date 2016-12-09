ebf6b9279dac40dff7606ef2d78cfbb21bf995ec
Hari Sekhon <harisekhon@gmail.com>
2016-12-09 23:03:02 +0000

    updated test_consul.sh

--- tests/test_consul.sh
+++ tests/test_consul.sh
@@ -15,8 +15,7 @@
 
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
-
-srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd "$srcdir/.."
 
@@ -38,76 +37,84 @@ export CONSUL_HOST
 export CONSUL_PORT="${CONSUL_PORT:-8500}"
 
 export DOCKER_IMAGE="harisekhon/consul"
-export DOCKER_CONTAINER="nagios-plugins-consul-test"
+
+export SERVICE="${0#*test_}"
+export SERVICE="${SERVICE%.sh}"
+export DOCKER_CONTAINER="nagios-plugins-$SERVICE-test"
+export COMPOSE_PROJECT_NAME="$DOCKER_CONTAINER"
+export COMPOSE_FILE="$srcdir/docker/$SERVICE-docker-compose.yml"
 
 export MNTDIR="/pl"
 
 startupwait 10
 
-if ! is_docker_available; then
-    echo 'WARNING: Docker not found, skipping Consul checks!!!'
-    exit 0
-fi
+check_docker_available
 
-dockerexec(){
-    docker exec "$DOCKER_CONTAINER-dev" $MNTDIR/$@
+docker_exec(){
+    docker-compose exec "$SERVICE" $MNTDIR/$@
 }
 
 test_consul(){
     local version="$1"
     echo "Setting up Consul $version test container"
     hr
-    local DOCKER_CMD="agent -dev -data-dir /tmp -client 0.0.0.0"
-    launch_container "$DOCKER_IMAGE:$version" "$DOCKER_CONTAINER" $CONSUL_PORT
+    #local DOCKER_CMD="agent -dev -data-dir /tmp -client 0.0.0.0"
+    #launch_container "$DOCKER_IMAGE:$version" "$DOCKER_CONTAINER" $CONSUL_PORT
+    VERSION="$version" docker-compose up -d
+    consul_port="`docker-compose port "$SERVICE" "$CONSUL_PORT" | sed 's/.*://'`"
     if [ -n "${NOTESTS:-}" ]; then
         return 0
     fi
-    when_ports_available $startupwait $CONSUL_HOST $CONSUL_PORT
+    when_ports_available "$startupwait" "$CONSUL_HOST" "$consul_port"
+    # give it some extra time to settle, otherwise get no peers found error
+    sleep 2
     hr
     local testkey="nagios/consul/testkey1"
     echo "Writing random value to test key $testkey"
     local random_val=$RANDOM
-    curl -X PUT -d "$random_val" "http://$CONSUL_HOST:$CONSUL_PORT/v1/kv/$testkey"
+    curl -X PUT -d "$random_val" "http://$CONSUL_HOST:$consul_port/v1/kv/$testkey"
     echo
     hr
+    local expected_version="$version"
     if [ "$version" = "latest" ]; then
-        local version="*"
+        local expected_version="*"
     fi
     set +e
-    found_version=$(docker exec "$DOCKER_CONTAINER" consul version | head -n1 | tee /dev/stderr | sed 's/.*v//')
+    found_version=$(docker-compose exec "$SERVICE" consul version | head -n1 | tee /dev/stderr | sed 's/.*v//')
     set -e
-    if [[ "$found_version" != $version* ]]; then
+    if [[ "$found_version" != $expected_version* ]]; then
         echo "Docker container version does not match expected version! (found '$found_version', expected '$version')"
         exit 1
     fi
     hr
     echo "Consul version $found_version"
     hr
-    ./check_consul_peer_count.py
+    ./check_consul_peer_count.py -P $consul_port
     hr
-    ./check_consul_key.py -k /nagios/consul/testkey1 -r "^$random_val$" -v
+    ./check_consul_key.py -k /nagios/consul/testkey1 -P $consul_port -r "^$random_val$" -v
     hr
     echo "writing deterministic test key to check thresholds"
-    curl -X PUT -d "5" "http://$CONSUL_HOST:$CONSUL_PORT/v1/kv/$testkey"
+    curl -X PUT -d "5" "http://$CONSUL_HOST:$consul_port/v1/kv/$testkey"
     echo
     hr
-    ./check_consul_key.py -k /nagios/consul/testkey1 -r '^\d$' -w 5 -v
+    ./check_consul_key.py -k /nagios/consul/testkey1 -P $consul_port -r '^\d$' -w 5 -v
     hr
-    ./check_consul_key.py -k /nagios/consul/testkey1 -r '^\d$' -c 5 -v
+    ./check_consul_key.py -k /nagios/consul/testkey1 -P $consul_port -r '^\d$' -c 5 -v
     hr
-    ./check_consul_key.py -k /nagios/consul/testkey1 -r '^\d$' -w 5 -c 5 -v
+    ./check_consul_key.py -k /nagios/consul/testkey1 -P $consul_port -r '^\d$' -w 5 -c 5 -v
     hr
     echo "checking threshold failures are caught correctly"
     hr
     set +o pipefail
-    ./check_consul_key.py -k /nagios/consul/testkey1 -r '^\d$' -w 4 -c 5 -v | tee /dev/stderr | grep --color=yes ^WARNING
+    ./check_consul_key.py -k /nagios/consul/testkey1 -P $consul_port -r '^\d$' -w 4 -c 5 -v | tee /dev/stderr | grep --color=yes ^WARNING
     hr
-    ./check_consul_key.py -k /nagios/consul/testkey1 -r '^\d$' -w 4 -c 4 -v | tee /dev/stderr | grep --color=yes ^CRITICAL
+    ./check_consul_key.py -k /nagios/consul/testkey1 -P $consul_port -r '^\d$' -w 4 -c 4 -v | tee /dev/stderr | grep --color=yes ^CRITICAL
     set -o pipefail
     hr
-    ./check_consul_write.py -v
+    ./check_consul_write.py -P $consul_port -v
     hr
-    delete_container
+    #delete_container
+    docker-compose down
     echo
 
     hr
@@ -116,9 +123,11 @@ test_consul(){
     local DOCKER_OPTS="-v $srcdir/..:$MNTDIR"
     local DOCKER_CMD=""
     launch_container "$DOCKER_IMAGE-dev:$version" "$DOCKER_CONTAINER-dev"
-
     hr
-    dockerexec check_consul_version.py
+    if [ "$version" = "latest" ]; then
+        local expected_version=".*"
+    fi
+    docker exec -i "$DOCKER_CONTAINER-dev" "$MNTDIR/check_consul_version.py" -e "$expected_version"
     hr
     delete_container "$DOCKER_CONTAINER-dev"
     echo
