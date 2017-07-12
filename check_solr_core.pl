c14ca4653276778a2b19d67a391f52f3f851d55a
Hari Sekhon <harisekhon@gmail.com>
2017-07-12 15:15:19 +0100

    updated check_solr_core.pl

--- check_solr_core.pl
+++ check_solr_core.pl
@@ -13,7 +13,7 @@ $DESCRIPTION = "Nagios Plugin to check the stats of a Solr 4 core on the server 
 
 Optional thresholds on the core's index size, heap size, number of documents and query time
 
-Tested on Solr 3.1, 3.6.2 and Solr / SolrCloud 4.7.2, 4.10.3, 5.4.0, 5.5.0, 6.0.0, 6.1.0, 6.2.0";
+Tested on Solr 3.1, 3.6.2 and Solr / SolrCloud 4.7.2, 4.10.3, 5.4.0, 5.5.0, 6.0.0, 6.1.0, 6.2.0, 6.2.1, 6.3.0, 6.4.2, 6.5.1, 6.6.0";
 
 our $VERSION = "0.3.1";
 
