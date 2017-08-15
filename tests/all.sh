cf4b2569d2c40b97bca370752daff5c6bfbe8df2
Hari Sekhon <harisekhon@gmail.com>
2017-08-15 19:32:32 +0100

    fixed unbound variable NOTESTS

--- tests/all.sh
+++ tests/all.sh
@@ -35,14 +35,15 @@ cd "$srcdir/..";
 
 . bash-tools/all.sh
 
+#is_travis ||
 time tests/help.sh
 
 # try to minimize time by skipping this as Travis CI is killing the build during testing after 50 mins
-is_travis ||
+#is_travis ||
 tests/test_docker.sh
 
 for script in $(find tests -name 'test*.sh'); do
-    if [ -n "$NOTESTS" -a "$script" = "run_tests.sh" ]; then
+    if [ -n "${NOTESTS:-}" -a "$script" = "run_tests.sh" ]; then
         echo "NOTESTS env var specified, skipping length dockerized tests"
         continue
     fi
