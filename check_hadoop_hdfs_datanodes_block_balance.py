df8bbdb3562fb02621b718c39ae1cbbe550c7f2a
Hari Sekhon <harisekhon@gmail.com>
2017-09-06 14:24:47 +0200

    inherited from RestNagiosPlugin to reduce code

--- check_hadoop_hdfs_datanodes_block_balance.py
+++ check_hadoop_hdfs_datanodes_block_balance.py
@@ -37,7 +37,6 @@ from __future__ import unicode_literals
 import json
 import os
 import sys
-import time
 import traceback
 srcdir = os.path.abspath(os.path.dirname(__file__))
 libdir = os.path.join(srcdir, 'pylib')
@@ -46,63 +45,46 @@ try:
     # pylint: disable=wrong-import-position
     from harisekhon.utils import log, plural, isInt
     from harisekhon.utils import UnknownError, support_msg_api
-    from harisekhon.utils import validate_host, validate_port
-    from harisekhon import NagiosPlugin
-    from harisekhon import RequestHandler
+    from harisekhon import RestNagiosPlugin
 except ImportError as _:
     print(traceback.format_exc(), end='')
     sys.exit(4)
 
 __author__ = 'Hari Sekhon'
-__version__ = '0.2'
+__version__ = '0.4'
 
 
-class CheckHadoopHdfsDatanodesBlockBalance(NagiosPlugin):
+class CheckHadoopHdfsDatanodesBlockBalance(RestNagiosPlugin):
 
     def __init__(self):
         # Python 2.x
         super(CheckHadoopHdfsDatanodesBlockBalance, self).__init__()
         # Python 3.x
         # super().__init__()
-        self.host = None
-        self.port = None
+        self.name = ['Hadoop NameNode', 'Hadoop']
+        self.path = '/jmx?qry=Hadoop:service=NameNode,name=NameNodeInfo'
+        self.default_port = 50070
+        self.json = True
+        self.auth = False
         self.msg = 'Message Not Defined'
-        self.protocol = 'http'
-        self.request = RequestHandler()
 
     def add_options(self):
-        self.add_hostoption(name='Hadoop NameNode', default_host='localhost', default_port=50070)
-        self.add_opt('-S', '--ssl', action='store_true', help='Use SSL')
+        super(CheckHadoopHdfsDatanodesBlockBalance, self).add_options()
         self.add_thresholds(default_warning=10, default_critical=30, percent=True)
 
     def process_options(self):
-        self.no_args()
-        self.host = self.get_opt('host')
-        self.port = self.get_opt('port')
-        validate_host(self.host)
-        validate_port(self.port)
-        if self.get_opt('ssl'):
-            self.protocol = 'https'
+        super(CheckHadoopHdfsDatanodesBlockBalance, self).process_options()
         self.validate_thresholds()
 
-    def run(self):
-        url = '{protocol}://{host}:{port}/jmx?qry=Hadoop:service=NameNode,name=NameNodeInfo'\
-              .format(host=self.host, port=self.port, protocol=self.protocol)
-        start_time = time.time()
-        req = self.request.get(url)
-        query_time = time.time() - start_time
-        self.check_block_balance(req)
-        self.msg += ' | query_time={0:f}s'.format(query_time)
-
-    def check_block_balance(self, req):
+    def parse_json(self, json_data):
         log.info('parsing response')
         try:
-            json_data = json.loads(req.content)
             live_nodes = json_data['beans'][0]['LiveNodes']
             live_node_data = json.loads(live_nodes)
             num_datanodes = len(live_node_data)
             if num_datanodes < 1:
-                raise UnknownError("no datanodes returned by JMX API from namenode '{0}:{1}'".format(self.host, self.port))
+                raise UnknownError("no datanodes returned by JMX API from namenode '{0}:{1}'"\
+                                   .format(self.host, self.port))
             max_blocks = 0
             min_blocks = None
             for datanode in live_node_data:
@@ -138,7 +120,8 @@ class CheckHadoopHdfsDatanodesBlockBalance(NagiosPlugin):
             raise UnknownError("failed to parse json returned by NameNode at '{0}:{1}': {2}. {3}"\
                                .format(self.host, self.port, _, support_msg_api()))
         except ValueError as _:
-            raise UnknownError("invalid json returned by Namenode '{0}:{1}': {2}".format(self.host, self.port, _))
+            raise UnknownError("invalid json returned for LiveNodes by Namenode '{0}:{1}': {2}"\
+                               .format(self.host, self.port, _))
 
 
 if __name__ == '__main__':
