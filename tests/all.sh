c004b6c3d5eb8d6bf993656956f5169f3bab7fce
Hari Sekhon <harisekhon@gmail.com>
2017-09-07 13:13:20 +0200

    updated all.sh

--- tests/all.sh
+++ tests/all.sh
@@ -21,7 +21,7 @@ srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$srcdir/..";
 
 . tests/utils.sh
-. tests/docker.sh
+. bash-tools/docker.sh
 
 section "Running Nagios Plugins ALL"
 
