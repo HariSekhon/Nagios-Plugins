eb2cf96046f058bc4fffe376223edfbb6d3c00dc
Hari Sekhon <harisekhon@gmail.com>
2017-09-07 15:56:29 +0200

    improved query efficiency to bean directly and rewrote to inherit from RestNagiosPlugin to reduce code

--- check_hbase_regionserver_compaction_in_progress.py
+++ check_hbase_regionserver_compaction_in_progress.py
@@ -16,7 +16,7 @@
 
 """
 
-Nagios Plugin to check if an HBase major compaction is in progress on a given RegionServer via the RegionServer's JMX
+Nagios Plugin to check if an HBase major compaction is in progress on a given RegionServer via its JMX API
 
 Raises Warning if any compaction is running - use this to check that no major compactions get triggered during the day
 / peak hours.
@@ -26,7 +26,7 @@ compaction time.
 
 See also check_hbase_table_compaction_in_progress.py which checks for compactions on a table by table basis.
 
-Tested on Hortonworks HDP 2.3 (HBase 1.1.2) and Apache HBase 1.0.3, 1.1.6, 1.2.2
+Tested on Hortonworks HDP 2.3 (HBase 1.1.2) and Apache HBase 1.0.3, 1.1.6, 1.2.2, 1.3.1
 
 """
 
@@ -35,64 +35,44 @@ from __future__ import division
 from __future__ import print_function
 #from __future__ import unicode_literals
 
-import json
 import logging
 import os
 import sys
 import traceback
-try:
-    import requests
-except ImportError:
-    print(traceback.format_exc(), end='')
-    sys.exit(4)
 srcdir = os.path.abspath(os.path.dirname(__file__))
 libdir = os.path.join(srcdir, 'pylib')
 sys.path.append(libdir)
 try:
     # pylint: disable=wrong-import-position
     from harisekhon.utils import log, qquit, jsonpp
-    from harisekhon.utils import validate_host, validate_port
     from harisekhon.utils import support_msg_api, isInt
-    from harisekhon import NagiosPlugin
+    from harisekhon import RestNagiosPlugin
 except ImportError as _:
     print(traceback.format_exc(), end='')
     sys.exit(4)
 
 __author__ = 'Hari Sekhon'
-__version__ = '0.2'
+__version__ = '0.3'
 
 
-class CheckHBaseCompactionInProgress(NagiosPlugin):
+class CheckHBaseCompactionInProgress(RestNagiosPlugin):
 
     def __init__(self):
         # Python 2.x
         super(CheckHBaseCompactionInProgress, self).__init__()
         # Python 3.x
         # super().__init__()
+        self.name = 'HBase RegionServer'
+        self.default_host = 'localhost'
+        self.default_port = 16301
+        self.path = '/jmx?qry=Hadoop:service=HBase,name=RegionServer,sub=Server'
+        self.json = True
+        self.auth = False
         self.msg = 'msg not defined'
         self.ok()
 
-    def add_options(self):
-        self.add_hostoption(name='HBase RegionServer', default_host='localhost', default_port=16030)
-
-    def run(self):
-        self.no_args()
-        host = self.get_opt('host')
-        port = self.get_opt('port')
-        validate_host(host)
-        validate_port(port)
-
-        url = 'http://%(host)s:%(port)s/jmx' % locals()
-        log.debug('GET %s', url)
-        try:
-            req = requests.get(url)
-        except requests.exceptions.RequestException as _:
-            qquit('CRITICAL', _)
-        log.debug("response: %s %s", req.status_code, req.reason)
-        log.debug("content:\n%s\n%s\n%s", '='*80, req.content.strip(), '='*80)
-        if req.status_code != 200:
-            qquit('CRITICAL', "%s %s" % (req.status_code, req.reason))
-        compaction_queue_size = self.parse(req.content)
+    def parse_json(self, json_data):
+        compaction_queue_size = self.parse(json_data)
         self.msg = 'HBase RegionServer compaction '
         if compaction_queue_size > 0:
             self.warning()
@@ -103,13 +83,13 @@ class CheckHBaseCompactionInProgress(NagiosPlugin):
         self.msg += ' | compactionQueueSize={0};0;0'.format(compaction_queue_size)
 
     @staticmethod
-    def parse(content):
+    def parse(json_data):
         try:
-            _ = json.loads(content)
-            if log.isEnabledFor(logging.DEBUG):
-                log.debug('%s', jsonpp(_))
+            # it's already nicely layed out
+            #if log.isEnabledFor(logging.DEBUG):
+            #    log.debug('%s', jsonpp(json_data))
             compaction_queue_size = None
-            for bean in _['beans']:
+            for bean in json_data['beans']:
                 if bean['name'] == 'Hadoop:service=HBase,name=RegionServer,sub=Server':
                     if log.isEnabledFor(logging.DEBUG):
                         log.debug('found RegionServer section:')
@@ -118,7 +98,7 @@ class CheckHBaseCompactionInProgress(NagiosPlugin):
                     if not isInt(compaction_queue_size):
                         qquit('UNKNOWN', 'non-integer returned for compactionQueueLength! ' + support_msg_api())
                     return compaction_queue_size
-        except ValueError as _:
+        except KeyError as _:
             qquit('UNKNOWN', _ + ': failed to parse HBase Master jmx info. ' + support_msg_api())
         qquit('UNKNOWN', 'RegionServer mbean not found, double check this is pointing to an HBase RegionServer')
 
