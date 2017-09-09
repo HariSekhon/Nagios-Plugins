224c8fec24875edd5508810cfc66cdd151fc8379
Hari Sekhon <harisekhon@gmail.com>
2017-09-09 15:33:43 +0100

    updated test_dockerhub.sh

--- tests/test_dockerhub.sh
+++ tests/test_dockerhub.sh
@@ -29,14 +29,17 @@ section "DockerHub Plugins"
 
 # test one of the more stable automated builds
 echo "testing repo expected to have successful build:"
+echo "./check_dockerhub_repo_build_status.py -r harisekhon/apache-drill"
 ./check_dockerhub_repo_build_status.py -r harisekhon/apache-drill
 hr
 echo "testing repo for successful build with extended output information"
+echo "./check_dockerhub_repo_build_status.py -r harisekhon/apache-drill -v"
 ./check_dockerhub_repo_build_status.py -r harisekhon/apache-drill -v
 hr
 echo "testing detection of failing repo build"
 # intentionally broken repo created specifically for this test
 set +e
+echo "./check_dockerhub_repo_build_status.py -r harisekhon/ci_intentionally_broken_test_do_not_use -v"
 ./check_dockerhub_repo_build_status.py -r harisekhon/ci_intentionally_broken_test_do_not_use -v
 check_exit_code 2
 echo "successfully detected failing repo build"
