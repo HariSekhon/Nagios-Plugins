327f4f5e5dc00f02a1a6487b31a99dcdd0265500
Hari Sekhon <harisekhon@gmail.com>
2017-09-09 15:57:01 +0100

    added tests/test_gentoo.sh

---
+++ tests/test_gentoo.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-10-07 13:41:58 +0100 (Wed, 07 Oct 2015)
+#
+#  https://github.com/harisekhon/nagios-plugins
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$srcdir/..";
+
+. ./tests/utils.sh
+
+section "G e n t o o"
+
+export DOCKER_IMAGE="gentoo/stage3-amd64"
+export DOCKER_CONTAINER="nagios-plugins-gentoo-test"
+
+export MNTDIR="/tmp/nagios-plugins"
+
+if ! is_docker_available; then
+    echo 'WARNING: Docker not found, skipping Gentoo checks!!!'
+    exit 0
+fi
+
+docker_exec(){
+    local cmd="$@"
+    echo "docker exec '$DOCKER_CONTAINER' $MNTDIR/$*"
+    docker exec "$DOCKER_CONTAINER" $MNTDIR/$*
+}
+
+#trap "docker rm -f $DOCKER_CONTAINER &>/dev/null" SIGINT SIGTERM EXIT
+
+startupwait=0
+
+if ! is_docker_available; then
+    echo 'WARNING: Docker not found, skipping Gentoo checks!!!'
+    exit 0
+fi
+
+echo "Setting up Gentoo test container"
+DOCKER_OPTS="-v $srcdir/..:$MNTDIR"
+DOCKER_CMD="tail -f /dev/null"
+launch_container "$DOCKER_IMAGE" "$DOCKER_CONTAINER"
+#docker exec "$DOCKER_CONTAINER" yum makecache fast
+if [ -n "${NOTESTS:-}" ]; then
+    exit 0
+fi
+hr
+docker_exec older/check_gentoo_portage.py --all-updates
+hr
+docker_exec older/check_gentoo_portage.py --warn-on-any-update
+hr
+delete_container
+echo
+echo "All Gentoo tests completed successfully"
+echo
+echo
