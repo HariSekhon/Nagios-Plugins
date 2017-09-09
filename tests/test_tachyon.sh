fc087fc739a3d5108c31c798926eb4449c6db831
Hari Sekhon <harisekhon@gmail.com>
2017-09-09 17:51:27 +0100

    updated test_tachyon.sh

--- tests/test_tachyon.sh
+++ tests/test_tachyon.sh
@@ -21,11 +21,7 @@ cd "$srcdir/.."
 
 . "$srcdir/utils.sh"
 
-echo "
-# ============================================================================ #
-#                                 T a c h y o n
-# ============================================================================ #
-"
+section "T a c h y o n"
 
 # Tachyon 0.7 doesn't always start up properly, but has passed all the plugin tests
 #export TACHYON_VERSIONS="${@:-${TACHYON_VERSIONS:-latest 0.7 0.8}}"
@@ -36,12 +32,14 @@ TACHYON_HOST="${TACHYON_HOST##*/}"
 TACHYON_HOST="${TACHYON_HOST%%:*}"
 export TACHYON_HOST
 
-export TACHYON_MASTER_PORT="${TACHYON_MASTER_PORT:-19999}"
-export TACHYON_WORKER_PORT="${TACHYON_WORKER_PORT:-30000}"
+export TACHYON_MASTER_PORT_DEFAULT="${TACHYON_MASTER_PORT:-19999}"
+export TACHYON_WORKER_PORT_DEFAULT="${TACHYON_WORKER_PORT:-30000}"
 
 startupwait 15
 
 check_docker_available
+
+trap_debug_env tachyon 
 
 test_tachyon(){
     local version="$1"
@@ -50,10 +48,8 @@ test_tachyon(){
     hr
     #launch_container "$DOCKER_IMAGE:$version" "$DOCKER_CONTAINER" $TACHYON_MASTER_PORT $TACHYON_WORKER_PORT
     VERSION="$version" docker-compose up -d
-    tachyon_master_port="`docker-compose port "$DOCKER_SERVICE" "$TACHYON_MASTER_PORT" | sed 's/.*://'`"
-    tachyon_worker_port="`docker-compose port "$DOCKER_SERVICE" "$TACHYON_WORKER_PORT" | sed 's/.*://'`"
-    local TACHYON_MASTER_PORT="$tachyon_master_port"
-    local TACHYON_WORKER_PORT="$tachyon_worker_port"
+    export TACHYON_MASTER_PORT="`docker-compose port "$DOCKER_SERVICE" "$TACHYON_MASTER_PORT_DEFAULT" | sed 's/.*://'`"
+    export TACHYON_WORKER_PORT="`docker-compose port "$DOCKER_SERVICE" "$TACHYON_WORKER_PORT_DEFAULT" | sed 's/.*://'`"
     if [ -n "${NOTESTS:-}" ]; then
         exit 0
     fi
@@ -67,17 +63,23 @@ test_tachyon(){
         ./check_tachyon_master_version.py -v -e "$version" && break
         sleep 1
     done
+    echo "./check_tachyon_master_version.py -v -e '$version'"
     ./check_tachyon_master_version.py -v -e "$version"
     hr
+    echo "./check_tachyon_worker_version.py -v -e '$version'"
     ./check_tachyon_worker_version.py -v -e "$version"
     hr
+    echo "./check_tachyon_master.py -v"
     ./check_tachyon_master.py -v
     hr
     #docker exec -ti "$DOCKER_CONTAINER" ps -ef
+    echo "./check_tachyon_worker.py -v"
     ./check_tachyon_worker.py -v
     hr
+    echo "./check_tachyon_running_workers.py -v"
     ./check_tachyon_running_workers.py -v
     hr
+    echo "./check_tachyon_dead_workers.py -v"
     ./check_tachyon_dead_workers.py -v
     hr
     #delete_container
@@ -85,6 +87,4 @@ test_tachyon(){
     echo
 }
 
-for version in $(ci_sample $TACHYON_VERSIONS); do
-    test_tachyon $version
-done
+run_test_versions Tachyon
