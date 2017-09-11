1d8a170a2af69a422997b62380beffb41a071524
Hari Sekhon <harisekhon@gmail.com>
2017-09-11 16:27:50 +0200

    added check_docker_image.py tests

--- tests/test_docker.sh
+++ tests/test_docker.sh
@@ -32,9 +32,34 @@ if is_docker_available; then
     [ -n "${NO_PULL:-}" ] ||
         docker pull "$DOCKER_IMAGE"
     hr
-    ./check_docker_image_id.py --docker-image "$DOCKER_IMAGE:latest"
+    echo "./check_docker_image.py --docker-image $DOCKER_IMAGE:latest"
+    ./check_docker_image.py --docker-image "$DOCKER_IMAGE:latest"
     hr
-    ./check_docker_image_id.py --docker-image "$DOCKER_IMAGE:latest" --id "$(docker images | awk "/^${DOCKER_IMAGE//\//\\/}.*latest/{print \$3; exit}")"
+    echo "./check_docker_image.py --docker-image $DOCKER_IMAGE:latest --warning $((700 * 1024 * 1024))"
+    ./check_docker_image.py --docker-image "$DOCKER_IMAGE:latest" --warning $((700 * 1024 * 1024))
+    hr
+    echo "checking thresholds fail as expected:"
+    set +e
+    echo "./check_docker_image.py --docker-image $DOCKER_IMAGE:latest --warning $((300 * 1024 * 1024))"
+    ./check_docker_image.py --docker-image "$DOCKER_IMAGE:latest" --warning $((300 * 1024 * 1024))
+    check_exit_code 1
+    hr
+    echo "./check_docker_image.py --docker-image $DOCKER_IMAGE:latest --critical $((300 * 1024 * 1024))"
+    ./check_docker_image.py --docker-image "$DOCKER_IMAGE:latest" --critical $((300 * 1024 * 1024))
+    check_exit_code 2
+    set -e
+    hr
+    id="$(docker images | awk "/^${DOCKER_IMAGE//\//\\/}.*latest/{print \$3; exit}")"
+    echo "testing against expected id of $id"
+    echo "./check_docker_image.py --docker-image $DOCKER_IMAGE:latest --id $id"
+    ./check_docker_image.py --docker-image "$DOCKER_IMAGE:latest" --id "$id"
+    hr
+    echo "testing intentional id failure:"
+    echo "./check_docker_image.py --docker-image $DOCKER_IMAGE:latest --id wrongid"
+    set +e
+    ./check_docker_image.py --docker-image "$DOCKER_IMAGE:latest" --id "wrongid"
+    check_exit_code 2
+    set -e
     hr
     set +e
     echo "docker run --rm -e DEBUG='$DEBUG' '$DOCKER_IMAGE' check_ssl_cert.pl --help"
