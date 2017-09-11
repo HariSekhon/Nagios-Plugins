6afa5e53d139ef634315d12ca2424821542448f8
Hari Sekhon <harisekhon@gmail.com>
2017-09-11 12:46:13 +0200

    updated check_docker_image_id.py

--- check_docker_image_id.py
+++ check_docker_image_id.py
@@ -26,6 +26,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import logging
 import os
 import re
 import sys
@@ -55,6 +56,8 @@ class CheckDockerImageChecksum(NagiosPlugin):
         # super().__init__()
         self.ok()
         self.msg = 'docker msg not defined'
+        self.docker_image = None
+        self.expected_id = None
 
     def add_options(self):
         self.add_opt('-d', '--docker-image', help='Docker image, in form of <repository>:<tag>')
@@ -62,11 +65,11 @@ class CheckDockerImageChecksum(NagiosPlugin):
 
     def run(self):
         self.no_args()
-        docker_image = self.get_opt('docker_image')
-        expected_id = self.get_opt('id')
+        self.docker_image = self.get_opt('docker_image')
+        self.expected_id = self.get_opt('id')
         if not which('docker'):
             raise UnknownError("'docker' command not found in $PATH")
-        process = subprocess.Popen(['docker', 'images', '{repo}'.format(repo=docker_image)],
+        process = subprocess.Popen(['docker', 'images', '{repo}'.format(repo=self.docker_image)],
                                    stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE)
         (stdout, stderr) = process.communicate()
@@ -80,23 +83,41 @@ class CheckDockerImageChecksum(NagiosPlugin):
             raise UnknownError("exit code returned was '{0}': {1} {2}".format(exitcode, stdout, stderr))
         if not stdout:
             raise UnknownError('no output from docker images command!')
+        self.parse(stdout)
+
+    def parse(self, stdout):
         output = [_ for _ in stdout.split('\n') if _]
         if len(output) < 2:
-            raise CriticalError("docker image '{repo}' not found!".format(repo=docker_image))
+            raise CriticalError("docker image '{repo}' not found!".format(repo=self.docker_image))
+        col_start = len(self.docker_image)
+        if len(output) > 2:
+            tags = set([line[col_start:col_start + 10].strip() for line in output[1:]])
+            tags = [tag for tag in tags if tag != '<none>']
+            tags = sorted(list(tags))
+            if logging.isEnabledFor(log.DEBUG):
+                for tag in tags:
+                    log.debug('found tag: %s', tag)
+            raise UnknownError('too many results returned - did you forget to suffix a specific :tag to ' + \
+                               '--docker-image? (eg. :latest, :1.1). The following tags were found: ' + \
+                               ', '.join(tags)
+                              )
         header_line = output[0]
-        image_header = header_line[48:60].strip()
+        image_header = header_line[col_start + 10:col_start + 10 + 20].strip()
+        log.debug('image header column: %s', image_header)
         if image_header != 'IMAGE ID':
             raise UnknownError("3rd column in header '{0}' is not 'IMAGE ID' as expected, parsing failed!"\
                                .format(image_header))
-        _id = output[1][48:60].strip()
-        self.msg = "docker image '{repo}' id '{id}'".format(repo=docker_image, id=_id)
-        if not expected_id:
+        _id = output[1][col_start + 10:col_start + 10 + 20].strip()
+        log.debug('id: %s', _id)
+        self.msg = "docker image '{repo}' id '{id}'".format(repo=self.docker_image, id=_id)
+        if not self.expected_id:
+            log.debug('no --id given, not testing checksum')
             return
         if not re.match(r'sha\d+:\w+', _id):
             raise UnknownError("{msg} not in sha format as expected!".format(msg=self.msg))
-        if _id != expected_id:
+        if _id != self.expected_id:
             self.critical()
-            self.msg += " (expected '{0}')".format(expected_id)
+            self.msg += " (expected '{0}')".format(self.expected_id)
 
 
 if __name__ == '__main__':
