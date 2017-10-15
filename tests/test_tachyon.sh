1136a9bd33538f0718e71bb7a185214d4e548ea2
Hari Sekhon <harisekhon@gmail.com>
2017-10-15 12:44:04 +0200

    updated test_tachyon.sh

--- tests/test_tachyon.sh
+++ tests/test_tachyon.sh
@@ -44,7 +44,9 @@ trap_debug_env tachyon
 test_tachyon(){
     local version="$1"
     section2 "Setting up Tachyon $version test container"
-    VERSION="$version" docker-compose pull $docker_compose_quiet
+    if is_CI; then
+        VERSION="$version" docker-compose pull $docker_compose_quiet
+    fi
     VERSION="$version" docker-compose up -d
     echo "getting Tachyon dynamic port mappings:"
     printf "Tachyon Master port => "
