0fee6972f966aad08b766cf82341c4dfbad4e81f
Hari Sekhon <harisekhon@gmail.com>
2017-10-27 16:01:59 +0200

    simplified code by integrating with rest nagios plugin class

--- check_tachyon_running_workers.py
+++ check_tachyon_running_workers.py
@@ -34,7 +34,6 @@ import re
 import sys
 try:
     from bs4 import BeautifulSoup
-    import requests
 except ImportError as _:
     print(_)
     sys.exit(4)
@@ -42,9 +41,8 @@ libdir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'pylib'))
 sys.path.append(libdir)
 try:
     # pylint: disable=wrong-import-position
-    from harisekhon.utils import log, qquit
-    from harisekhon.utils import validate_host, validate_port
-    from harisekhon import NagiosPlugin
+    from harisekhon.utils import log, UnknownError
+    from harisekhon import RestNagiosPlugin
 except ImportError as _:
     print('module import failed: %s' % _, file=sys.stderr)
     print("Did you remember to build the project by running 'make'?", file=sys.stderr)
@@ -52,58 +50,49 @@ except ImportError as _:
     sys.exit(4)
 
 __author__ = 'Hari Sekhon'
-__version__ = '0.3'
+__version__ = '0.4.0'
 
 
-class CheckTachyonRunningWorkers(NagiosPlugin):
+class CheckTachyonRunningWorkers(RestNagiosPlugin):
 
     def __init__(self):
         # Python 2.x
         super(CheckTachyonRunningWorkers, self).__init__()
         # Python 3.x
         # super().__init__()
+        self.name = ['Tachyon Master', 'Tachyon']
         self.software = 'Tachyon'
+        self.default_port = 19999
+        self.path = '/home'
+        self.auth = False
+        self.json = False
+        self.msg = 'Tachyon msg not defined'
 
     def add_options(self):
-        self.add_hostoption(name='%s Master' % self.software,
-                            # TODO: ['Tachyon Master', 'Tachyon']. add_hostopt
-                            default_host='localhost',
-                            default_port=19999)
+        super(CheckTachyonRunningWorkers, self).add_options()
+        self.add_thresholds(default_critical=1)
 
-    def run(self):
-        self.no_args()
-        host = self.get_opt('host')
-        port = self.get_opt('port')
-        validate_host(host)
-        validate_port(port)
+    def process_options(self):
+        super(CheckTachyonRunningWorkers, self).process_options()
+        self.validate_thresholds(simple='lower')
 
-        log.info('querying %s Master', self.software)
-        url = 'http://%(host)s:%(port)s/home' % locals()
-        log.debug('GET %s', url)
-        try:
-            req = requests.get(url)
-        except requests.exceptions.RequestException as _:
-            qquit('CRITICAL', _)
-        log.debug("response: %s %s", req.status_code, req.reason)
-        log.debug("content:\n%s\n%s\n%s", '='*80, req.content.strip(), '='*80)
-        if req.status_code != 200:
-            qquit('CRITICAL', "%s %s" % (req.status_code, req.reason))
+    def parse(self, req):
         soup = BeautifulSoup(req.content, 'html.parser')
         try:
+            log.info('parsing %s page for number of running workers', self.path)
             running_workers = soup.find('th', text=re.compile(r'Running\s+Workers:?', re.I))\
                 .find_next_sibling().get_text()
         except (AttributeError, TypeError):
-            qquit('UNKNOWN', 'failed to find parse %s Master info for running workers' % self.software)
+            raise UnknownError('failed to find parse {0} Master info for running workers'.format(self.software))
         try:
             running_workers = int(running_workers)
         except (ValueError, TypeError):
-            qquit('UNKNOWN', '{0} Master live workers parsing returned non-integer: {1}'.
-                  format(self.software, running_workers))
-        self.msg = '{0} running workers = {1}'.format(self.software, running_workers)  # pylint: disable=attribute-defined-outside-init
-        self.ok()
-        # TODO: thresholds on number of live workers (coming soon)
-        if running_workers < 1:
-            self.critical()
+            raise UnknownError('{0} Master live workers parsing returned non-integer: {1}'.
+                               format(self.software, running_workers))
+        self.msg = '{0} running workers = {1}'.format(self.software, running_workers)
+        self.check_thresholds(running_workers)
+        self.msg += ' | '
+        self.msg += 'running_workers={0}{1}'.format(running_workers, self.get_perf_thresholds())
 
 
 if __name__ == '__main__':
