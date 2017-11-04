662de5f9c2fe39270bd6c8c98c1665e6c9a8ca21
Hari Sekhon <harisekhon@gmail.com>
2017-11-04 01:16:12 +0100

    collapsed code by subclassing CheckCouchDBDatabaseStats

--- check_couchdb_database_compaction_running.py
+++ check_couchdb_database_compaction_running.py
@@ -38,61 +38,24 @@ libdir = os.path.join(srcdir, 'pylib')
 sys.path.append(libdir)
 try:
     # pylint: disable=wrong-import-position
-    from harisekhon.utils import UnknownError, ERRORS, validate_chars, isList
-    from harisekhon import RestNagiosPlugin
+    from check_couchdb_database_stats import CheckCouchDBDatabaseStats
 except ImportError as _:
     print(traceback.format_exc(), end='')
     sys.exit(4)
 
 __author__ = 'Hari Sekhon'
-__version__ = '0.1'
+__version__ = '0.2'
 
 
-class CheckCouchDBDatabaseCompaction(RestNagiosPlugin):
+class CheckCouchDBDatabaseCompaction(CheckCouchDBDatabaseStats):
 
     def __init__(self):
         # Python 2.x
         super(CheckCouchDBDatabaseCompaction, self).__init__()
         # Python 3.x
         # super().__init__()
-        self.name = ['CouchDB', 'Couch']
-        self.default_port = 5984
-        self.path = None # set to /<database> further down
-        self.auth = False
-        self.json = True
-        self.msg = 'CouchDB database '
-        self.database = None
 
-    def add_options(self):
-        super(CheckCouchDBDatabaseCompaction, self).add_options()
-        self.add_opt('-d', '--database', help='Database to assert exists')
-        self.add_opt('-l', '--list', action='store_true', default=False, help='List databases and exit')
-        self.add_thresholds()
-
-    def process_options(self):
-        super(CheckCouchDBDatabaseCompaction, self).process_options()
-        if self.get_opt('list'):
-            self.path = '/_all_dbs'
-        else:
-            self.database = self.get_opt('database')
-            # lowercase characters (a-z), digits (0-9), and any of the characters _, $, (, ), +, -, and /
-            validate_chars(self.database, 'database', r'a-z0-9_\$\(\)\+\-/')
-            self.path = '/{0}'.format(self.database)
-        self.validate_thresholds(optional=True)
-
-    def parse_json(self, json_data):
-        if self.get_opt('list'):
-            if not isList(json_data):
-                raise UnknownError('non-list returned by CouchDB for databases')
-            databases = json_data
-            print('CouchDB databases:\n')
-            if databases:
-                for database in databases:
-                    print('{0}'.format(database))
-            else:
-                print('<none>')
-            sys.exit(ERRORS['UNKNOWN'])
-        assert json_data['db_name'] == self.database
+    def check_couchdb_stats(self, json_data):
         compact_running = json_data['compact_running']
         self.msg += "'{0}' compaction running = {1}".format(self.database, compact_running)
         if compact_running:
