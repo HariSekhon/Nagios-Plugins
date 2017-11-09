8ef50e9f87addb1fbae6acca41d2129c23cd0682
Hari Sekhon <harisekhon@gmail.com>
2017-11-09 17:04:17 +0100

    updated tested versions

--- check_presto_worker_nodes_failed.py
+++ check_presto_worker_nodes_failed.py
@@ -27,7 +27,7 @@ according to tests, so this can also be used to catch adding or restarting worke
 
 Tested on:
 
-- Presto Facebook versions:               0.152, 0.157, 0.167, 0.179, 0.185
+- Presto Facebook versions:               0.152, 0.157, 0.167, 0.179, 0.185, 0.187, 0.188
 - Presto Teradata distribution versions:  0.152, 0.157, 0.167, 0.179
 
 """
