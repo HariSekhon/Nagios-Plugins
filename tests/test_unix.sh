86b056b0b05004fb4c5ea9bda881f86def5e81a9
Hari Sekhon <harisekhon@gmail.com>
2017-11-09 15:44:03 +0100

    removed hr() calls

--- tests/test_unix.sh
+++ tests/test_unix.sh
@@ -24,43 +24,45 @@ cd "$srcdir/..";
 section "U n i x"
 
 run $perl -T ./check_disk_write.pl -d .
-hr
+
 run $perl -T ./check_git_branch_checkout.pl -d . -b "$(git branch | awk '/^\*/ {print $2; exit}')"
-hr
+
 run ./check_git_branch_checkout.py -d . -b "$(git branch | awk '/^\*/ {print $2; exit}')"
-hr
-echo "Testing failure detection of wrong git branch"
+
+echo "Testing failure detection of wrong git branch:"
 run_fail 2 $perl -t ./check_git_branch_checkout.pl -d . -b nonexistentbranch
-hr
+
 run_fail 2 ./check_git_branch_checkout.py -d . -b nonexistentbranch
-hr
+
 tmpfile="$(mktemp /tmp/check_file_checksum.txt.XXXXXX)"
 echo test > "$tmpfile"
 run $perl -T ./check_file_checksum.pl -f "$tmpfile" -v -c '4e1243bd22c66e76c2ba9eddc1f91394e57f9f83'
-hr
+
 run $perl -T ./check_file_checksum.pl -f "$tmpfile" -vn -a adler32
-hr
+
 run $perl -T ./check_file_adler32.pl  -f "$tmpfile" -v -c '062801cb'
-hr
+
 run $perl -T ./check_file_crc.pl      -f "$tmpfile" -v -c '3bb935c6'
-hr
+
 run $perl -T ./check_file_md5.pl      -f "$tmpfile" -v -c 'd8e8fca2dc0f896fd7cb4cb0031ba249'
-hr
+
 run $perl -T ./check_file_sha1.pl     -f "$tmpfile" -v --checksum '4e1243bd22c66e76c2ba9eddc1f91394e57f9f83'
-hr
+
 run $perl -T ./check_file_sha256.pl   -f "$tmpfile" -v -c 'f2ca1bb6c7e907d06dafe4687e579fce76b37e4e93b7605022da52e6ccc26fd2'
-hr
+
 run $perl -T ./check_file_sha512.pl   -f "$tmpfile" -v -c '0e3e75234abc68f4378a86b3f4b32a198ba301845b0cd6e50106e874345700cc6663a86c1ea125dc5e92be17c98f9a0f85ca9d5f595db2012f7cc3571945c123'
-rm -f "$tmpfile"
+
+rm -vf "$tmpfile"
 hr
+
 # test real login against HP iLO or similar if local environment is configured for it
 if [ -n "${SSH_HOST:-}" -a -n "${SSH_USER:-}" -a -n "${SSH_PASSWORD:-}" ]; then
     run $perl -T ./check_ssh_login.pl -H "$SSH_HOST" -u "$SSH_USER" -p "$SSH_PASSWORD"
 fi
-hr
+
 echo "check fails on non-existent user when SSH'ing localhost:"
 run_fail 2 $perl -T ./check_ssh_login.pl -H localhost -u check_ssh_login_nagios_plugin_test -p test
-hr
+
 set +e
 localtime="$(readlink /etc/localtime | sed 's/.*zoneinfo\///')"
 timezone="$(date +%Z)"
@@ -78,7 +80,7 @@ if [ $? -ne 0 ]; then
     set -eo pipefail
     run $perl -T ./check_timezone.pl --timezone "$localtime" --alternate "$timezone" --zoneinfo-file "$timezone_file"
 fi
-hr
+
 echo "Completed $run_count Unix tests"
 echo
 echo "All Unix tests passed successfully"
