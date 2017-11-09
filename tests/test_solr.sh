d93f53dc52b3c0de5e72d218dfb1521bc4f27abe
Hari Sekhon <harisekhon@gmail.com>
2017-11-09 15:33:57 +0100

    removed hr() calls and added NOSETUP support

--- tests/test_solr.sh
+++ tests/test_solr.sh
@@ -55,7 +55,7 @@ test_solr(){
     hr
     when_url_content "http://$SOLR_HOST:$SOLR_PORT/solr/" "Solr Admin"
     hr
-    if [ "$version" != "3.1" ]; then
+    if [ "$version" != "3.1" -a -z "${NOSETUP:-}" ]; then
         echo "attempting to create Solr Core"
         # excluding from 3.1 test due to following error:
         # rpc error: code = 2 desc = oci runtime error: exec failed: container_linux.go:247: starting container process caused "exec: \"solr\": executable file not found in $PATH"
@@ -63,61 +63,61 @@ test_solr(){
         hr
         echo "attempting to create Solr Collection"
         docker-compose exec "$DOCKER_SERVICE" "$SOLR_HOME/bin/post" -c "$SOLR_CORE" "$SOLR_HOME/example/exampledocs/money.xml" || :
+        echo "Setup done"
         hr
     fi
     if [ -n "${NOTESTS:-}" ]; then
         exit 0
     fi
-    echo "Setup done, starting checks ..."
     if [ "${version:0:1}" != "3" ]; then
         if [ "$version" = "latest" ]; then
             local version="$(dockerhub_latest_version solr)"
         fi
         # 4.x+
-        hr
         run ./check_solr_version.py -e "$version"
-        hr
+
         run_fail 2 ./check_solr_version.py -e "fail-version"
     else
         docker_compose_version_test solr "$version"
+        hr
     fi
-    hr
+
     run_conn_refused ./check_solr_version.py -e "$version"
-    hr
+
     # not available in Solr 3.x and collection not loaded in 4.10 above due to lack of bin/post command
     if ! [[ "$version" =~ ^3|^4 ]]; then
         run $perl -T ./check_solr_api_ping.pl -v -w 1000 -c 2000
-        hr
     fi
+
     run_conn_refused $perl -T ./check_solr_api_ping.pl -v -w 1000 -c 2000
-    hr
+
     if ! [[ "$version" =~ ^3|^4 ]]; then
         run $perl -T ./check_solr_metrics.pl --cat CACHE -K queryResultCache -s cumulative_hits
-        hr
     fi
+
     run_conn_refused $perl -T ./check_solr_metrics.pl --cat CACHE -K queryResultCache -s cumulative_hits
-    hr
+
     # core / collection not created above in versions < 5
     if ! [[ "$version" =~ ^3|^4 ]]; then
         run $perl -T ./check_solr_core.pl -v --index-size 100 --heap-size 100 --num-docs 10 -w 2000
-        hr
     fi
+
     run_conn_refused $perl -T ./check_solr_core.pl -v --index-size 100 --heap-size 100 --num-docs 10 -w 2000
-    hr
+
     num_expected_docs=4
     # docs are not loaded in 3.1 test
     [ "$version" = "3.1" ] && num_expected_docs=0
     # core / collection not created above in versions < 5
     if ! [[ "$version" =~ ^3|^4 ]]; then
         run $perl -T ./check_solr_query.pl -n 0:$num_expected_docs -w 200 -v
-        hr
+
         run $perl -T ./check_solr_write.pl -v -w 1000 # because Travis is slow
-        hr
     fi
+
     run_conn_refused $perl -T ./check_solr_query.pl -n 0:$num_expected_docs -w 200 -v
-    hr
+
     run_conn_refused $perl -T ./check_solr_write.pl -v -w 1000
-    hr
+
     echo "Completed $run_count Solr tests"
     hr
     [ -n "${KEEPDOCKER:-}" ] ||
