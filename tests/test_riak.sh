5c728dca6b8a984a3b7db3b598e4c8be562c2630
Hari Sekhon <harisekhon@gmail.com>
2017-11-09 15:28:01 +0100

    removed hr() calls and added NOSETUP support

--- tests/test_riak.sh
+++ tests/test_riak.sh
@@ -53,6 +53,7 @@ test_riak(){
         VERSION="$version" docker-compose pull $docker_compose_quiet
     fi
     VERSION="$version" docker-compose up -d
+    hr
     echo "getting Riak dynamic port mapping:"
     echo "Riak HTTP port => "
     export RIAK_PORT="`docker-compose port "$DOCKER_SERVICE" "$RIAK_PORT_DEFAULT" | sed 's/.*://'`"
@@ -63,37 +64,27 @@ test_riak(){
     when_url_content "http://$RIAK_HOST:$RIAK_PORT/ping" OK
     hr
     echo "waiting for up to $startupwait secs for Riak to come fully up:"
-    SECONDS=0
-    count=1
-    while true; do
-        echo -n "try $count:  "
-        if $perl -T check_riak_write.pl -v; then
-            break
-        fi
-        if ! [ $SECONDS -le $startupwait ]; then
-            echo "FAIL: Riak not ready after $startupwait secs"
-            exit 1
-        fi
-        let count+=1
-        sleep 1
-    done
+    retry $startupwait $perl -T check_riak_write.pl -v
     hr
-    # Riak 2.x
-    #echo "creating myBucket with n_val setting of 1 (to avoid warnings in riak-admin)"
-    #docker exec -ti -u riak "$DOCKER_CONTAINER" riak-admin bucket-type create myBucket '{"props":{"n_val":1}}' || :
-    #docker exec -ti -u riak "$DOCKER_CONTAINER" riak-admin bucket-type activate myBucket
-    #docker exec -ti -u riak "$DOCKER_CONTAINER" riak-admin bucket-type update myBucket '{"props":{"n_val":1}}'
-    echo "creating test Riak document"
-    # don't use new bucket types yet
-    #curl -XPUT localhost:8098/types/myType/buckets/myBucket/keys/myKey -d 'hari'
-    # This doesn't fail, returns
-    #
-    # Error:
-    # all_nodes_down
-    #
-    # relying on check_riak_write.pl iterating test above to latch until Riak is ready so this will succeed
-    curl -XPUT "$RIAK_HOST:$RIAK_PORT/buckets/myBucket/keys/myKey" -d 'hari'
-    echo "done"
+    if [ -z "${NOSETUP:-}" ]; then
+        # Riak 2.x
+        #echo "creating myBucket with n_val setting of 1 (to avoid warnings in riak-admin)"
+        #docker exec -ti -u riak "$DOCKER_CONTAINER" riak-admin bucket-type create myBucket '{"props":{"n_val":1}}' || :
+        #docker exec -ti -u riak "$DOCKER_CONTAINER" riak-admin bucket-type activate myBucket
+        #docker exec -ti -u riak "$DOCKER_CONTAINER" riak-admin bucket-type update myBucket '{"props":{"n_val":1}}'
+        echo "creating test Riak document"
+        # don't use new bucket types yet
+        #curl -XPUT localhost:8098/types/myType/buckets/myBucket/keys/myKey -d 'hari'
+        # This doesn't fail, returns
+        #
+        # Error:
+        # all_nodes_down
+        #
+        # relying on check_riak_write.pl iterating test above to latch until Riak is ready so this will succeed
+        curl -XPUT "$RIAK_HOST:$RIAK_PORT/buckets/myBucket/keys/myKey" -d 'hari'
+        echo "done"
+        hr
+    fi
     if [ -n "${NOTESTS:-}" ]; then
         exit 0
     fi
@@ -104,17 +95,17 @@ test_riak(){
         # TODO: fix and remove
         version=".*"
     fi
+    hr
     run $perl -T check_riak_version.pl -v -e "$version"
-    hr
+
     run_fail 2 $perl -T check_riak_version.pl -v -e 'fail-version'
-    hr
+
     run_conn_refused $perl -T check_riak_version.pl -v
-    hr
-    hr
+
     run $perl -T check_riak_api_ping.pl
-    hr
+
     run_conn_refused $perl -T check_riak_api_ping.pl
-    hr
+
     # riak-admin doesn't work in Dockerized environments, fails trying to stat '/proc/sys/net/core/wmem_default'
     #docker_exec check_riak_diag.pl --ignore-warnings -v
     # must attempt to check this locally if available - but may get "CRITICAL: 'riak-admin diag' returned 1 -  Node is not running!"
@@ -122,32 +113,33 @@ test_riak(){
         run_fail "0 2" $perl -T check_riak_diag.pl --ignore-warnings -v
     else
         echo "WARNING: riak-admin not available locally, skipping test of check_riak_diag.pl as it doesn't work in dockerized environments (fails to stat  '/proc/sys/net/core/wmem_default')"
+        hr
     fi
-    hr
+
     run $perl -T check_riak_key.pl -b myBucket -k myKey -e hari -v
-    hr
+
     run_conn_refused $perl -T check_riak_key.pl -b myBucket -k myKey -e hari -v
-    hr
+
     docker_exec check_riak_member_status.pl -v
-    hr
+
     docker_exec check_riak_ringready.pl -v
-    hr
+
     run $perl -T check_riak_stats.pl --all -v
-    hr
+
     run_conn_refused $perl -T check_riak_stats.pl --all -v
-    hr
+
     run $perl -T check_riak_stats.pl -s ring_num_partitions -c 64:64 -v
-    hr
+
     if [ "${version:0:1}" != 1 ]; then
         run $perl -T check_riak_stats.pl -s disk.0.size -c 1024: -v
     fi
-    hr
+
     run $perl -T check_riak_write.pl -v
-    hr
+
     run_conn_refused $perl -T check_riak_write.pl -v
-    hr
+
     docker_exec check_riak_write_local.pl -v
-    hr
+
     echo "Completed $run_count Riak tests"
     hr
     echo
