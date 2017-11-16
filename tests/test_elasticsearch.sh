9ba118e9da7bcd03ee1f32e70f314329f4c39d6b
Hari Sekhon <harisekhon@gmail.com>
2017-11-16 14:16:47 +0100

    updated test_elasticsearch.sh

--- tests/test_elasticsearch.sh
+++ tests/test_elasticsearch.sh
@@ -47,9 +47,7 @@ startupwait 120
 test_elasticsearch(){
     local version="$1"
     section2 "Setting up Elasticsearch $version test container"
-    if is_CI || [ -n "${DOCKER_PULL:-}" ]; then
-        VERSION="$version" docker-compose pull $docker_compose_quiet
-    fi
+    docker_compose_pull
     VERSION="$version" docker-compose up -d
     hr
     echo "getting Elasticsearch dynamic port mapping:"
