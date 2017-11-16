57407832f60b4631fedc48a74f534d774d1708e0
Hari Sekhon <harisekhon@gmail.com>
2017-11-16 14:18:34 +0100

    updated test_memcached.sh

--- tests/test_memcached.sh
+++ tests/test_memcached.sh
@@ -41,9 +41,7 @@ startupwait 1
 test_memcached(){
     local version="$1"
     section2 "Setting up Memcached $version test container"
-    if is_CI || [ -n "${DOCKER_PULL:-}" ]; then
-        VERSION="$version" docker-compose pull $docker_compose_quiet
-    fi
+    docker_compose_pull
     VERSION="$version" docker-compose up -d
     hr
     echo "getting Memcached dynamic port mapping:"
