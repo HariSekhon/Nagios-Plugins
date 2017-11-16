927c91810db1299abb7edeb9a7666e0f21508da4
Hari Sekhon <harisekhon@gmail.com>
2017-11-16 14:20:44 +0100

    updated test_tachyon.sh

--- tests/test_tachyon.sh
+++ tests/test_tachyon.sh
@@ -42,9 +42,7 @@ trap_debug_env tachyon
 test_tachyon(){
     local version="$1"
     section2 "Setting up Tachyon $version test container"
-    if is_CI || [ -n "${DOCKER_PULL:-}" ]; then
-        VERSION="$version" docker-compose pull $docker_compose_quiet
-    fi
+    docker_compose_pull
     if [ -z "${KEEPDOCKER:-}" ]; then
         docker-compose down || :
     fi
