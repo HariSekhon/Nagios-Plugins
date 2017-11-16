0ce7050e2aeb15c67871412f9d3827a2821df3b4
Hari Sekhon <harisekhon@gmail.com>
2017-11-16 14:20:34 +0100

    updated test_spark.sh

--- tests/test_spark.sh
+++ tests/test_spark.sh
@@ -46,9 +46,7 @@ test_spark(){
     local version="$1"
     section2 "Setting up Spark $version test container"
     docker-compose down &>/dev/null
-    if is_CI || [ -n "${DOCKER_PULL:-}" ]; then
-        VERSION="$version" docker-compose pull $docker_compose_quiet
-    fi
+    docker_compose_pull
     if [ -z "${KEEPDOCKER:-}" ]; then
         docker-compose down || :
     fi
