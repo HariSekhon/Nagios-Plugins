5b9e0268a0ca1e7dc2ea8e8b64600a3e57215578
Hari Sekhon <harisekhon@gmail.com>
2017-11-23 14:10:27 +0100

    added 1.11 test and query check to catch breaking JDK vs JRE requirement change in Apache Drill 1.10 as raised in https://github.com/HariSekhon/Dockerfiles/pull/15

--- tests/test_apache-drill.sh
+++ tests/test_apache-drill.sh
@@ -24,7 +24,7 @@ cd "$srcdir/.."
 
 section "A p a c h e   D r i l l"
 
-export APACHE_DRILL_VERSIONS="${@:-${APACHE_DRILL_VERSIONS:-latest 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 1.10}}"
+export APACHE_DRILL_VERSIONS="${@:-${APACHE_DRILL_VERSIONS:-latest 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 1.10 1.11}}"
 
 APACHE_DRILL_HOST="${DOCKER_HOST:-${APACHE_DRILL_HOST:-${HOST:-localhost}}}"
 APACHE_DRILL_HOST="${APACHE_DRILL_HOST##*/}"
@@ -67,6 +67,20 @@ test_apache_drill(){
     run $perl -T ./check_apache_drill_metrics.pl -v
 
     run_conn_refused $perl -T ./check_apache_drill_metrics.pl -v
+
+    # check container query capability is working
+    #
+    # Apache Drill 1.10 onwards requires JDK not JRE:
+    #
+    # https://github.com/HariSekhon/Dockerfiles/pull/15
+    #
+    # looks like the Apache Drill /status API doesn't reflect the break either, raised in:
+    #
+    # https://issues.apache.org/jira/browse/DRILL-5990
+    #
+    #docker_exec sqlline -u jdbc:drill:zk=zookeeper <<< "select * from sys.options limit 1;"
+    # more reliable for some versions of drill eg. 0.7
+    docker_exec sqlline -u jdbc:drill:zk=zookeeper -f /dev/stdin <<< "select * from sys.options limit 1;"
 
     echo "Completed $run_count Apache Drill tests"
     hr
