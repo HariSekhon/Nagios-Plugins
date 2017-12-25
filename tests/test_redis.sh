e954037b93389d6c39336e507be20d5ce9d3ec41
Hari Sekhon <harisekhon@gmail.com>
2017-12-25 18:26:23 +0000

    updated test_redis.sh

--- tests/test_redis.sh
+++ tests/test_redis.sh
@@ -30,7 +30,8 @@ REDIS_HOST="${REDIS_HOST##*/}"
 REDIS_HOST="${REDIS_HOST%%:*}"
 export REDIS_HOST
 
-export REDIS_PORT_DEFAULT="6379"
+export REDIS_PORT_DEFAULT=6379
+export HAPROXY_PORT_DEFAULT=6379
 
 #export REDIS_PASSWORD="testpass123"
 unset REDIS_PASSWORD
@@ -51,8 +52,9 @@ test_redis(){
     hr
     echo "getting Redis dynamic port mapping:"
     docker_compose_port Redis
+    DOCKER_SERVICE=redis-haproxy docker_compose_port HAProxy
     hr
-    when_ports_available "$REDIS_HOST" "$REDIS_PORT"
+    when_ports_available "$REDIS_HOST" "$REDIS_PORT" "$HAPROXY_PORT"
     hr
     if [ -z "${NOSETUP:-}" ]; then
         echo "creating test Redis key-value"
@@ -66,20 +68,36 @@ EOF
     if [ -n "${NOTESTS:-}" ]; then
         exit 0
     fi
-    local version="${version%%-*}"
+    local expected_version="${version%%-*}"
     if [ "$version" = "latest" ]; then
         local version=".*"
     fi
+
+    redis_tests
+
+    redis_test_conn_refused
+
+    echo
+    section2 "HAProxy Redis tests:"
+    echo
+
+    REDIS_PORT="$HAPROXY_PORT" \
+    redis_tests
+
+    [ -z "${NODELETE:-}" ] && rm -v /tmp/.check_redis_config.conf
+
+    echo "Completed $run_count Redis tests"
+    hr
+    [ -n "${KEEPDOCKER:-}" ] ||
+    docker-compose down
+    hr
+    echo
+}
+
+redis_tests(){
     run $perl -T ./check_redis_version.pl -v # -e "$version"  TODO: change to regex and enable this with .* for latest
 
     run_fail 2 $perl -T ./check_redis_version.pl -v -e 'fail-version'
-
-    run_conn_refused $perl -T ./check_redis_version.pl -v
-
-    # REDIS_HOST obtained via .travis.yml
-    run $perl -T ./check_redis_clients.pl -v
-
-    run_conn_refused $perl -T ./check_redis_clients.pl -v
 
     # there is no redis.conf in the Docker container :-/
     #docker cp "$DOCKER_CONTAINER":/etc/redis.conf /tmp/redis.conf
@@ -92,39 +110,31 @@ EOF
     $perl -T ./check_redis_config.pl -H $REDIS_HOST -C /tmp/.check_redis_config.conf --no-warn-extra -v | grep -v -e '^debug:' | sed 's/.*extra config found on running server://;s/=/ /g' | tr ',' '\n' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' | tee /tmp/.check_redis_config.conf
     run $perl -T ./check_redis_config.pl -H $REDIS_HOST -C /tmp/.check_redis_config.conf --no-warn-extra -vv
 
-    run_conn_refused $perl -T ./check_redis_config.pl -H $REDIS_HOST -C /tmp/.check_redis_config.conf
-
-    [ -z "${NODELETE:-1}" ] && rm -v /tmp/.check_redis_config.conf
-
-    run $perl -T ./check_redis_key.pl -k myKey -e hari -v
-
-    run_conn_refused $perl -T ./check_redis_key.pl -k myKey -e hari -v
-
-    run $perl -T ./check_redis_publish_subscribe.pl -v
-
-    run_conn_refused $perl -T ./check_redis_publish_subscribe.pl -v
+    run $perl -T ./check_redis_clients.pl -v
 
     run $perl -T ./check_redis_stats.pl -v
 
-    run_conn_refused $perl -T ./check_redis_stats.pl -v
+    run $perl -T ./check_redis_key.pl -k myKey -e hari -v
+
+    run $perl -T ./check_redis_publish_subscribe.pl -v
 
     run $perl -T ./check_redis_stats.pl -s connected_clients -c 1:1 -v
 
-    run_conn_refused $perl -T ./check_redis_stats.pl -s connected_clients -c 1:1 -v
-
     run $perl -T ./check_redis_write.pl -v
-
-    run_conn_refused $perl -T ./check_redis_write.pl -v
 
     echo "checking for no code failure masking root cause in catch quit handler"
     ERRCODE=2 run_grep 'Connection refused' $perl -T ./check_redis_stats.pl -P 9999 -s connected_clients -c 1:1 -v
+}
 
-    echo "Completed $run_count Redis tests"
-    hr
-    [ -n "${KEEPDOCKER:-}" ] ||
-    docker-compose down
-    hr
-    echo
+redis_test_conn_refused(){
+    run_conn_refused $perl -T ./check_redis_version.pl -v
+    run_conn_refused $perl -T ./check_redis_config.pl -H $REDIS_HOST -C /tmp/.check_redis_config.conf
+    run_conn_refused $perl -T ./check_redis_clients.pl -v
+    run_conn_refused $perl -T ./check_redis_stats.pl -v
+    run_conn_refused $perl -T ./check_redis_key.pl -k myKey -e hari -v
+    run_conn_refused $perl -T ./check_redis_publish_subscribe.pl -v
+    run_conn_refused $perl -T ./check_redis_stats.pl -s connected_clients -c 1:1 -v
+    run_conn_refused $perl -T ./check_redis_write.pl -v
 }
 
 run_test_versions Redis
