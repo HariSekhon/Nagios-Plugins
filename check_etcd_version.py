d46c1e4b2348b16b0d5db9183656c08090d7b2ef
Hari Sekhon <harisekhon@gmail.com>
2018-03-17 17:15:20 +0000

    shrunk code using RestVersionNagiosPlugin class and added support for parsing 2.0 version

--- check_etcd_version.py
+++ check_etcd_version.py
@@ -16,9 +16,9 @@
 
 """
 
-Nagios Plugin to check the deployed version of Etcd
+Nagios Plugin to check the Etcd server version via its Rest API
 
-Tested on Etcd 3.0.15
+Tested on Etcd 2.0, 2.1, 2.2, 2.3, 3.0
 
 """
 
@@ -32,95 +32,60 @@ import os
 import re
 import sys
 import traceback
-try:
-    import requests
-except ImportError:
-    print(traceback.format_exc(), end='')
-    sys.exit(4)
 srcdir = os.path.abspath(os.path.dirname(__file__))
 libdir = os.path.join(srcdir, 'pylib')
 sys.path.append(libdir)
 try:
     # pylint: disable=wrong-import-position
-    from harisekhon.utils import log, qquit, support_msg_api
-    from harisekhon.utils import validate_host, validate_port, validate_regex, isVersion
-    from harisekhon import NagiosPlugin
+    from harisekhon.utils import UnknownError, support_msg_api, version_regex
+    from harisekhon import RestVersionNagiosPlugin
 except ImportError as _:
     print(traceback.format_exc(), end='')
     sys.exit(4)
 
 __author__ = 'Hari Sekhon'
-__version__ = '0.1'
+__version__ = '0.2'
 
 
-class CheckEtcdVersion(NagiosPlugin):
+class CheckEtcdVersion(RestVersionNagiosPlugin):
 
     def __init__(self):
         # Python 2.x
         super(CheckEtcdVersion, self).__init__()
         # Python 3.x
         # super().__init__()
-        self.software = 'Etcd'
+        self.name = 'Etcd'
         self.default_host = 'localhost'
         self.default_port = 2379
-        self.host = None
-        self.port = None
-        self.expected = None
-        self.msg = '{0} version unknown - no message defined'.format(self.software)
+        self.path = '/version'
+        # Etcd2.1+ response is json
+        # Etcd2.0 response is non-json so parse and handle manually to cover both
+        self.json = False
+        self.auth = False
+        self.cluster_version = None
+        self.msg = 'Etcd msg not defined yet'
         self.ok()
 
-    def add_options(self):
-        self.add_hostoption(name=self.software,
-                            default_host=self.default_host,
-                            default_port=self.default_port)
-        self.add_opt('-e', '--expected', help='Expected version regex (optional)')
-
-    def process_options(self):
-        self.no_args()
-        self.host = self.get_opt('host')
-        self.port = self.get_opt('port')
-        validate_host(self.host)
-        validate_port(self.port)
-        self.expected = self.get_opt('expected')
-        if self.expected is not None:
-            validate_regex(self.expected)
-            log.info('expected version regex: %s', self.expected)
-
-    def run(self):
-        (version, cluster_version) = self.get_version()
-        if not isVersion(version):
-            qquit('UNKNOWN', '{0} version unrecognized \'{1}\'. {2}'\
-                             .format(self.software, version, support_msg_api()))
-        if not isVersion(cluster_version):
-            qquit('UNKNOWN', '{0} cluster version unrecognized \'{1}\'. {2}'\
-                             .format(self.software, cluster_version, support_msg_api()))
-        self.msg = '{0} version = {1}'.format(self.software, version)
-        if self.expected is not None and not re.search(self.expected, version):
-            self.msg += " (expected '{0}')".format(self.expected)
-            self.critical()
-        #super(CheckEtcdVersion, self).run()
-        self.msg += ', cluster version = {0}'.format(cluster_version)
-
-    def get_version(self):
-        log.info('querying %s', self.software)
-        url = 'http://{host}:{port}/version'.format(host=self.host, port=self.port)
-        log.debug('GET %s', url)
-        try:
-            req = requests.get(url)
-        except requests.exceptions.RequestException as _:
-            qquit('CRITICAL', _)
-        log.debug("response: %s %s", req.status_code, req.reason)
-        log.debug("content:\n%s\n%s\n%s", '='*80, req.content.strip(), '='*80)
-        if req.status_code != 200:
-            qquit('CRITICAL', '{0}: {1}'.format(req.status_code, req.reason))
+    def parse(self, req):
         try:
             json_dict = json.loads(req.content)
-            version = json_dict['etcdserver']
-            cluster_version = json_dict['etcdcluster']
-        except KeyError as _:
-            qquit('UNKNOWN', 'error parsing output from {software}: {error}. {support_msg}'\
-                             .format(software=self.software, error=_, support_msg=support_msg_api()))
-        return (version, cluster_version)
+        except ValueError:
+            # probably Etcd2 - parse version manually
+            _ = re.match('^etcd ({})$'.format(version_regex), req.content)
+            if _:
+                version = _.group(1)
+                return version
+            else:
+                raise UnknownError('non-json response returned (not Etcd3?) ' + \
+                                   'and could not parse Etcd2 version either. {}'.format(support_msg_api()))
+        version = json_dict['etcdserver']
+        self.cluster_version = json_dict['etcdcluster']
+        return version
+
+    def extra_info(self):
+        if self.cluster_version is not None:
+            return ', cluster version = {0}'.format(self.cluster_version)
+        return ''
 
 
 if __name__ == '__main__':
