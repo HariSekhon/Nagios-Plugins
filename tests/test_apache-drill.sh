d19f0c3ad28a371a993b671108b429151dedb9af
Hari Sekhon <harisekhon@gmail.com>
2018-05-09 21:28:21 +0100

    updated test_apache-drill.sh

--- tests/test_apache-drill.sh
+++ tests/test_apache-drill.sh
@@ -24,7 +24,7 @@ cd "$srcdir/.."
 
 section "A p a c h e   D r i l l"
 
-export APACHE_DRILL_VERSIONS="${@:-${APACHE_DRILL_VERSIONS:-latest 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 1.10 1.11}}"
+export APACHE_DRILL_VERSIONS="${@:-${APACHE_DRILL_VERSIONS:-latest 0.7 0.8 0.9 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 1.10 1.11 1.12 1.13 latest}}"
 
 APACHE_DRILL_HOST="${DOCKER_HOST:-${APACHE_DRILL_HOST:-${HOST:-localhost}}}"
 APACHE_DRILL_HOST="${APACHE_DRILL_HOST##*/}"
@@ -77,17 +77,90 @@ test_apache_drill(){
 }
 
 test_drill(){
-    #run ./check_apache_drill_version.py -v -e "$version"
+    expected_version="$version"
+    if [ "$version" = "latest" ]; then
+        expected_version=".*"
+    fi
+    # API endpoint not available in 0.7
+    if [ "$version" != "0.7" ]; then
+        run ./check_apache_drill_version.py -v -e "$expected_version"
+    fi
 
-    #run_fail 2 ./check_apache_drill_version.py -v -e "fail-version"
+    run_fail 2 ./check_apache_drill_version.py -v -e "fail-version"
+
+    run_conn_refused ./check_apache_drill_version.py -v
+
+    # ============================================================================ #
 
     run ./check_apache_drill_status.py -v
 
     run_conn_refused ./check_apache_drill_status.py -v
 
+    # ============================================================================ #
+
+    # API endpoint not available in 0.7
+    if [ "$version" != "0.7" ]; then
+        run ./check_apache_drill_status2.py -v
+    fi
+
+    run_conn_refused ./check_apache_drill_status2.py -v
+
+    # ============================================================================ #
+
+    # API endpoint not available in 0.7
+    if [ "$version" != "0.7" ]; then
+        run ./check_apache_drill_cluster_nodes.py -v -w 1
+
+        run_fail 1 ./check_apache_drill_cluster_nodes.py -v
+    fi
+
+    run_fail 2 ./check_apache_drill_cluster_nodes.py -v -w 3 -c 2
+
+    run_conn_refused ./check_apache_drill_cluster_nodes.py -v
+
+    # ============================================================================ #
+
+    # Encryption is available only in Apache Drill 1.11+
+    if [ "$version" = "latest" ] || [[ "$version" > 1.10 ]]; then
+        run_fail 2 ./check_apache_drill_encryption_enabled.py
+    fi
+
+    run_conn_refused ./check_apache_drill_encryption_enabled.py
+
+    # ============================================================================ #
+
+    # API endpoint not available in 0.7
+    if [ "$version" != "0.7" ]; then
+        run ./check_apache_drill_cluster_mismatched_versions.py
+    fi
+
+    run_conn_refused ./check_apache_drill_cluster_mismatched_versions.py
+
+    # ============================================================================ #
+
     run $perl -T ./check_apache_drill_metrics.pl -v
 
     run_conn_refused $perl -T ./check_apache_drill_metrics.pl -v
+
+    # ============================================================================ #
+
+    run_fail 3 ./check_apache_drill_storage_plugin.py --list
+
+    run ./check_apache_drill_storage_plugin.py --name dfs
+
+    run ./check_apache_drill_storage_plugin.py --name dfs --type file
+
+    run_fail 2 ./check_apache_drill_storage_plugin.py --name dfs --type wrong
+
+    run ./check_apache_drill_storage_plugin.py -n cp --type file
+
+    run_fail 2 ./check_apache_drill_storage_plugin.py -n hive --type hive
+
+    run_fail 2 ./check_apache_drill_storage_plugin.py -n hbase --type hbase
+
+    run_fail 2 ./check_apache_drill_storage_plugin.py -n mongo --type mongo
+
+    # ============================================================================ #
 
     # check container query capability is working
     #
@@ -101,10 +174,11 @@ test_drill(){
     #
     #docker_exec sqlline -u jdbc:drill:zk=zookeeper <<< "select * from sys.options limit 1;"
     # more reliable for some versions of drill eg. 0.7
-    docker_exec sqlline -u jdbc:drill:zk=zookeeper -f /dev/stdin <<< "select * from sys.options limit 1;"
+    #docker_exec sqlline -u jdbc:drill:zk=zookeeper -f /dev/stdin <<< "select * from sys.options limit 1;"
+    docker_exec sqlline -u jdbc:drill:zk=zookeeper -f /dev/stdin <<< "select * from sys.drillbits;"
 
 }
 
-startupwait 50
+startupwait 70
 
 run_test_versions "Apache Drill"
