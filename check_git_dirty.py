7343c29b36c4ea1168197dd17bca498abdc27494
Hari Sekhon <harisekhon@gmail.com>
2018-07-02 23:17:15 +0100

    added check_git_dirty.py

---
+++ check_git_dirty.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2016-07-25 14:57:36 +0100 (Mon, 25 Jul 2016)
+#
+#  https://github.com/harisekhon/nagios-plugins
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+"""
+
+Nagios Plugin to check a Git checking working directory isn't 'dirty' - ie has no uncommitted changes
+(doesn't count new uncommitted files though)
+
+Written for environments where deployment servers are running off git checkouts
+to ensure that the files haven't been modified and not backported
+
+Requires the 'git' command in the $PATH, otherwise you can set the path to the git
+executable using the environment variable GIT_PYTHON_GIT_EXECUTABLE
+
+See also check_git_branch_checkout.py
+
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+#from __future__ import unicode_literals
+
+import os
+import sys
+import traceback
+import git
+srcdir = os.path.abspath(os.path.dirname(__file__))
+libdir = os.path.join(srcdir, 'pylib')
+sys.path.append(libdir)
+try:
+    # pylint: disable=wrong-import-position
+    from git import InvalidGitRepositoryError
+    from harisekhon.utils import CriticalError, validate_directory
+    from harisekhon import NagiosPlugin
+except ImportError as _:
+    print(traceback.format_exc(), end='')
+    sys.exit(4)
+
+__author__ = 'Hari Sekhon'
+__version__ = '0.1'
+
+
+class CheckGitDirty(NagiosPlugin):
+
+    def __init__(self):
+        # Python 2.x
+        super(CheckGitDirty, self).__init__()
+        # Python 3.x
+        # super().__init__()
+        self.msg = 'CheckGitDirty msg not defined'
+        self.ok()
+
+    def add_options(self):
+        self.add_opt('-d', '--directory', action='store', help='Path to git checkout directory')
+
+    def run(self):
+        self.no_args()
+        directory = self.get_opt('directory')
+        validate_directory(directory)
+        directory = os.path.abspath(directory)
+        try:
+            repo = git.Repo(directory)
+        except InvalidGitRepositoryError:
+            raise CriticalError("directory '{}' does not contain a valid Git repository!".format(directory))
+        is_dirty = repo.is_dirty()
+        self.msg = "git checkout dirty = '{}' for directory '{}'".format(is_dirty, directory)
+        if is_dirty:
+            self.critical()
+
+
+if __name__ == '__main__':
+    CheckGitDirty().main()
