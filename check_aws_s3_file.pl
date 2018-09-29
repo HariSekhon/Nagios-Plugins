478530cbc8cca831a7b7ed02e6000bed9eb7b8e6
Hari Sekhon <harisekhon@gmail.com>
2018-09-29 11:26:45 +0100

    added --host/--port support (#175) and added bucket to url so that it works with Mino too

--- check_aws_s3_file.pl
+++ check_aws_s3_file.pl
@@ -11,9 +11,14 @@
 
 $DESCRIPTION = "Nagios Plugin to check if a given file is present in AWS S3 via the HTTP Rest API
 
-Bucket names must follow the more restrictive 3 to 63 alphanumeric character international standard, dots are not supported in the bucket name due to using strict DNS shortname regex validation";
+Useful for checking latest ETL files are available or _SUCCESS type files that a job has completed
 
-$VERSION = "0.4.1";
+Bucket names must follow the more restrictive 3 to 63 alphanumeric character international standard, dots are not supported in the bucket name due to using strict DNS shortname regex validation
+
+Tested on AWS S3 and Minio (open source private cloud S3 storage)
+";
+
+$VERSION = "0.5";
 
 use strict;
 use warnings;
@@ -32,7 +37,10 @@ use XML::Simple;
 our $ua = LWP::UserAgent->new;
 $ua->agent("Hari Sekhon $progname version $main::VERSION");
 
-my $aws_host = "s3.amazonaws.com";
+env_creds('AWS');
+
+set_host_default("s3.amazonaws.com");
+set_port_default(443);
 my $bucket;
 my $file;
 
@@ -45,16 +53,17 @@ my $ssl_ca_path;
 my $ssl_noverify;
 
 %options = (
-    "f|file=s"         => [ \$file,             "AWS S3 object path for the file" ],
+    %hostoptions,
     "b|bucket=s"       => [ \$bucket,           "AWS S3 bucket" ],
+    "f|file=s"         => [ \$file,             "AWS S3 object path for the file" ],
     "aws-access-key=s" => [ \$aws_access_key,   "\$AWS_ACCESS_KEY - can be passed on command line or preferably taken from environment variable" ],
     "aws-secret-key=s" => [ \$aws_secret_key,   "\$AWS_SECRET_KEY - can be passed on command line or preferably taken from environment variable" ],
     "G|get"            => [ \$GET,              "Perform full HTTP GET request instead of default HTTP HEAD. This will download the whole file, useful if you want to see the full download time from AWS S3. You may need to increase the --timeout to fetch file if more than a few MB" ],
-    "no-ssl"           => [ \$no_ssl,           "Don't use SSL, connect to AWS S3 with plaintext HTTP instead of HTTPS (not recommended)" ],
+    "no-ssl"           => [ \$no_ssl,           "Don't use SSL, connect to AWS S3 with plaintext HTTP instead of HTTPS (not recommended unless you're using a private cloud storage like Minio)" ],
     "ssl-CA-path=s"    => [ \$ssl_ca_path,      "Path to CA certificate directory for validating SSL certificate" ],
     "ssl-noverify"     => [ \$ssl_noverify,     "Do not verify SSL certificate from AWS S3 (not recommended)" ],
 );
-@usage_order = qw/file bucket aws-access-key aws-secret-key get no-ssl ssl-CA-path ssl-noverify/;
+@usage_order = qw/host port bucket file aws-access-key aws-secret-key get no-ssl ssl-CA-path ssl-noverify/;
 
 if(not defined($aws_access_key) and defined($ENV{"AWS_ACCESS_KEY"})){
     $aws_access_key = $ENV{"AWS_ACCESS_KEY"};
@@ -65,7 +74,8 @@ if(not defined($aws_secret_key) and defined($ENV{"AWS_SECRET_KEY"})){
 
 get_options();
 
-$aws_host       = validate_host($aws_host);
+$host           = validate_host($host);
+$port           = validate_port($port);
 $file           = validate_filename($file);
 $bucket         = validate_aws_bucket($bucket);
 $aws_access_key = validate_aws_access_key($aws_access_key);
@@ -83,6 +93,9 @@ if(defined($ssl_ca_path)){
 }
 if($no_ssl){
     vlog_option "ssl enabled",  "false";
+    if($port eq 443){
+        $port = 80;
+    }
 } else {
     vlog_option "ssl enabled",  "true";
     vlog_option "SSL CA Path",  $ssl_ca_path  if defined($ssl_ca_path);
@@ -97,19 +110,18 @@ $ua->show_progress(1) if $debug;
 
 $status = "OK";
 
-my $host_header = "$bucket.$aws_host";
+my $host_header = "$bucket.$host";
 my $date_header = strftime("%a, %d %b %Y %T %z", gmtime);
 
 $file =~ s/^\///;
 
 my $protocol = "https";
 $protocol = "http" if $no_ssl;
-my $url = "$protocol://$aws_host/$file";
+my $url = "$protocol://$host:$port/$bucket/$file";
 
 my $request_type = "HEAD";
 $request_type = "GET" if $GET;
 
-# very tricky, had to read the docs to get this
 # http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html
 my $canonicalized_string = "$request_type\n\n\n$date_header\n/$bucket/$file";
 # converts in place
@@ -124,7 +136,7 @@ my $signature = encode_base64(hmac_sha1($canonicalized_string, $aws_secret_key))
 my $authorization_header = "AWS $aws_access_key:$signature";
 $request->header("Authorization" => $authorization_header);
 
-validate_resolvable($aws_host);
+validate_resolvable($host);
 vlog2 "querying $request_type $url";
 my $start_time = time;
 my $res = $ua->request($request);
@@ -139,14 +151,14 @@ if($res->code eq 200){
     } else {
         $msg = "verified file '$file' exists in";
     }
-    $msg .= " $aws_host in $time_taken secs | time_taken=${time_taken}s";
+    $msg .= " $host in $time_taken secs | time_taken=${time_taken}s";
 } else {
     critical;
     my $data;
     if($res->content){
         $data = XMLin($res->content, forcearray => 1, keyattr => []);
     }
-    $msg = "failed to retrieve file '$file' from $aws_host: " . $res->status_line;
+    $msg = "failed to retrieve file '$file' from $host: " . $res->status_line;
     if(defined($data)){
         $msg .= " - " . $data->{Message}[0];
     }
