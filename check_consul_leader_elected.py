d10d8d96a95502c0ca37b0a0b026e108ca1052d0
Hari Sekhon <harisekhon@gmail.com>
2018-12-27 23:28:47 +0000

    updated to tested on Consul 1.4

--- check_consul_leader_elected.py
+++ check_consul_leader_elected.py
@@ -18,7 +18,7 @@
 
 Nagios Plugin to check Consul has a leader elected (key writes will fail otherwise)
 
-Tested on Consul 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3
+Tested on Consul 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4
 
 """
 
