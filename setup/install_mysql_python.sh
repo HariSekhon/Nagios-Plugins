77a4b0ecea8c6bd7efa99fdc9aa6bc09eb8710a9
Hari Sekhon <harisekhon@gmail.com>
2019-09-16 15:53:30 +0100

    added install_mysql_python.sh

---
+++ setup/install_mysql_python.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#
+#  Author: Hari Sekhon
+#  Date: 2019-09-16
+#
+#  https://github.com/harisekhon/devops-bash-tools
+#
+#  License: see accompanying LICENSE file
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Installs MySQL-python dependencies on Mac as needs 2 packages that conflict
+#
+# solution from:
+#
+# https://stackoverflow.com/a/51483898
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+if [ "$(uname -s)" = Darwin ]; then
+    echo "Running workaround installation for MySQL-Python on Mac OS X"
+    set -x
+    brew install --force mysql
+    brew unlink mysql
+    brew install --force mysql-connector-c
+    brew link mysql-connector-c
+    #sed -i -e 's/libs="$libs -l "/libs="$libs -lmysqlclient -lssl -lcrypto"/g' /usr/local/bin/mysql_config
+    export OPENSSL_INCLUDE=/usr/local/opt/openssl/include
+    export OPENSSL_LIB=/usr/local/opt/openssl/lib
+    export LIBRARY_PATH="${LIBRARY_PATH:-}:/usr/local/opt/openssl/lib/"
+    pip install --user MySQL-python
+    brew unlink mysql-connector-c
+    brew link --overwrite mysql
+    python -c 'import MySQLdb'
+    echo "SUCCESS!!"
+else
+    echo "MySQL-Python workaround install only needed on Mac, install via regular methods on other platforms"
+fi
