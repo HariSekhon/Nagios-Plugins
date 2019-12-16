7e52cd7878a1de51ce5bb06dc9b686463f696fcd
Hari Sekhon <harisekhon@gmail.com>
2019-12-16 17:10:33 +0000

    updated check_aws_access_keys_age.py

--- check_aws_access_keys_age.py
+++ check_aws_access_keys_age.py
@@ -24,6 +24,8 @@ Iterates all AWS IAM users so if you have a lot of users you will need to increa
 
 Verbose mode will output the users, key status, key created date and age in days
 
+See also aws_users_access_key_age.py from DevOps Python Tools repo if you just want a list - https://github.com/harisekhon/devops-python-tools
+
 """
 
 from __future__ import absolute_import
