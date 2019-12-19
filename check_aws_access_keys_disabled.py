5315b848c70a55d968c6a91a8823d563c100ceef
Hari Sekhon <harisekhon@gmail.com>
2019-12-19 11:00:39 +0000

    updated check_aws_access_keys_disabled.py

--- check_aws_access_keys_disabled.py
+++ check_aws_access_keys_disabled.py
@@ -22,7 +22,7 @@ Iterates all AWS IAM users so if you have a lot of users you will need to increa
 
 Verbose mode will output the users, key status and key created date
 
-Uses Boto, read here for the list of ways to configure your AWS credentials:
+Uses the Boto python library, read here for the list of ways to configure your AWS credentials:
 
     https://boto3.amazonaws.com/v1/documentation/api/latest/guide/configuration.html
 
