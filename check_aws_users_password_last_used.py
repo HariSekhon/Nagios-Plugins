9b1994004186b9bc649cec2cff4d218828002c8e
Hari Sekhon <harisekhon@gmail.com>
2019-12-19 11:00:12 +0000

    updated check_aws_users_password_last_used.py

--- check_aws_users_password_last_used.py
+++ check_aws_users_password_last_used.py
@@ -18,8 +18,6 @@
 
 Nagios Plugin to check the age of AWS user accounts last password used to find and remove old users
 
-Uses Boto's standard AWS credentials mechanism from environment, config files etc
-
 Iterates all AWS IAM users so if you have a lot of users you will need to increase the --timeout
 
 Verbose mode will output the users, date of last password use and days ago
