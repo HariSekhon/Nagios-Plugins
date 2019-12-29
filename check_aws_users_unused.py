f238ecbf2f2793d84e31a6f308ee3da7d2fa6b5e
Hari Sekhon <harisekhon@gmail.com>
2019-12-29 18:27:58 +0000

    updated check_aws_users_unused.py

--- check_aws_users_unused.py
+++ check_aws_users_unused.py
@@ -26,6 +26,8 @@ Generates an IAM credential report, then parses it to determine the time since e
 and access keys were last used, using the most recent timestamps among the password and access keys
 one as the last used age of the account
 
+Requires iam:GenerateCredentialReport on resource: *
+
 Uses the Boto python library, read here for the list of ways to configure your AWS credentials:
 
     https://boto3.amazonaws.com/v1/documentation/api/latest/guide/configuration.html
