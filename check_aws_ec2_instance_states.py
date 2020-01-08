73821ce80422f0fe6633cced236c48ff183e77e8
Hari Sekhon <harisekhon@gmail.com>
2020-01-08 17:06:02 +0000

    added thresholds per warning type

--- check_aws_ec2_instance_states.py
+++ check_aws_ec2_instance_states.py
@@ -18,7 +18,7 @@
 
 Nagios Plugin to check AWS EC2 instance states
 
-Gives counts of each state and raises warning for any state other than 'running'
+Checks thresholds for each status type (bundles stopped, stopping and shutting-down together)
 
 Uses the Boto python library, read here for the list of ways to configure your AWS credentials:
 
@@ -64,23 +64,34 @@ class CheckAwsEC2InstanceStates(NagiosPlugin):
         super(CheckAwsEC2InstanceStates, self).__init__()
         # Python 3.x
         # super().__init__()
+        self.instance_count = 0
+        self.thresholds = {}
         self.msg = 'AWS EC2 states msg not defined'
         self.ok()
 
     def add_options(self):
-        self.add_opt('-p', '--allow-pending', action='store_true', help="Don't raising warning for pending state")
-        self.add_opt('-s', '--allow-stopped', action='store_true',
-                     help="Don't raising warning for shutting-down / stopping / stopped states")
-        self.add_opt('-T', '--allow-terminated', action='store_true', help="Don't raising warning for terminated state")
+        self.add_opt('-r', '--max-running', default=None, type=int,
+                     help="Max instances in running state (default: None)")
+        self.add_opt('-p', '--max-pending', default=0, type=int,
+                     help="Max instances in pending state (default: 0)")
+        self.add_opt('-s', '--max-stopped', default=0, type=int,
+                     help="Max instances in stopped / stopping / shutting-down state (default: 0)")
+        self.add_opt('-T', '--max-terminated', default=0, type=int,
+                     help="Max instances in terminated state (default: 0)")
 
     def process_args(self):
         self.no_args()
+        self.thresholds = {
+            'running': self.get_opt('max_running'),
+            'pending': self.get_opt('max_pending'),
+            'stopped': self.get_opt('max_stopped'),
+            'terminated': self.get_opt('max_terminated'),
+        }
 
     def run(self):
         log.info('testing AWS API call')
         # there isn't really a .ping() type API endpoint so just connect to IAM and list users
         ec2 = boto3.client('ec2')
-        num_instances = 0
         #instances = ec2.describe_instances()
         describe_instances = ec2.get_paginator('describe_instances')
         statuses = OrderedDict(
@@ -92,9 +103,6 @@ class CheckAwsEC2InstanceStates(NagiosPlugin):
                 ('shutting-down', 0),
             ]
         )
-        allow_pending = self.get_opt('allow_pending')
-        allow_stopped = self.get_opt('allow_stopped')
-        allow_terminated = self.get_opt('allow_terminated')
         flatten = lambda _: [item for sublist in _ for item in sublist]
         # this might time out if there are a lot of EC2 instances
         for instances_response in describe_instances.paginate():
@@ -102,26 +110,43 @@ class CheckAwsEC2InstanceStates(NagiosPlugin):
                 log.debug('\n\n%s', jsonpp(instances_response))
             instances = flatten([_['Instances'] for _ in instances_response['Reservations']])
             for instance in instances:
-                num_instances += 1
+                self.instance_count += 1
                 #if log.isEnabledFor(logging.DEBUG):
                 #    log.debug('\n\n%s', instance)
                 statuses[instance['State']['Name']] = statuses.get(instance['State']['Name'], 0) + 1
-        self.msg = 'AWS EC2 instance total = {}'.format(num_instances)
+        self.msg = 'AWS EC2 instance total = {}'.format(self.instance_count)
+        self.check_statuses(statuses)
+
+    def check_statuses(self, statuses):
+        stopped_bundle = ['stopped', 'stopping', 'shutting-down']
+        thresholds = self.thresholds
         for status in statuses:
             self.msg += ', {} = {}'.format(status, statuses[status])
-            if status != 'running':
-                if (status == 'pending' and allow_pending):
-                    pass
-                elif status == 'terminated' and allow_terminated:
-                    pass
-                elif allow_stopped and status in ['shutting-down', 'stopping', 'stopped']:
-                    pass
-                elif statuses[status] > 0:
+            if status in thresholds:
+                if thresholds[status] is None:
+                    continue
+                elif statuses[status] > thresholds[status]:
+                    self.msg_warn()
+            elif status in stopped_bundle:
+                total_stopped = 0
+                for _ in stopped_bundle:
+                    total_stopped += statuses[_]
+                if total_stopped > thresholds['stopped']:
                     self.warning()
-                    self.msg += ' (!)'
-        self.msg += ' | total={}'.format(num_instances)
+            else:
+                self.msg_warn()
+        self.msg += ' | total={}'.format(self.instance_count)
         for status in statuses:
             self.msg += ' {}={}'.format(status, statuses[status])
+            if status in thresholds:
+                if thresholds[status] is not None:
+                    self.msg += ';{}'.format(thresholds[status])
+            elif status in stopped_bundle:
+                self.msg += ';{}'.format(thresholds['stopped'])
+
+    def msg_warn(self):
+        self.warning()
+        self.msg += ' (!)'
 
 
 if __name__ == '__main__':
