a4606a6ed1007a6406a677a5f183466680772c8d
Hari Sekhon <harisekhon@gmail.com>
2020-02-22 22:54:57 +0000

    added install_mysql_perl.sh

---
+++ setup/install_mysql_perl.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2020-02-22 22:53:05 +0000 (Sat, 22 Feb 2020)
+#
+#  https://github.com/harisekhon/nagios-plugins
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+cpanm="${CPANM:-cpanm}"
+
+set +e
+mysql_config="$(type -P mysql_config)"
+set -e
+if [ -z "$mysql_config" ]; then
+    echo "mysql_config not found in \$PATH!! ($PATH)"
+    exit 1
+fi
+
+# shellcheck disable=SC2016
+sed -ibak 's/^libs="$libs -lssl/libs="$libs -lmysqlclient -lssl/' "$mysql_config"
+
+$cpanm --notest DBD::mysql
