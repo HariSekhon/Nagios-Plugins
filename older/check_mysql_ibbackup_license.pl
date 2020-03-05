36fffa1b8c48b110876869baa72bd21ea3f7b573
Hari Sekhon <harisekhon@gmail.com>
2020-03-05 15:18:33 +0000

    updated check_mysql_ibbackup_license.pl

--- older/check_mysql_ibbackup_license.pl
+++ older/check_mysql_ibbackup_license.pl
@@ -18,6 +18,13 @@ BEGIN {
     use File::Basename;
     use lib dirname(__FILE__) . "/lib";
 }
+# Redhat RPM utils.pm install location from nagios-plugins skeleton rpm
+use lib '/usr/lib64/nagios/plugins';
+# Debian monitoring-plugins-common and Alpine nagios-plugins utils.pm install location
+use lib '/usr/lib/nagios/plugins';
+#use lib '/usr/lib/icinga';
+# Mac Homebrew utils.pm install location
+use lib '/usr/local/sbin';
 use utils qw(%ERRORS);
 
 delete @ENV{qw(IFS CDPATH ENV BASH_ENV)};
