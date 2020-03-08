a6ae0376547fc6f27f3b41466e09a659cc3a4b53
Hari Sekhon <harisekhon@gmail.com>
2020-03-08 21:28:24 +0000

    updated check_yum.py

--- check_yum.py
+++ check_yum.py
@@ -262,7 +262,7 @@ class YumTester(object):
         signal.alarm(self.timeout)
 
 
-    def sighandler(self, _, _):
+    def sighandler(self, _discarded, _discarded2):
         """Function to be called by signal.alarm to kill the plugin"""
 
         end(CRITICAL, "Yum nagios plugin has self terminated after " \
