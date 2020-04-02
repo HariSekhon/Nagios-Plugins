07433c004b82d0aeeb51974c1788a79d77b94910
Hari Sekhon <harisekhon@gmail.com>
2020-04-02 15:45:53 +0100

    updated excluded.sh

--- tests/excluded.sh
+++ tests/excluded.sh
@@ -39,6 +39,7 @@ isExcluded(){
     [[ "$prog" =~ ^\.[[:alnum:]] ]] && return 0
     [[ "$prog" = "check_puppet.rb" ]] && return 0
     [[ "$prog" =~ ./inc/* ]] && return 0
+    [[ "$prog" =~ ./dev/* ]] && return 0
     # temporarily disable check_kafka.pl check as there is an upstream library breakage
     # library bug is not auto fixed in Makefile due to Mac's new System Integrity Protection
     is_mac && [[ "$prog" =~ "check_kafka.pl" ]] && return 0
