c21cddff9ed70a0ea8d833b5b7f65b9b0adbd72c
Hari Sekhon <harisekhon@gmail.com>
2020-04-02 18:44:25 +0100

    updated test_adapter_geneos.sh

--- tests/test_adapter_geneos.sh
+++ tests/test_adapter_geneos.sh
@@ -46,7 +46,7 @@ run ./adapter_geneos.py $perl -T ./check_disk_write.pl -d .
 # copied from tests/test_git.sh
 if is_CI; then
     echo '> git branch'
-    git branch
+    git --no-pager branch
     echo
 fi
 current_branch="$(git branch | grep '^\*' | sed 's/^*[[:space:]]*//;s/[()]//g')"
