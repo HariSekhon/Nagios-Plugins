f99bb7fe9c32db6d76de044d1bc0dbc8792aa32a
Hari Sekhon <harisekhon@gmail.com>
2020-04-09 18:50:42 +0100

    updated test_redis.sh

--- tests/test_redis.sh
+++ tests/test_redis.sh
@@ -19,11 +19,12 @@ srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$srcdir/..";
 
-. ./tests/utils.sh
+# shellcheck disable=SC1090
+. "$srcdir/utils.sh"
 
 section "R e d i s"
 
-export REDIS_VERSIONS="${@:-${REDIS_VERSIONS:-2.6 2.8 3.0-alpine 3.2-alpine 4.0-alpine latest}}"
+export REDIS_VERSIONS="${*:-${REDIS_VERSIONS:-2.6 2.8 3.0-alpine 3.2-alpine 4.0-alpine latest}}"
 
 REDIS_HOST="${DOCKER_HOST:-${REDIS_HOST:-${HOST:-localhost}}}"
 REDIS_HOST="${REDIS_HOST##*/}"
@@ -54,6 +55,7 @@ test_redis(){
     docker_compose_port Redis
     DOCKER_SERVICE=redis-haproxy docker_compose_port HAProxy
     hr
+    # shellcheck disable=SC2153
     when_ports_available "$REDIS_HOST" "$REDIS_PORT" "$HAPROXY_PORT"
     hr
     if [ -z "${NOSETUP:-}" ]; then
@@ -62,13 +64,14 @@ test_redis(){
         docker exec -i "$DOCKER_CONTAINER" sh <<EOF
             echo set myKey hari | redis-cli
 EOF
-        echo done
+        echo "Done"
         hr
     fi
     if [ -n "${NOTESTS:-}" ]; then
         exit 0
     fi
-    local expected_version="${version%%-*}"
+    local expected_version
+    export expected_version="${version%%-*}"
     if [ "$version" = "latest" ]; then
         local version=".*"
     fi
@@ -86,6 +89,8 @@ EOF
 
     [ -z "${NODELETE:-}" ] && rm -v /tmp/.check_redis_config.conf
 
+    # defined and tracked in bash-tools/lib/utils.sh
+    # shellcheck disable=SC2154
     echo "Completed $run_count Redis tests"
     hr
     [ -n "${KEEPDOCKER:-}" ] ||
@@ -95,6 +100,8 @@ EOF
 }
 
 redis_tests(){
+    # $perl defined in bash-tools/lib/perl.sh (imported by utils.sh)
+    # shellcheck disable=SC2154
     run "$perl" -T ./check_redis_version.pl -v # -e "$version"  TODO: change to regex and enable this with .* for latest
 
     run_fail 2 "$perl" -T ./check_redis_version.pl -v -e 'fail-version'
@@ -103,12 +110,13 @@ redis_tests(){
     #docker cp "$DOCKER_CONTAINER":/etc/redis.conf /tmp/redis.conf
     # doesn't match
     #wget -O /tmp/redis.conf https://raw.githubusercontent.com/antirez/redis/3.0/redis.conf
-    > /tmp/.check_redis_config.conf
+    echo > /tmp/.check_redis_config.conf
     #$perl -T ./check_redis_config.pl -H $REDIS_HOST -C /tmp/.check_redis_config.conf --no-warn-extra -v | grep -v -e '^debug:' | sed 's/.*extra config found on running server://;s/=/ /g' | tr ',' '\n' | grep -v requirepass | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' | tee /tmp/.check_redis_config.conf
     run++
+    # shellcheck disable=SC2028
     echo "$perl -T ./check_redis_config.pl -H $REDIS_HOST -C /tmp/.check_redis_config.conf --no-warn-extra -v | grep -v -e '^debug:' | sed 's/.*extra config found on running server://;s/=/ /g' | tr ',' '\n' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' | tee /tmp/.check_redis_config.conf"
-    "$perl" -T ./check_redis_config.pl -H $REDIS_HOST -C /tmp/.check_redis_config.conf --no-warn-extra -v | grep -v -e '^debug:' | sed 's/.*extra config found on running server://;s/=/ /g' | tr ',' '\n' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' | tee /tmp/.check_redis_config.conf
-    run "$perl" -T ./check_redis_config.pl -H $REDIS_HOST -C /tmp/.check_redis_config.conf --no-warn-extra -vv
+    "$perl" -T ./check_redis_config.pl -H "$REDIS_HOST" -C /tmp/.check_redis_config.conf --no-warn-extra -v | grep -v -e '^debug:' | sed 's/.*extra config found on running server://;s/=/ /g' | tr ',' '\n' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' | tee /tmp/.check_redis_config.conf
+    run "$perl" -T ./check_redis_config.pl -H "$REDIS_HOST" -C /tmp/.check_redis_config.conf --no-warn-extra -vv
 
     run "$perl" -T ./check_redis_clients.pl -v
 
@@ -128,7 +136,7 @@ redis_tests(){
 
 redis_test_conn_refused(){
     run_conn_refused "$perl" -T ./check_redis_version.pl -v
-    run_conn_refused "$perl" -T ./check_redis_config.pl -H $REDIS_HOST -C /tmp/.check_redis_config.conf
+    run_conn_refused "$perl" -T ./check_redis_config.pl -H "$REDIS_HOST" -C /tmp/.check_redis_config.conf
     run_conn_refused "$perl" -T ./check_redis_clients.pl -v
     run_conn_refused "$perl" -T ./check_redis_stats.pl -v
     run_conn_refused "$perl" -T ./check_redis_key.pl -k myKey -e hari -v
