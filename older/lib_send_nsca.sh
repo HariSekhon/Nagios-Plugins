7306c2dab3cec809bc8b556ae455305274e5a9f8
Hari Sekhon <harisekhon@gmail.com>
2022-06-01 19:06:27 +0100

    updated lib_send_nsca.sh

--- older/lib_send_nsca.sh
+++ older/lib_send_nsca.sh
@@ -8,18 +8,19 @@
 #  License: see accompanying LICENSE file
 #
 
-srcdir=`dirname $0`
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 export PATH=$PATH:/usr/sbin:/usr/lib64/nagios/plugins:/usr/lib/nagios/plugins:/usr/nagios/libexec:/usr/local/nagios/libexec
 
 if ! type -P send_nsca &>/dev/null; then
     echo "CRITICAL: send_nsca was not found in path"
-    exit $CRITICAL
+    exit "$CRITICAL"
 fi
-send_nsca=`which send_nsca`
+send_nsca="$(type -P send_nsca)"
 send_nsca_cfg="$srcdir/../send_nsca.cfg"
 
 lib=lib_nagios.sh
+# shellcheck disable=SC1090
 . "$srcdir/$lib" || { echo "Lib Send NSCA: FAILED to source $lib"; exit 2; }
 
 for x in "$send_nsca" "$send_nsca_cfg"; do
